fba628d3026de8eafb30441dfbf6c62b91e85d79
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-09 23:44:07 +0900

    Title: 두 숫자의 차의 최솟값, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 231109/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -1,3 +1,4 @@
+''' itertools 사용한 풀이 
 from itertools import combinations 
 
 N = int(input()) 
@@ -9,4 +10,18 @@ min_val = 1000
 for n1,n2 in numbers: 
     if min_val > abs(n1-n2): 
         min_val = abs(n1-n2) 
+print(min_val)
+'''
+
+N = int(input()) 
+nums = list(map(int,input().split())) 
+
+# 숫자들이 오름차순으로 주어짐 
+# 두 숫자 최솟값은 반드시 인접한 두 숫자의 차이 중에 있음 
+# 초기값 적고, 비교 
+
+min_val = nums[1] - nums[0] 
+for i in range(2,N): 
+    if min_val > nums[i] - nums[i-1]: # 지금까지 나온 값들보다 더 작으면 
+        min_val = nums[i] - nums[i-1] #  갱신
 print(min_val)
