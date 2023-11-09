3df0e6754887a04fc42257195701332b84a45eac
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-09 23:51:24 +0900

    Title: 대문자로 바꾸기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/대문자로 바꾸기/change-to-capital.py
+++ 231109/대문자로 바꾸기/change-to-capital.py
@@ -1,10 +1,9 @@
+nums = []
 for i in range(5): 
-    nums = list(input().split())
+    nums.append(list(input().split()))
 
 for i in range(5): 
     for j in range(3): 
-        print(nums[i][j])
-        nums[i][j] = nums[i][j].upper()
-
-for n in nums: 
-    print(n)
+        nums[i][j] = chr(ord(nums[i][j]) + ord('A') - ord('a'))
+        print(nums[i][j], end=" ")
+    print()
