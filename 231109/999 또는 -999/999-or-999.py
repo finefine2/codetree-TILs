0fafe9738729eeda500f0a153b815b44bed1729e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-09 22:40:03 +0900

    Title: 999 또는 -999, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/999 또는 -999/999-or-999.py
+++ 231109/999 또는 -999/999-or-999.py
@@ -2,8 +2,7 @@ nums = list(map(int,input().split()))
 
 cand = []
 for n in nums: 
-    if n == 999 or -999: 
+    if n == 999 or n == -999: 
         break 
     cand.append(n) 
-
 print(max(cand), min(cand))
