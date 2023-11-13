f7e1e61eea7deefc85ed65a731cf2fb9fa947ef3
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-13 14:13:28 +0900

    Title: 배열로 사각형 만들기, Time: 121ms, Memory: 31MB, Status: Passed - CodeTree

--- 231113/배열로 사각형 만들기/print-array-in-rectangle.py
+++ 231113/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -1,6 +1,14 @@
-nums = [[1,1,1,1,1],[1,2,3,4,5],[1,3,6,10,15],[1,4,10,20,35],[1,5,15,35,70]]
+nums = [[0 for _ in range(5)] for _ in range(5)] 
+
+for i in range(5): 
+    nums[0][i] = 1 
+    nums[i][0] = 1 
+
+for i in range(1,5): 
+    for j in range(1,5): 
+        nums[i][j] = nums[i-1][j] + nums[i][j-1] 
 
 for i in range(5): 
     for j in range(5): 
-        print(nums[i][j],end=" ") 
+        print(nums[i][j],end=" ")
     print()
