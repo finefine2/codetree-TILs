4e295cb7be65816a94c411048f32b53958c8215a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-13 14:53:38 +0900

    Title: 격자에 점 그리기, Time: 235ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 231113/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,14 @@
+n, m = map(int,input().split()) 
+
+nums = [[0 for _ in range(n)] for _ in range(n)] 
+
+cnt = 1 
+for _ in range(m): 
+    r,c = map(int,input().split())
+    nums[r-1][c-1] = cnt 
+    cnt += 1 
+
+for i in range(n): 
+    for j in range(n): 
+        print(nums[i][j],end=" ") 
+    print()
