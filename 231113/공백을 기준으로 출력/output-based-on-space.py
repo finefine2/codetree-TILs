ea31534a97f751bb1d89743e78a5a744bc274a59
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-13 15:43:21 +0900

    Title: 공백을 기준으로 출력, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/공백을 기준으로 출력/output-based-on-space.py
+++ 231113/공백을 기준으로 출력/output-based-on-space.py
@@ -1,5 +1,13 @@
 ch1 = input() 
 ch2 = input()
 
-print("".join(ch1))
-print("".join(ch2))
+for c1 in ch1: 
+    if c1 == " ": 
+        continue
+    else: 
+        print(c1,end="")
+for c2 in ch2: 
+    if c2 == " ":
+        continue 
+    else: 
+        print(c2,end="")
