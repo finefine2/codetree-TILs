8925cc0023d16f909fb75ffe3a2831e8e2be2750
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-13 10:20:57 +0900

    Title: 배열의 평균, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/배열의 평균/ave-of-array.py
+++ 231113/배열의 평균/ave-of-array.py
@@ -1,27 +1,28 @@
-nums = [] 
+# 2차원 배열을 구현해 각 줄마다 정수를 입력받습니다.
+arr_2d = [
+	list(map(int, input().split()))
+	for _ in range(2)
+]
 
-for _ in range(2): 
-    nums.append(list(map(int,input().split()))) 
+# 가로 평균을 출력합니다.
+for i in range(2):
+	sum_val = 0
+	for j in range(4):
+		sum_val += arr_2d[i][j]
+	print(f"{sum_val / 4:.1f}", end=" ")
+print()
 
-# 가로 평균
-for i in range(2): 
-    sum_val = 0 
-    for j in range(4): 
-        sum_val += nums[i][j] 
-    print(f"{sum_val / 4:.1f}",end=" ")
-print() 
+# 세로 평균을 출력합니다.
+for j in range(4):
+	sum_val = 0
+	for i in range(2):
+		sum_val += arr_2d[i][j]
+	print(f"{sum_val / 2:.1f}", end=" ")
+print()
 
-# 세로 평균 
-for j in range(4): 
-    sum_val = 0 
-    for i in range(2): 
-        sum_val += nums[i][j] 
-    print(f"{sum_val / 2:.1f}",end=" ")
-print() 
-
-# 전체 평균 
+# 전체 평균을 출력합니다.
 sum_val = 0
-for i in range(4): 
-    for j in range(2): 
-        sum_val += nums[i][j] 
+for i in range(4):
+	for j in range(2):
+		sum_val += arr_2d[j][i]
 print(f"{sum_val / 8:.1f}")
