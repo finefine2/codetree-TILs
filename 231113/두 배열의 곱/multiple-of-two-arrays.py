0a957fb6b401b28762927a39ca6058eea25c34ad
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-13 10:35:16 +0900

    Title: 두 배열의 곱, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/두 배열의 곱/multiple-of-two-arrays.py
+++ 231113/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,7 +1,9 @@
 nums1 = [list(map(int,input().split())) for _ in range(3)]
-nums2 = [list(map(int,input().split())) for _ in range(3)]
+input()
+nums2 = [list(map(int,input().split())) for _ in range(3)] 
 
 ans = [[0 for _ in range(3)] for _ in range(3)]
+
 
 for i in range(3): 
     for j in range(3): 
