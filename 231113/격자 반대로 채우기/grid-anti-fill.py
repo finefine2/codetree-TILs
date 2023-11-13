c24679d32ff72fc64576d963408deba62a4db374
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-13 14:08:20 +0900

    Title: 격자 반대로 채우기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/격자 반대로 채우기/grid-anti-fill.py
+++ 231113/격자 반대로 채우기/grid-anti-fill.py
@@ -3,28 +3,37 @@ n = int(input())
 start = 1 
 
 nums = [[0 for _ in range(n)] for _ in range(n)] 
-if n % 2 == 0: 
-    for j in range(n-1,-1,-1): 
-        if j % 2 == 0: 
-            for i in range(n): 
-                nums[i][j] = start 
-                start += 1 
-        else: 
-            for i in range(n-1,-1,-1):
-                nums[i][j] = start
-                start += 1
-else: 
-    for j in range(n-1,-1,-1): 
-        if j % 2 == 1: 
-            for i in range(n): 
-                nums[i][j] = start 
-                start += 1 
-        else: 
-            for i in range(n-1,-1,-1): 
-                nums[i][j] = start
-                start += 1 
-                
-    
+# if n % 2 == 0: 
+#     for j in range(n-1,-1,-1): 
+#         if j % 2 == 0: 
+#             for i in range(n): 
+#                 nums[i][j] = start 
+#                 start += 1 
+#         else: 
+#             for i in range(n-1,-1,-1):
+#                 nums[i][j] = start
+#                 start += 1
+# else: 
+#     for j in range(n-1,-1,-1): 
+#         if j % 2 == 1: 
+#             for i in range(n): 
+#                 nums[i][j] = start 
+#                 start += 1 
+#         else: 
+#             for i in range(n-1,-1,-1): 
+#                 nums[i][j] = start
+#                 start += 1 
+
+for j in range(n-1,-1,-1): 
+    if (n-1-j) % 2 == 0: 
+        for i in range(n-1,-1,-1): 
+            nums[i][j] = start
+            start += 1 
+    else: 
+        for i in range(n): 
+            nums[i][j] = start
+            start += 1 
+   
 for i in range(n): 
     for j in range(n): 
         print(nums[i][j],end=" ")
