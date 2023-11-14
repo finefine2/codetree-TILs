8bcc3241071da5ddd0f81e83646ad2f2b231026b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-14 10:02:11 +0900

    Title: 단어로 구분하기 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/단어로 구분하기 2/separate-words-with-words-2.py
+++ 231114/단어로 구분하기 2/separate-words-with-words-2.py
@@ -1,5 +1,8 @@
 chars = list(input().split()) 
 
-for i in range(len(chars)): 
-    if i % 2 == 0:     
-        print(chars[i])
+# for i in range(len(chars)): 
+#     if i % 2 == 0:     
+#         print(chars[i])
+
+for i in range(0,len(chars),2):
+    print(chars[i])
