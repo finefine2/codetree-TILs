5e421581829cba3dcf348f0ad4d08caa2efe730e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-15 17:29:09 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
+++ 231115/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
@@ -1,6 +1,18 @@
-A = input() 
-A_even = ""
-for i in range(1,len(A),2): 
-    A_even += A[i]
+# my solution
+# A = input() 
+# A_even = ""
+# for i in range(1,len(A),2): 
+#     A_even += A[i]
 
-print(A_even[::-1])
+# print(A_even[::-1])
+
+# suggestion 
+
+A = input() 
+
+# 가장 먼저 출력할 문자의 인덱스 
+idx = len(A) - 1 
+if idx % 2 == 0: idx -= 1
+
+for i in range(idx,-1,-2): 
+    print(A[i],end="")
