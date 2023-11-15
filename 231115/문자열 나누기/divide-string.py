988dcd1092ad4c148b51f1b0790648e255acdf44
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-15 17:37:59 +0900

    Title: 문자열 나누기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/문자열 나누기/divide-string.py
+++ 231115/문자열 나누기/divide-string.py
@@ -3,6 +3,6 @@ n = int(input())
 chars = list(input().split()) 
 
 chars_new = "".join(chars) 
-
-for i in range(5): 
+idx = len(chars_new) // 5 + 1 
+for i in range(idx): 
     print(chars_new[5*i:5*i+5])
