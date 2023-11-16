2e43554a084c6637a4237f79fe9854f487885097
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-16 13:41:59 +0900

    Title: 두 번째를 첫 번째로, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/두 번째를 첫 번째로/second-to-first.py
+++ 231116/두 번째를 첫 번째로/second-to-first.py
@@ -1,12 +1,21 @@
+# s = input() 
+
+# target = s[1] 
+# change = s[0] 
+
+# s = list(s) 
+
+# for i in range(len(s)): 
+#     if s[i] == target: 
+#         s[i] = change
+
+# print("".join(s))
+
 s = input() 
 
-target = s[1] 
-change = s[0] 
-
-s = list(s) 
+a,b = s[0], s[1] 
 
 for i in range(len(s)): 
-    if s[i] == target: 
-        s[i] = change
-
-print("".join(s))
+    if s[i] == b: 
+        s = s[:i] + a + s[i+1:] 
+print(s)
