c718383f383490cea4c2f66525a8dac713ffa799
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-16 10:10:06 +0900

    Title: 특정 문자의 등장 횟수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 231116/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -3,4 +3,12 @@ s = input()
 x1 = 'ee' 
 x2 = 'eb' 
 
-print(s.count(x1), s.count(x2))
+cnt1 = 0 
+cnt2 = 0 
+for i in range(len(s)-1): 
+    if s[i] == 'e' and s[i+1] == 'e':
+        cnt1 += 1 
+    elif s[i] == 'e' and s[i+1] == 'b': 
+        cnt2 += 1 
+
+print(cnt1, cnt2)
