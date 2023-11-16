aefb3913c3eb80a9ba6b16175cbbb7d0ba44433e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-16 13:37:48 +0900

    Title: 문자열 놀이, Time: 129ms, Memory: 28MB, Status: Passed - CodeTree

--- 231116/문자열 놀이/play-with-string.py
+++ 231116/문자열 놀이/play-with-string.py
@@ -1,14 +1,34 @@
+# s,q = input().split()
+# q = int(q) 
+# s_list = list(s) 
+# for _ in range(q): 
+#     q1,q2,q3 = input().split()
+
+#     if q1 == '1':
+#         s_list[int(q2)-1], s_list[int(q3)-1] = s_list[int(q3)-1], s_list[int(q2)-1] 
+#     elif q1 == '2': 
+#         for idx, s_ in enumerate(s_list): 
+#             if q2 == s_: 
+#                 s_list[idx] = q3
+
+#     print("".join(s_list)) 
+
 s,q = input().split()
 q = int(q) 
-s_list = list(s) 
+
 for _ in range(q): 
-    q1,q2,q3 = input().split()
+    q1,a,b = input().split() 
 
-    if q1 == '1':
-        s_list[int(q2)-1], s_list[int(q3)-1] = s_list[int(q3)-1], s_list[int(q2)-1] 
-    elif q1 == '2': 
-        for idx, s_ in enumerate(s_list): 
-            if q2 == s_: 
-                s_list[idx] = q3
+    if int(q1) == 1: 
+        a,b = int(a), int(b) 
+        tmp = s[a-1] 
 
-    print("".join(s_list))
+        s = s[:a-1] + s[b-1] + s[a:] 
+        s = s[:b-1] + tmp + s[b:] 
+
+        print(s)
+    else: 
+        for i in range(len(s)): 
+            if s[i] == a: 
+                s = s[:i] + b + s[i+1:]
+        print(s)
