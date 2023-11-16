e34ac3d62e1a99d69160a5e4b26e124b54ec702a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-16 17:05:50 +0900

    Title: 문자열 계속 지우기, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/문자열 계속 지우기/keep-removing-string.py
+++ 231116/문자열 계속 지우기/keep-removing-string.py
@@ -1,18 +1,10 @@
-A = input() 
+A = input()
 B = input() 
 
-idx = 0 
-while idx <= len(A) - len(B): 
-    check = True 
-    for j in range(len(B)): 
-        if A[idx+j] != B[j]: 
-            check = False 
-            break 
-    if check: 
-        A = A[:idx] + A[idx+len(B):] 
-        if idx != 0 and A[idx-1] == B[0]: 
-            idx -= 2 
-        else: 
-            idx -= 1
-    idx += 1 
-print(A)
+'''
+solution1. 
+while A.find(B) != -1: 
+    start = A.find(B) 
+    A = A[:start] + A[start + len(B):] 
+print(A) 
+'''
