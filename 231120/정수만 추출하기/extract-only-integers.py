0490073f184043dd29dcfc9c39998b4ee938d0fa
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-20 10:15:43 +0900

    Title: 정수만 추출하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231120/정수만 추출하기/extract-only-integers.py
+++ 231120/정수만 추출하기/extract-only-integers.py
@@ -1,6 +1,5 @@
 s1,s2 = input().split()
 
-
 n1 = ""
 n2 = ""
 
@@ -9,19 +8,12 @@ cnt2 = 0
 for i in range(len(s1)): 
     if "0" <= s1[i] <= "9": 
         n1 += s1[i] 
-        cnt1 += 1 
-        if cnt1 == 2: 
-            break 
     else: 
-        continue
+        break
 
 for i in range(len(s2)): 
     if "0" <= s2[i] <= "9": 
         n2 += s2[i] 
-        cnt2 += 1 
-        if cnt2 == 2: 
-            break 
     else: 
-        continue
-
+        break
 print(int(n1)+int(n2))
