1a680177220187395d001a122e5f3f58c9b3c082
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-20 12:37:20 +0900

    Title: 미는 횟수, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 231120/미는 횟수/number-of-pushes.py
+++ 231120/미는 횟수/number-of-pushes.py
@@ -1,13 +1,16 @@
 A = input() 
 B = input() 
-cnt = 0 
 
+cnt = 0 
+# 문자열 A를 우측으로 한 칸씩 밀어보면서 비교 
 for i in range(len(A)): 
-    A = A[-1] + A[:-1] 
-    cnt += 1  
+    A = A[len(A)-1] + A[:len(A)-1]
+    cnt += 1 
+
+    # 문자열이 같을 경우 민 횟수를 출력 
     if A == B: 
-        print(cnt)
-        break
-    if cnt >= len(A): 
+        print(cnt) 
+        break 
+    
+    if i == len(A) -1: 
         print(-1)
-        break
