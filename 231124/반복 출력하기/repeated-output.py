07891a90acdea45e5fe40383574dc1f8437a7084
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-24 12:53:28 +0900

    Title: 반복 출력하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231124/반복 출력하기/repeated-output.py
@@ -0,0 +1,12 @@
+# def print_lines(n): 
+#     for _ in range(n): 
+#         print("*" * 5)
+
+# row_num = int(input())
+# print_lines(row_num)
+
+def print_lines(n): 
+    for _ in range(n): 
+        print("12345^&*()_")
+n = int(input())
+print_lines(n)
