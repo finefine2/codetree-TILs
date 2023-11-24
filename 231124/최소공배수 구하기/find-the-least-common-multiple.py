ee11f558aa0d81addcf845f0a4aa0563d69312bf
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-24 13:28:27 +0900

    Title: 최소공배수 구하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 231124/최소공배수 구하기/find-the-least-common-multiple.py
+++ 231124/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,21 +1,35 @@
-def gcd(num1,num2): 
-    ans = 0 
-    small_num = min(num1,num2)
-    for i in range(1,small_num+1): 
-        if num1 % i == 0 and num2 % i == 0: 
-            ans = i 
-    return ans 
+# my solution 
 
+# def gcd(num1,num2): 
+#     ans = 0 
+#     small_num = min(num1,num2)
+#     for i in range(1,small_num+1): 
+#         if num1 % i == 0 and num2 % i == 0: 
+#             ans = i 
+#     return ans 
 
-def lcm(num1,num2): 
-    gc = gcd(num1,num2)
-    ans = 0
-    for i in range(1,10000): 
-        ans = gc * i 
-        if ans % num1 == 0 and ans % num2 == 0: 
-            print(ans) 
-            break 
+# def lcm(num1,num2): 
+#     gc = gcd(num1,num2)
+#     ans = 0
+#     for i in range(1,10000): 
+#         ans = gc * i 
+#         if ans % num1 == 0 and ans % num2 == 0: 
+#             print(ans) 
+#             break 
 
+# n,m = map(int,input().split()) 
+# lcm(n,m) 
+
+'''
+lcm = num1 * num2 / (gcd of num1 and num2) 
+'''
 
 n,m = map(int,input().split()) 
+def lcm(n,m): 
+    gcd = 0 
+    for i in range(1,min(n,m) + 1): 
+        if n % i == 0 and m % i == 0: 
+            gcd = i 
+    print(n*m // gcd)
+
 lcm(n,m)
