e1d4bbd66140348427cdaa2352e735a5e3223d10
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-25 20:13:39 +0900

    Title: 함수를 이용한 소수 판별, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231125/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,21 @@
+# 단, 하나라도 --> bool 타입 변수 선언하여 해결
+# 이보다 깔끔한 건 함수 선언 
+
+'''
+소수 판별 
+def is_prime(n): 
+    for i in range(2,n): 
+        if n % i == 0: 
+            return False 
+    return True 
+
+'''
+
+a,b = map(int,input().split()) 
+ans = 0 
+
+for i in range(a,b+1): 
+    if is_prime(i): 
+        ans += i 
+
+print(ans)
