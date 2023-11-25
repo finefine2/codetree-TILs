948c8ca325f2eea07a7a3e0a9863011b3896414e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-25 20:24:03 +0900

    Title: 함수를 이용한 윤년 판별, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 231125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,9 +1,9 @@
 def lunar_year(n): 
-    if n % 4 == 0: 
-        return True 
-    if n % 4 == 0 and n % 100 == 0: 
-        return False 
-    if n % 4 == 0 and n % 100 == 0 and n % 400 == 0: 
+    if n % 4 == 0:  
+        if n % 100 == 0: 
+            return False 
+        if n % 100 == 0 and n % 400 == 0: 
+            return True 
         return True 
     return False 
 
