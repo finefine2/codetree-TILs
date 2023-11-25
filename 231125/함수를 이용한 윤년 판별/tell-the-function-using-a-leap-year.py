8085ad7fe45086c8c344a326b0f24670b186baa0
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-25 20:28:39 +0900

    Title: 함수를 이용한 윤년 판별, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 231125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 231125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,17 +1,25 @@
-def lunar_year(n): 
-    if n % 4 == 0:  
-        if n % 100 == 0: 
-            return False 
-        if n % 100 == 0 and n % 400 == 0: 
-            return True 
-        elif n % 100 != 0: 
-            return True
-    return False 
-
-    
 n = int(input()) 
 
-if lunar_year(n): 
+def leap_year(n): 
+    # 4의 배수가 아니면 윤년이 아님 
+    if n % 4 != 0: 
+        return False 
+
+    # 여기까지 온 이상 4의 배수로 가정 
+    # 그 중 100의 배수가 아니면 윤년
+    if n % 100 != 0: 
+        return True 
+
+    # 여기까지 온 이상 100의 배수로 가정 
+    # 그 중 400의 배수면 윤년 
+    if n % 400 == 0:
+        return True 
+    
+    # 여기까지 온 이상 100의 배수지만, 400의 배수가 아님 
+    # 따라서 윤년이 아님 
+    return False 
+
+if leap_year(n): 
     print("true") 
-else: 
+else:
     print("false")
