fd759fe2067d907a08d570b8c2f341566ed59ef1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-26 15:51:59 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231126/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 231126/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -1,23 +1,53 @@
+# my solution 
+# def is_prime(n): 
+#     if n == 1: 
+#         return False 
+#     for i in range(2,n): 
+#         if n % i == 0: 
+#             return False 
+#     return True 
+
+# def is_even(n): 
+#     ans = 0
+#     n_str = str(n)
+#     for s in n_str: 
+#         ans += int(s) 
+#     if ans % 2 == 0: 
+#         return True 
+#     return False
+
+# a,b = map(int,input().split()) 
+# cnt = 0  
+# for i in range(a,b+1): 
+#     if is_prime(i) and is_even(i):  
+#         cnt += 1 
+# print(cnt) 
+
+# given solution 
+a,b = map(int,input().split()) 
+
 def is_prime(n): 
     if n == 1: 
         return False 
     for i in range(2,n): 
         if n % i == 0: 
-            return False 
+            return False
     return True 
 
 def is_even(n): 
-    ans = 0
-    n_str = str(n)
-    for s in n_str: 
-        ans += int(s) 
-    if ans % 2 == 0: 
+    digit_sum = (n//100) + ((n//10) % 10) + (n % 10) 
+    if digit_sum % 2 == 0: 
         return True 
-    return False
+    return False 
 
-a,b = map(int,input().split()) 
-cnt = 0  
+cnt = 0 
+
+def check_num(n): 
+    if is_prime(n) and is_even(n): 
+        return True 
+    return False 
+
 for i in range(a,b+1): 
-    if is_prime(i) and is_even(i):  
-        cnt += 1 
+    if check_num(i): 
+        cnt += 1
 print(cnt)
