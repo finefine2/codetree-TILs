53f4be5fb56a62df92b1eb423c4c208a8bcff96d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-27 17:02:35 +0900

    Title: 절댓값으로 변경, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 231127/절댓값으로 변경/find-the-absolute-value.py
+++ 231127/절댓값으로 변경/find-the-absolute-value.py
@@ -1,5 +1,15 @@
 N = int(input()) 
 nums = list(map(int,input().split())) 
 
-for n in nums: 
-    print(abs(n), end=" ")
+# for n in nums: 
+#     print(abs(n), end=" ")
+
+def absolute_val(nums): 
+    for i in range(N): 
+        if nums[i] < 0: 
+            nums[i] = -nums[i] 
+    return 
+absolute_val(nums) 
+
+for elem in nums: 
+    print(elem,end=" ")
