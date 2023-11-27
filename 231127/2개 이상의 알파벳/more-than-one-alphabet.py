81cf24e616c652c67e70f22449507b838fc3d73b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-27 18:34:52 +0900

    Title: 2개 이상의 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231127/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,13 @@
+A = input() 
+def count(s): 
+    for i in range(len(s)-1): 
+        if s[i] != s[i-1]: 
+            cnt += 1
+    if cnt >= 2: 
+        return True 
+    return False 
+
+if count(A): 
+    print("Yes") 
+else: 
+    print("No")
