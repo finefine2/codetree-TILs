b126b6ae524b659d7204c7ed1955aa64600d737f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-27 16:07:10 +0900

    Title: 그 계절, 그 날, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231127/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,70 @@
+Y,M,D = map(int,input().split()) 
+'''
+31: 1 3 5 7 8 10 12 
+30: 4 6 9 11
+28: 2 
+
+'''
+# 1. 윤년여부를 체크한다 
+
+def leap_year(y): 
+    # 4의 배수가 아니면 윤년이 아님 
+    if y % 4 != 0: 
+        return False 
+
+    # 여기까지 온 이상 4의 배수로 가정 
+    # 그 중 100의 배수가 아니면 윤년
+    if y % 100 != 0: 
+        return True 
+
+    # 여기까지 온 이상 100의 배수로 가정 
+    # 그 중 400의 배수면 윤년 
+    if y % 400 == 0:
+        return True 
+    
+    # 여기까지 온 이상 100의 배수지만, 400의 배수가 아님 
+    # 따라서 윤년이 아님 
+    return False 
+
+# 2. M월 D일이 존재하는지 확인한다 
+def check_day(y,m,d): 
+    if leap_year(y): # 2월은 29일
+        if m == 2: 
+            if d <= 29: 
+                return True 
+        elif m == 4 or m == 6 or m == 9 or m == 11: 
+            if d <= 30: 
+                return True
+        else: 
+            if d <= 31: 
+                return True 
+    else: # 윤년이면 2월은 28일까지 
+        if m == 2: 
+            if d <= 28: 
+                return True 
+        elif m == 4 or m == 6 or m == 9 or m == 11: 
+            if d <= 30: 
+                return True 
+        else: 
+            if d <= 31: 
+                return True 
+    return False
+    
+# 계절을 출력한다 
+def season(m): 
+    ans = ""
+    if 3 <= m <= 5: 
+        ans = "Spring"
+    elif 6 <= m <= 8: 
+        ans = "Summer" 
+    elif 9 <= m <= 11: 
+        ans = "Fall"
+    else: 
+        ans = "Winter" 
+    return ans 
+
+
+if check_day(Y,M,D): 
+    print(season(M)) 
+else: 
+    print(-1)
