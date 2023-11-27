9dfbdc0d9ca281957851362bca315585d337a144
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-27 16:49:03 +0900

    Title: 두 정수에 대한 연산값, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231127/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,11 @@
+a,b = map(int,input().split()) 
+
+def big(num1,num2): 
+    ans1, ans2 = 0, 0
+    ans1 = max(num1,num2) + 25 
+    ans2 = min(num1,num2) * 2 
+
+    return ans1, ans2 
+
+a,b = big(a,b) 
+print(a, b)
