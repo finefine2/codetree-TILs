86e14242d5412842c7f1f2985e9ffdb3d4e8aff0
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-28 09:44:34 +0900

    Title: 두 정수에 대한 연산값 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231128/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split()) 
+
+def calc(n1,n2): 
+    if n1 > n2: 
+        n1 *= 2 
+        n2 += 10 
+    else: 
+        n1 += 10
+        n2 *= 2 
+    return n1, n2 
+
+print(calc(a,b))
