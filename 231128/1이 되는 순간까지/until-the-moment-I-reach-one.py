95af6ea292232941a2cc0447440221473bb0d0d1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-28 15:43:56 +0900

    Title: 1이 되는 순간까지, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231128/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 231128/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -14,20 +14,36 @@
 #     else: 
 #         return f(n // 10)  + remainder
 
-N = int(input()) 
+# my solution 
+# N = int(input()) 
 
-# N 이 1이 되면 끝나는 조건임 
-# N 이 1이 아니면 나누고 카운트를 추가해야한다 
-cnt = 0 
-def calc(n): 
-    global cnt 
-    cnt += 1 
-    if n == 1: 
-        return cnt 
+# # N 이 1이 되면 끝나는 조건임 
+# # N 이 1이 아니면 나누고 카운트를 추가해야한다 
+# cnt = 0 
+# def calc(n): 
+#     global cnt 
+#     cnt += 1 
+#     if n == 1: 
+#         return cnt 
     
+#     if n % 2 == 0: 
+#         return calc(n//2) 
+#     else: 
+#         return calc(n//3) 
+# calc(N) 
+# print(cnt-1) 
+
+# given solution 
+N = int(input())
+# n 에서 시작하여 1이 되기 위해 필요한 횟수를 카운팅 
+def get_num(n): 
+    # 1이면 끝나므로 0회가 더 필요함 
+    if n == 1: 
+        return 0 
+    # 짝수라면 2로 나눴을 때의 횟수 + 1번이 소요됨 
     if n % 2 == 0: 
-        return calc(n//2) 
+        return get_num(n//2) + 1 
+    # 홀수라면 3으로 나눈 몫으로 진행한 횟수 + 1번 
     else: 
-        return calc(n//3) 
-calc(N) 
-print(cnt-1)
+        return get_num(n//3) + 1 
+print(get_num(N))
