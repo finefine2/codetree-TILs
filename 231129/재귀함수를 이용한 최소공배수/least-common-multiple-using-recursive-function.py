399136427a2627ef6aede13495aa376a295930ce
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-29 16:13:50 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231129/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,17 @@
+N = int(input()) 
+nums = list(map(int,input().split())) 
+
+def gcd(n1,n2): 
+    if n2 == 0: 
+        return n1 
+    return gcd(n2, n1 % n2)
+
+def lcm(n1,n2): 
+    return (n1 * n2) // gcd(n1,n2) 
+ans = 1 
+start = lcm(nums[0],nums[1])
+for i in range(1,len(nums)-1): 
+    ans = lcm(start,nums[i])
+    
+
+print(ans)
