a6ffe235b2b45847fd446786489ad517fa82941c
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-29 12:48:02 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231129/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 231129/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,4 +1,9 @@
 N = int(input()) 
 
 nums = list(map(int,input().split())) 
-print(max(nums) )
+# a 인덱스까지 중 가장 큰 값을 리턴 
+def get_max(a):
+    if a == 0: 
+        return nums[0] 
+    return max(get_max(a-1), nums[a]) 
+print(get_max(N-1))
