aac83ac52c6e11a7e5a7d2e5643365891191d2f0
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-30 14:50:38 +0900

    Title: 코드네임, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 231130/코드네임/code-name.py
+++ 231130/코드네임/code-name.py
@@ -10,10 +10,30 @@
 #     students.append(Student(kor,eng,math))
 # student3 = students[2] 
 # print(f"student3: {student3.kor}, {student3.eng}, {student3.math}")
-people = []
+
+# my solution 
+# people = []
+# for _ in range(5): 
+#     name, score = input().split() 
+#     people.append((name,int(score)))
+# people.sort(key = lambda x: x[1]) 
+# ans = people[0] 
+# print(ans[0], ans[1])
+
+class User: 
+    def __init__(self,code_name,score): 
+        self.code_name = code_name
+        self.score = score
+
+users = [] 
+
 for _ in range(5): 
-    name, score = input().split() 
-    people.append((name,int(score)))
-people.sort(key = lambda x: x[1]) 
-ans = people[0] 
-print(ans[0], ans[1])
+    code_name, score = input().split()
+    users.append(User(code_name, int(score)))
+
+# 최소 점수 
+min_idx = 0 
+for i in range(1,5): 
+    if users[min_idx].score > users[i].score: 
+        min_idx = i 
+print(users[min_idx].code_name, users[min_idx].score)
