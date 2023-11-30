559abf007428f56d6b133ba2cee5944ae3e81b35
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-30 10:40:42 +0900

    Title: 중앙값 계산 2, Time: 203ms, Memory: 24MB, Status: Passed - CodeTree

--- 231130/중앙값 계산 2/get-median-2.py
+++ 231130/중앙값 계산 2/get-median-2.py
@@ -1,9 +1,20 @@
-n = int(input()) 
-nums = list(map(int,input().split()))
-def find_mid(a): 
-    a.sort() 
-    return a[len(a)//2]
+# my solution 
+# n = int(input()) 
+# nums = list(map(int,input().split()))
+# def find_mid(a): 
+#     a.sort() 
+#     return a[len(a)//2]
 
-for i in range(1, len(nums)+1, 2):
-    sub_num = nums[:i] 
-    print(find_mid(sub_num),end=" ")
+# for i in range(1, len(nums)+1, 2):
+#     sub_num = nums[:i] 
+#     print(find_mid(sub_num),end=" ")
+
+# given solution 
+N = int(input()) 
+nums = list(map(int,input().split())) 
+# 홀수번째 수를 지날때마다 정렬하고 중앙값 출력
+for i in range(N): 
+    if i % 2 == 0: 
+        # 오름차순 정렬 
+        sorted_nums = sorted(nums[:i+1]) 
+        print(sorted_nums[i//2],end=" ")
