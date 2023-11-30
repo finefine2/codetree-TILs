c27049db1a2d7a50686975c7589df102c7b7ba42
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-11-30 10:19:45 +0900

    Title: 2개씩 그룹짓기, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 231130/2개씩 그룹짓기/group-of-pairs.py
+++ 231130/2개씩 그룹짓기/group-of-pairs.py
@@ -1,33 +1,28 @@
+# my solution 
 N = int(input()) 
 nums = list(map(int,input().split())) 
 
-# 부분집합은 원소가 2개이며 총 N개 쌍이 나오겠네 
+# # 부분집합은 원소가 2개이며 총 N개 쌍이 나오겠네 
 
-if N == 1: 
-    print(sum(nums))
+# if N == 1: 
+#     print(sum(nums))
 
-else: 
-    min_val = -1e9
-    nums.sort()
-    for i in range(N): 
-        mid = nums[i] + nums[2*N-i-1]
-        if mid > min_val:
-            min_val = mid
-    print(min_val)
-'''
-8 2 7 1 3 5
-8 2 / 7 1 / 3 5
+# else: 
+#     min_val = -1e9
+#     nums.sort()
+#     for i in range(N): 
+#         mid = nums[i] + nums[2*N-i-1]
+#         if mid > min_val:
+#             min_val = mid
+#     print(min_val)
 
-1 2 3 5 7 8 
+# 배열 내의 최댓값과 최솟값을 묶는 것이 항상 최선임을 확인 
 
-1 8
-2 7
-3 5
-
-2 3 5 5
-
-1 5 3 7 5 6 10 4
-
-1 3 4 5 5 6 7 10
-
-'''
+nums.sort() 
+sum_max = 0 
+for i in range(N): 
+    # i번째와 2N-1-i번째 원소를 매칭 
+    sum_mid = nums[i] + nums[2*N-1-i] 
+    if sum_mid > sum_max: 
+        sum_max = sum_mid 
+print(sum_max)
