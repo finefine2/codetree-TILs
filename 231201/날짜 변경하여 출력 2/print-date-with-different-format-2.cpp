723c28b17bdea60128c0c67ac3872f0a56e20bcd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-01 18:10:39 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231201/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y, m, d;
+    char c;
+
+    cin >> m >> c >> d >> c >> y;
+
+    cout << y << "." << m << "." << d;
+
+    return 0;
+}
