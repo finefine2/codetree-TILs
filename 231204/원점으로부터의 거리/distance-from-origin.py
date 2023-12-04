bd4ba6f2aff7183015ba5ce2843b744a73deb604
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-04 14:26:46 +0900

    Title: 원점으로부터의 거리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231204/원점으로부터의 거리/distance-from-origin.py
+++ 231204/원점으로부터의 거리/distance-from-origin.py
@@ -1,15 +1,30 @@
-N = int(input())
+# my solution using Class
+# N = int(input())
 
-class Dot: 
-    def __init__(self,x,y,num): 
-        self.x = abs(x) + abs(y) 
-        self.num = num
+# class Dot: 
+#     def __init__(self,x,y,num): 
+#         self.x = abs(x) + abs(y) 
+#         self.num = num
 
+# dist = [] 
+# for i in range(1,N+1): 
+#     x,y = map(int,input().split()) 
+#     dist.append(Dot(x,y,i)) 
+
+# dist.sort(key=lambda x: x.x)
+# for d in dist: 
+#     print(d.num)
+
+N = int(input()) 
 dist = [] 
-for i in range(1,N+1): 
-    x,y = map(int,input().split()) 
-    dist.append(Dot(x,y,i)) 
 
-dist.sort(key=lambda x: x.x)
-for d in dist: 
-    print(d.num)
+def get_dist_from_origin(x,y): 
+    return abs(x) + abs(y) 
+
+for i in range(n): 
+    x,y = map(int,input().split()) 
+    dist.append((get_dist_from_origin(x,y), i+1))
+dist.sort() 
+
+for _, idx in dist: 
+    print(idx)
