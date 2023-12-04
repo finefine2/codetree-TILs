2df56a323d81fa7d2c877ca3a79580562364d417
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-04 09:54:52 +0900

    Title: 비오는 날, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231204/비오는 날/rainy-day.py
@@ -0,0 +1,15 @@
+n = int(input()) 
+
+class Forecast: 
+    def __init__(self,date,day,weather): 
+        self.date, self.day, self.weather = date, day, weather
+
+ans = Forecast("9999-99-99","","")
+for _ in range(n): 
+    date, day, weather = input().split()
+
+    f = Forecast(date,day,weather) 
+    if weather == "Rain": 
+        if ans.date >= f.date: 
+            ans = f 
+print(ans.date,ans.day,ans.weather)
