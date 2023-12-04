da495df4e896f93f41ec226ddcdd96b48797b188
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-04 23:17:49 +0900

    Title: 요일 맞추기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/요일 맞추기/guess-day-of-week.py
+++ 231204/요일 맞추기/guess-day-of-week.py
@@ -1,6 +1,7 @@
 day_list = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 dates= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 
+
 # monday_start = 1월 3일 --> 3 
 # if days % 7 == 3 -> monday 
 # 3 4 5 6 0 1 2 
@@ -15,23 +16,39 @@ def count_num_days(m,d):
     return (passed_days + d) % 7 
 
 # 문제는 d1 이 0일 때 mon일수도 있고, 1일 때 mon일 수도 있다는 사실임 
+'''
+m t w t f s s 
+0 1 2 3 4 5 6
+1 2 3 4 5 6 0
+2 3 4 5 6 0 1
+3 4 5 6 0 1 2
+4 5 6 0 1 2 3
+5 6 0 1 2 3 4
+6 0 1 2 3 4 5
 
+-0 1 2 3 4 5 6         0
+ 0 1 2 3 4 5 -1        1
+ 0 1 2 3 4 -2 -1       2
+ 0 1 2 3 -3 -2 -1      3
+ 0 1 2 -4 -3 -2 -1    4
+ 0 1 -5 -4 -3 -2 -1
+ 0 -6 -5 -4 -3 -2 -1
+'''
 def print_ans(d1,d2): 
-    dates[d1] == "Mon"
-    if d2 == d1-1: 
-        print("Sun")
-    elif d2 == d1: 
-        print("Mon") 
-    elif d2 == d1 + 1:
+    if d2 - d1 == 0: 
+        print("Mon")
+    elif d2 - d1 == 1 or d2 - d1 == -6: 
         print("Tue")
-    elif d2 == d1 + 2:
+    elif d2 - d1 == 2 or d2 - d1 == -5: 
         print("Wed")
-    elif d2 == d1 + 3:
+    elif d2 - d1 == 3 or d2 - d1 == -4: 
         print("Thu")
-    elif d2 == d1 + 4:
-        print("Fri")    
-    elif d2 == d1 + 5:
+    elif d2 - d1 == 4 or d2 - d1 == -3: 
+        print("Fri")
+    elif d2 - d1 == 5 or d2 - d1 == -2: 
         print("Sat")
+    elif d2 - d1 == 6 or d2 - d1 == -1: 
+        print("Sun")
 day1 = count_num_days(m1,d1)
 day2 = count_num_days(m2,d2)
 print_ans(day1,day2)
