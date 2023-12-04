7008a4e555c7911b4523d319967c3f21c5005923
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-04 22:34:08 +0900

    Title: Date to Date, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/Date to Date/date-to-date.py
+++ 231204/Date to Date/date-to-date.py
@@ -1,14 +1,30 @@
-m1, d1, m2, d2 = map(int,input().split())
-ans = 0 
-num_of_days = [31,28,31,30,31,30,31,31,30,31,30,31]
+# my solu
+# m1, d1, m2, d2 = map(int,input().split())
+# ans = 0 
+# num_of_days = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-while True: 
-    if m1 == m2 and d1 == d2: 
-        break 
-    ans += 1 
-    d1 += 1 
+# while True: 
+#     if m1 == m2 and d1 == d2: 
+#         break 
+#     ans += 1 
+#     d1 += 1 
 
-    if d1 > num_of_days[m1-1]: 
-        m1 += 1
-        d1 = 1
-print(ans+1)
+#     if d1 > num_of_days[m1-1]: 
+#         m1 += 1
+#         d1 = 1
+# print(ans+1)    
+
+# given sol
+m1,d1,m2,d2 = map(int,input().split())
+
+def num_of_days(m,d): 
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    for i in range(m-1): 
+        total_days += days[i]
+    total_days += d 
+
+    return total_days
+
+total_days = num_of_days(m2,d2) - num_of_days(m1,d1) +1
+print(total_days)
