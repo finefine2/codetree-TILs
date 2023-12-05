1c7718d8c5f1cf7483e85afebb64aefd04e0ef94
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-05 13:12:47 +0900

    Title: 블럭쌓는 명령2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 231205/블럭쌓는 명령2/block-stacking-commands2.py
+++ 231205/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,15 +1,13 @@
 N,K = map(int,input().split()) 
 
-nums = [0] * N 
+nums = [0] *(1+N)
 
-# def sort_arr(start,end): 
-#     global nums 
-#     for i in range(start,end+1): 
-#         nums[i] += 1 
+def sort_arr(start,end): 
+    global nums 
+    for i in range(start,end+1): 
+        nums[i] += 1 
 
 for _ in range(K): 
     A,B = map(int,input().split()) 
-    for i in range(A,B+1): 
-        nums[i] += 1 
-        
+    sort_arr(A,B)
 print(max(nums))
