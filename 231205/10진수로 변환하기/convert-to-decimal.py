3ce2e09a8314c2252ceee81e0c1b4cb8b90f534e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-05 09:56:01 +0900

    Title: 10진수로 변환하기, Time: 117ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 231205/10진수로 변환하기/convert-to-decimal.py
@@ -0,0 +1,28 @@
+'''
+from binary number to decimal  
+'''
+
+# 가장 왼쪽에서 시작하여 오른쪽 방향으로 한 칸씩 움직이며 구할 수 있음 
+# num을 0번 idx부터 i번 idx 까지 10진법으로 변환한 값이라 생각. 
+# num = prev_num * 2 + binary[i] 
+
+# binary = [1,1,1,0,1]
+# num = 0 
+
+# for i in range(5): 
+#     num = num * 2 + binary[i] 
+# print(num)
+
+n = int(input()) 
+
+n_str = str(n) 
+nums = []
+
+for i in range(len(n_str)): 
+    nums.append(int(n_str[i]))
+
+ans = 0 
+
+for i in range(len(nums)): 
+    ans = ans * 2 + nums[i] 
+print(ans)
