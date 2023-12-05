f5e8250adaab326fb5f9062b686a108ce766133f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-05 13:11:26 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231205/블럭쌓는 명령2/block-stacking-commands2.py
+++ 231205/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,13 +2,14 @@ N,K = map(int,input().split())
 
 nums = [0] * N 
 
-def sort_arr(start,end): 
-    global nums 
-    for i in range(start,end+1): 
-        nums[i] += 1 
+# def sort_arr(start,end): 
+#     global nums 
+#     for i in range(start,end+1): 
+#         nums[i] += 1 
 
 for _ in range(K): 
     A,B = map(int,input().split()) 
-    sort_arr(A,B) 
-
+    for i in range(A,B+1): 
+        nums[i] += 1 
+        
 print(max(nums))
