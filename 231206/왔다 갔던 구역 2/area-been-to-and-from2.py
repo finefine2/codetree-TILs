d00d1b725a5524e4654905b097f0cf8a0ca52df8
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-06 17:47:14 +0900

    Title: 왔다 갔던 구역 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231206/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 231206/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,25 +1,58 @@
-lines = [0] * 10000
-start = 5000
-N = int(input()) 
+# lines = [0] * 10000
+# start = 5000
+# N = int(input()) 
 
-for _ in range(N): 
-    x,d = input().split()
-    x = int(x) 
-    if d == "R": 
-        for i in range(start,start+x+1): 
-            lines[i] += 1
-        start = start + x
-    elif d == "L": 
-        for i in range(start,start-x-1,-1):
-            lines[i] += 1
-        start = start - x 
-cnt = [] 
-for i in range(len(lines)): 
-    if lines[i] >= 2: 
-        cnt.append(i) 
-ans = 0 
+# for _ in range(N): 
+#     x,d = input().split()
+#     x = int(x) 
+#     if d == "R": 
+#         for i in range(start,start+x+1): 
+#             lines[i] += 1
+#         start = start + x
+#     elif d == "L": 
+#         for i in range(start,start-x-1,-1):
+#             lines[i] += 1
+#         start = start - x 
+# cnt = [] 
+# for i in range(len(lines)): 
+#     if lines[i] >= 2: 
+#         cnt.append(i) 
+# ans = 0 
 
-for i in range(len(cnt)-1): 
-    if cnt[i+1] - cnt[i] == 1:
-        ans += 1
-print(ans-1)
+# for i in range(len(cnt)-1): 
+#     if cnt[i+1] - cnt[i] == 1:
+#         ans += 1
+# print(ans)
+n = int(input())
+
+arr = []
+
+#현재 위치
+loca = 0
+
+for _ in range(n):
+    a, b = input().split()
+    a = int(a)
+    if b == 'R':
+        right = loca + a
+        left = loca
+        loca += a
+            
+    elif b == 'L':
+        left = loca - a
+        right = loca
+        loca -= a
+
+    arr.append([left, right])
+
+check = [0] * 101
+
+for x1, x2 in arr:
+    for i in range(x1, x2):
+        check[i] += 1
+
+cnt = 0
+for i in check:
+    if i >= 2:
+        cnt += 1
+print(cnt)
