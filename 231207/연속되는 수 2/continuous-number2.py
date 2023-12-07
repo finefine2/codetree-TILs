56793fb5da1ac23a222312a01db50b701679d5b0
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-07 16:32:53 +0900

    Title: 연속되는 수 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 231207/연속되는 수 2/continuous-number2.py
+++ 231207/연속되는 수 2/continuous-number2.py
@@ -4,10 +4,14 @@ for _ in range(N):
     nums.append(int(input())) 
 max_num = -10
 cnt = 0 
-for i in range(len(nums)-1): 
-    if nums[i] == nums[i+1]:
-        cnt += 1
-    else: 
-        cnt = 0
-    max_num = max(cnt,max_num)
-print(max_num+1)
+
+if N == 1: 
+    print(1)
+else:
+    for i in range(len(nums)-1): 
+        if nums[i] == nums[i+1]:
+            cnt += 1
+        else: 
+            cnt = 0
+        max_num = max(cnt,max_num)
+    print(max_num+1)
