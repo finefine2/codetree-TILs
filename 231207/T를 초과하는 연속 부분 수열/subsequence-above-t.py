e20c30707e91ec840c5f27008213dc883acfa181
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-07 16:49:24 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 231207/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 231207/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -3,12 +3,9 @@ nums = list(map(int,input().split()))
 ans, cnt = 0, 0 
 for i in range(len(nums)): 
 
-    if i >= 1 and nums[i] > t and nums[i-1] > t: 
+    if nums[i] > t: 
         cnt += 1
     else: 
-        cnt = 1 
+        cnt = 0
     ans = max(ans,cnt) 
-if cnt == 1: 
-    print(0)
-else: 
-    print(ans)
+print(ans)
