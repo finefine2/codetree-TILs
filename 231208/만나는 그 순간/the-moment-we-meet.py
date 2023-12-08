0e53e9b4f93cdc3a03f65608063dae38c5310e33
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-08 12:53:27 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231208/만나는 그 순간/the-moment-we-meet.py
+++ 231208/만나는 그 순간/the-moment-we-meet.py
@@ -8,14 +8,14 @@ time_a = 1
 for _ in range(n): 
     d,t = input().split() 
     for _ in range(int(t)): 
-        pos_a[time_a] = pos_a[time_a-1] + (1 if d == "R" else -1)
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
         time_a += 1 
 # B 가 매 초마다 서있는 위치 기록
 time_b = 1 
 for _ in range(m): 
     d,t = input().split() 
     for _ in range(int(t)): 
-        pos_b[time_b] = pos_b[time_b-1] + (1 if d == "R" else -1) 
+        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
         time_b += 1
 
 # 최초로 만난다 
