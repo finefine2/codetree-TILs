95b31cf2d8764b8a32ce9f6128a407ae0fd80467
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-18 20:31:52 +0900

    Title: 좌우로 움직이는 로봇, Time: 108ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231218/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 231218/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -35,7 +35,7 @@ cnt = 0
 
 for i in range(1,len(pos_A)): 
     if pos_A[i-1] != pos_B[i-1] and pos_A[i] == pos_B[i]: 
-        cnt += 1
         if pos_A[i-1] * pos_B[i-1] == 0 and pos_A[i] * pos_B[i] == 0: 
-            cnt -= 1
+            continue
+        cnt += 1
 print(cnt)
