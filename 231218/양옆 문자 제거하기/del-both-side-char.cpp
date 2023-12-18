d34ae1be001aa43cbdf8be49eeca9c93f795714d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-18 21:56:33 +0900

    Title: 양옆 문자 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231218/양옆 문자 제거하기/del-both-side-char.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    int len = str.length();
+
+    str = str.erase(len - 2, 1);
+    str = str.erase(1, 1);
+
+    cout << str;
+
+    return 0;
+}
