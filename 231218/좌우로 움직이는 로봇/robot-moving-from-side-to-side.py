7476a2b2cec9455e4b19f4b274baab61e7bb072f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-18 20:25:56 +0900

    Title: 좌우로 움직이는 로봇, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231218/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,38 @@
+N,M = map(int,input().split()) 
+MAX_R = 50000
+pos_A = [0] * (MAX_R+1)
+pos_B = [0] * (MAX_R+1)
+
+time_a = 1
+time_b = 1
+
+# A가 있는 위치를 기록 
+for _ in range(N): 
+    t,d = input().split()
+    t = int(t)
+    if d == "L": 
+        for _ in range(t): 
+            pos_A[time_a] = pos_A[time_a-1] - 1 
+            time_a += 1 
+    elif d == "R": 
+        for _ in range(t): 
+            pos_A[time_a] = pos_A[time_a-1] + 1 
+            time_a += 1 
+    
+# B가 있는 위치를 기록 
+for _ in range(M): 
+    t,d = input().split()
+    t = int(t) 
+    if d == "L": 
+        for _ in range(t): 
+            pos_B[time_b] = pos_B[time_b-1] - 1
+            time_b += 1 
+    elif d == "R": 
+        for _ in range(t): 
+            pos_B[time_b] = pos_B[time_b-1] + 1 
+            time_b += 1 
+cnt = 0
+for i in range(1,len(pos_A)): 
+    if pos_A[i-1] != pos_B[i-1] and pos_A[i] == pos_B[i]: 
+        cnt += 1
+print(cnt)
