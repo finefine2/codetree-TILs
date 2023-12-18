a0e537f3c0153bd8f588a227a91eac42c80bf466
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-18 16:42:11 +0900

    Title: 선두를 지켜라, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231218/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,32 @@
+N,M = map(int,input().split()) 
+
+MAX_R = 1000 * 1000
+
+vel_A = [] 
+for _ in range(N): 
+    v,t = map(int,input().split()) 
+    # total += t
+    for _ in range(t): 
+        vel_A.append(v) 
+
+vel_B = []
+for _ in range(M): 
+    v,t = map(int,input().split())
+    for _ in range(t): 
+        vel_B.append(v) 
+pos_A = [0] * (len(vel_A) + 1)
+pos_B = [0] * (len(vel_B) + 1)
+
+cnt = [] 
+for i in range(len(pos_A)-1): 
+    pos_A[i] = pos_A[i-1] + vel_A[i] 
+    pos_B[i] = pos_B[i-1] + vel_B[i] 
+    cnt.append(pos_A[i] - pos_B[i])
+
+ans = 0     
+
+
+for i in range(len(cnt)-1): 
+    if cnt[i] == 0 and cnt[i] != cnt[i+1]: 
+        ans += 1 
+print(ans)
