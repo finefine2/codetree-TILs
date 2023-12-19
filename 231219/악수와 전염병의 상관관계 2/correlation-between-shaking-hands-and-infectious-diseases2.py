155e2857ec9d1062a240106e69c5a65c6a72fe92
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-19 13:40:21 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 231219/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231219/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -24,79 +24,40 @@
 2. 악수 횟수 카운팅하기 악수횟수가 K번이 되면 끝 
 '''
 
-# N, K, P, T = map(int,input().split()) 
-# # 최종감염 여부는 아래 리스트에 저장 
-# infects = [0] * N 
-# P = P-1 
-# # 처음 정해진 녀석은 감염 상태로 확정 
-# infects[P] = 1 
-
-# orders = [] 
-# for _ in range(T): 
-#     # t 초에 x와 y가 악수를 한다 
-#     t,x,y = map(int,input().split()) 
-#     x,y = x-1, y-1 
-#     orders.append([t,x,y]) 
-
-# orders.sort(key = lambda x: x[0]) 
-
-# # 전체 악수 횟수를 카운팅하면 안 되고 각각의 사람들별로 악수횟수를 카운팅해야?
-# cnt_list = [K] * N
-# print(infects)
-# print(cnt_list)
-# print("---------------")
-# for i in range(T): 
-#     x,y = orders[i][1], orders[i][2] 
-
-#     if infects[x] == 1:
-#         if cnt_list[x] > 0:
-#             infects[y] = 1 
-#             cnt_list[x] -= 1
-#             cnt_list[y] -= 1 
-#         elif cnt_list[x] == 0 or cnt_list[y] == 0: 
-#             continue
-#     elif infects[y] == 1:
-#         if cnt_list[y] > 0: 
-#             infects[x] = 1 
-#             cnt_list[x] -= 1
-#             cnt_list[y] -= 1
-#         elif cnt_list[y] == 0 or cnt_list[x] == 0: 
-#             continue
-#     print(i) 
-#     print(infects) 
-#     print(cnt_list)
-# for i in infects: 
-#     print(i,end="")
-
 N, K, P, T = map(int,input().split()) 
+# 최종감염 여부는 아래 리스트에 저장 
 infects = [0] * N 
 P = P-1 
+# 처음 정해진 녀석은 감염 상태로 확정 
 infects[P] = 1 
 
 orders = [] 
 for _ in range(T): 
+    # t 초에 x와 y가 악수를 한다 
     t,x,y = map(int,input().split()) 
     x,y = x-1, y-1 
     orders.append([t,x,y]) 
 
 orders.sort(key = lambda x: x[0]) 
-cnt_list = [K] * N
 
+# 전체 악수 횟수를 카운팅하면 안 되고 각각의 사람들별로 악수횟수를 카운팅해야?
+cnt_list = [K] * N
 for i in range(T): 
     x,y = orders[i][1], orders[i][2] 
+    # 전염된 사람이 아닌 개발자가 전염자와 악수하면, 전염은 되지만 전염 가능 횟수는 줄어들지 않음 
     if infects[x] == 1 and cnt_list[x] > 0:
         if infects[y] == 0: 
             infects[y] = 1 
-            cnt_list[x] -= 1
-        elif infects[y] == 1:
-            cnt_list[x] -= 1
-            cnt_list[y] -= 1
+            cnt_list[x] -= 1 
+        elif infects[y] == 1: 
+            cnt_list[x] -= 1 
+            cnt_list[y] -= 1 
     elif infects[y] == 1 and cnt_list[y] > 0: 
-        if infects[x] == 0:
+        if infects[x] == 0: 
             infects[x] = 1 
-            cnt_list[y] -= 1
-        elif infects[x] == 1:
-            cnt_list[x] -= 1
-            cnt_list[y] -= 1
+            cnt_list[y] -= 1 
+        elif infects[x] == 1: 
+            cnt_list[x] -= 1 
+            cnt_list[y] -= 1 
 for i in infects: 
     print(i,end="")
