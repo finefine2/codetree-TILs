66cd02c75b4372e916ce5dc4cf26563d5bee28ef
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-19 14:57:05 +0900

    Title: 문자에 따른 명령 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 231219/문자에 따른 명령 2/text-based-commands2.py
+++ 231219/문자에 따른 명령 2/text-based-commands2.py
@@ -1,24 +1,16 @@
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+# N E S W 
+# 0 
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
 dir_num = 0 
+s = input()
 x,y = 0,0
-s = input() 
-s_list = [] 
-for i in range(len(s)): 
-    s_list.append(s[i])
-nx, ny = 0,0 
-for s in s_list: 
-    if s == "L": 
-        dir_num = (dir_num - 1) % 4 
-        nx = x + dx[dir_num]
-        ny = y + dy[dir_num]
+for s in s: 
+    if s == "F":
+        x, y = x + dx[dir_num], y + dy[dir_num]
+    elif s == "L": 
+        dir_num = (dir_num-1) % 4 
     elif s == "R": 
-        dir_num = (dir_num + 1) % 4 
-        nx = x + dx[dir_num]
-        ny = y + dy[dir_num]
-    elif s == "F": 
-        nx = x + dx[dir_num]
-        ny = y + dy[dir_num]
-        
-print(nx, ny)
+        dir_num = (dir_num+1) % 4 
+print(x,y)
