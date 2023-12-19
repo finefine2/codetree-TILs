0a357cfa3a33c1b8358672c4f4d4979ac72a2bb5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-19 11:12:48 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231219/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231219/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -37,14 +37,18 @@ for _ in range(T):
     t,x,y = map(int,input().split()) 
     x,y = x-1, y-1 
     orders.append([t,x,y]) 
+
 orders.sort(key = lambda x: x[0]) 
+
 # 전체 악수 횟수를 카운팅하는 것 
 cnt = 0 
 for i in range(T): 
     if cnt == K: 
         break 
     t,x,y = orders[i][0], orders[i][1], orders[i][2] 
-    if infects[x] == 1 or infects[y] == 1: 
+    if infects[x] == 0 and infects[y] == 0: 
+        continue
+    else: 
         infects[x], infects[y] = 1,1 
         cnt += 1 
 
