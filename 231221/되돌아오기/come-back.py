65822893195a99376edc8a637114f729fd048b52
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-21 13:11:34 +0900

    Title: 되돌아오기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231221/되돌아오기/come-back.py
@@ -0,0 +1,25 @@
+N = int(input()) 
+
+mapper = {
+    'W': 0, 
+    'S': 1,
+    'N': 2,
+    'E': 3
+}
+drs = [0,1,-1,0]
+dcs = [-1,0,0,1]
+
+r,c = 0,0
+t = 0 
+
+for _ in range(N): 
+    d, s = input().split() 
+    move_d = mapper[d] 
+
+    for i in range(int(s)): 
+        r += drs[move_d]
+        c += dcs[move_d] 
+        t += 1
+        if r == 0 and c == 0:
+            print(t) 
+            break
