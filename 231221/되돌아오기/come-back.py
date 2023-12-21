2ef8284720ba422f11a03944051b5d5aeb209c6f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-21 13:29:43 +0900

    Title: 되돌아오기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231221/되돌아오기/come-back.py
+++ 231221/되돌아오기/come-back.py
@@ -10,18 +10,22 @@ drs = [0,1,-1,0]
 dcs = [-1,0,0,1]
 
 r,c = 0,0
-t = 0 
+t = 0
+flag = False
 ans = -1 
 for _ in range(N): 
     d, s = input().split() 
     move_d = mapper[d] 
 
     for i in range(int(s)): 
-        r += drs[move_d]
-        c += dcs[move_d] 
+        r = r + drs[move_d]
+        c = c + dcs[move_d] 
         t += 1
         
         if r == 0 and c == 0:
+            flag = True 
             ans = t
             break 
+    if flag: 
+        break 
 print(ans)
