4643bc96bb7a2c975edcfd8bbdd5fd9b5d4719bf
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-21 12:54:38 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 231221/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 231221/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -36,13 +36,17 @@ drs, dcs = [0,1,0,-1], [1,0,-1,0]
 dir_num = 0 
 
 board[r][c] = 1
+# N * M번 진행 
 for i in range(2, N * M + 1): 
+    # 현재 방향을 기준으로 다음 위치 값을 계산 
     nr, nc = r + drs[dir_num], c + dcs[dir_num]
+    # 더 이상 진행할 수 없다면 시계방향으로 회전 
     if not in_range(nr,nc) or board[nr][nc] != 0:
         dir_num = (dir_num + 1) % 4 
+    # 다음 위치로 이동한 다음 배열에 값을 입력 
     r,c = r + drs[dir_num], c+ dcs[dir_num] 
     board[r][c] = i 
-
+# 출력 
 for i in range(N): 
     for j in range(M): 
         print(board[i][j], end = " ")
