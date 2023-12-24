a0895f795344b49e0ee74c66733b0f2193661712
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-24 14:36:59 +0900

    Title: 되돌아오기 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 231224/되돌아오기 2/come-back-2.py
+++ 231224/되돌아오기 2/come-back-2.py
@@ -1,31 +1,60 @@
-s_in = input()
-r,c = 0,0
+# my solution 
+# s_in = input()
+# r,c = 0,0
 
+# drs = [0,1,0,-1]
+# dcs = [1,0,-1,0]
+
+# time = 0 
+# dirs = 3 
+# flag = False
+
+# def turn(direction, c): 
+#     if c == "L":
+#         direction = (direction-1) % 4 
+#     else: 
+#         direction = (direction+1) % 4 
+#     return direction
+
+# for s in s_in: 
+#     time += 1
+#     if s == "F":
+#         r += drs[dirs]
+#         c += dcs[dirs]
+#     else: 
+#         dirs = turn(dirs,s)
+#     if r == 0 and c == 0: 
+#         flag = True 
+#         break 
+# if flag: 
+#     print(time) 
+# else:
+#     print(-1)
+
+# given solution 
+
+dirs = input() 
+r,c = 0,0
+curr_dir = 3 
+
+# E S W N 
 drs = [0,1,0,-1]
 dcs = [1,0,-1,0]
 
-time = 0 
-dirs = 3 
 flag = False
 
-def turn(direction, c): 
-    if c == "L":
-        direction = (direction-1) % 4 
+for i in range(len(dirs)): 
+    c_dir = dirs[i] 
+    if c_dir == "L": 
+        curr_dir = (curr_dir-1) % 4
+    elif c_dir == "R": 
+        curr_dir = (curr_dir+1) % 4 
     else: 
-        direction = (direction+1) % 4 
-    return direction
-
-for s in s_in: 
-    time += 1
-    if s == "F":
-        r += drs[dirs]
-        c += dcs[dirs]
-    else: 
-        dirs = turn(dirs,s)
+        r = r + drs[curr_dir]
+        c = c + dcs[curr_dir]
     if r == 0 and c == 0: 
-        flag = True 
+        print(i+1) 
+        flag = True
         break 
-if flag: 
-    print(time) 
-else:
+if flag == False: 
     print(-1)
