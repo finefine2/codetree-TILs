a786720c2078a5d4f5c9361351c54d5d6e5d706e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-24 14:51:24 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231224/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 231224/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,27 +1,55 @@
-N, M = map(int,input().split()) 
+# my solution 
+# N, M = map(int,input().split()) 
 
+# board = [[0] * N for _ in range(N)] 
+
+# drs, dcs = [1,0,-1,0],[0,1,0,-1]
+# def in_range(r,c): 
+#     return 0 <= r < N and 0 <= c < N 
+
+# def count_block(r,c):
+#     cnt = 0  
+#     for i in range(4): 
+#         nr, nc = r + drs[i], c + dcs[i] 
+#         if in_range(nr,nc) and board[nr][nc] == 1: 
+#             cnt += 1 
+#     if cnt == 3: 
+#         return True 
+#     else: 
+#         return False 
+
+# for _ in range(M): 
+#     r,c = map(int,input().split()) 
+#     r,c = r-1, c-1 
+#     board[r][c] = 1 
+#     if count_block(r,c): 
+#         print(1) 
+#     else: 
+#         print(0)
+
+N,M = map(int,input().split()) 
 board = [[0] * N for _ in range(N)] 
 
-drs, dcs = [1,0,-1,0],[0,1,0,-1]
-def in_range(r,c): 
+drs = [1,0,-1,0]
+dcs = [0,1,0,-1]
+
+def in_range(r,c):
     return 0 <= r < N and 0 <= c < N 
 
-def count_block(r,c):
-    cnt = 0  
-    for i in range(4): 
-        nr, nc = r + drs[i], c + dcs[i] 
-        if in_range(nr,nc) and board[nr][nc] == 1: 
-            cnt += 1 
-    if cnt == 3: 
-        return True 
-    else: 
-        return False 
+def close_cnt(r,c): 
+    cnt = 0 
+    for dr, dc in zip(drs, dcs): 
+        nr,nc = r + dr, c + dc 
+        if in_range(nr,nc) and board[nr][nc] = 1: 
+            cnt += 1
+    return cnt 
 
 for _ in range(M): 
     r,c = map(int,input().split()) 
-    r,c = r-1, c-1 
+    r -= 1 
+    c -= 1 
     board[r][c] = 1 
-    if count_block(r,c): 
+    if close_cnt(r,c) == 3: 
         print(1) 
     else: 
-        print(0)
+        print(0 )
