03e7ab7ddcb4f72a134c18540ee8cb15bf517136
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-26 09:51:59 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231226/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,13 @@
+# 0 1 2 3 
+# E S W N 
+drs = [0,1,0,-1]
+dcs = [1,0,-1,0]
+N,M = map(int,input().split()) 
+
+board = [[0] * M for _ in range(N)] 
+
+board[0][0] = "A"
+
+print("A"+1)
+print(ord("A"))
+print(ord("Z"))
