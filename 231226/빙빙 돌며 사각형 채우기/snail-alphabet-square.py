ea0dae40fcd46f6d67dfc2a2c42a7d76cced2192
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-26 10:36:54 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 231226/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 231226/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -10,14 +10,55 @@ dir_num = 0
 def in_range(r,c): 
     return 0 <= r < N and 0 <= c < M
 
+alpha_list = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+            "Q","R","S","T","U","V","W","X","Y","Z"]
 def num_to_alpha(num): 
     ans = ""
-    if num <= 26: 
-        ans = chr(65+num-1) 
-    else: 
-        ans = chr(65+num-26-1) 
+    remain = num % 26 
+    ans = alpha_list[remain-1]
     return ans 
+    # if num <= 26: 
+    #     ans = chr(65+num-1) 
+    # else: 
+    #     ans = chr(65+num-26-1) 
+    # return ans 
 
+# def num_to_alpha(new_num): 
+#     ans = ""
+#     num = 0 
+#     if new_num < =26:
+#         num = 65 + new_num - 1
+#     else: 
+#         num = 65 + new_num -2
+#     if num % 26 == 0: 
+#     elif num % 26 == 1:
+#     elif num % 26 == 2:
+#     elif num % 26 == 3:
+#     elif num % 26 == 4:                
+#     elif num % 26 == 5:
+#     elif num % 26 == 6:
+#     elif num % 26 == 7:
+#     elif num % 26 == 8:
+#     elif num % 26 == 9:
+#     elif num % 26 == 10:
+#     elif num % 26 == 11:
+#     elif num % 26 == 12:
+#     elif num % 26 == 13:
+#     elif num % 26 == 14:
+#     elif num % 26 == 15:
+#     elif num % 26 == 16:
+#     elif num % 26 == 17:
+#     elif num % 26 == 18:
+#     elif num % 26 == 19:
+#     elif num % 26 == 20:
+#     elif num % 26 == 21:
+#     elif num % 26 == 22:
+#     elif num % 26 == 23:
+#     elif num % 26 == 24:
+#     elif num % 26 == 25:        
+
+# print(ord("A"))
+# print(ord("Z"))
 r,c = 0,0
 board[r][c] = "A"
 for i in range(2, N*M+1):
