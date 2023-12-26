2e9863d8ca0087a0a8fbd879139cc61d7464fdfa
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-26 19:16:19 +0900

    Title: 모이자, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 231226/모이자/gather.py
+++ 231226/모이자/gather.py
@@ -1,29 +1,32 @@
+# my solution 
+
 # N = int(input()) 
-# arr = list(map(int,input().split()))
+# nums = list(map(int,input().split())) 
 
-# max_sum = 0 
+# min_sum = 1e9 
+# '''
+# 1-1 + 1-2 + 1-3 + 1-4 
+# nums[0] * (0-0) + nums[1] * (0-1) + nums[2] * (0-2) + nums[3] * (0-3) + nums[4] * (0-4) 
+
+# '''
+# ans = [] 
 # for i in range(N): 
-#     arr[i] *= 2 
-#     sum_diff = 0 
-#     for j in range(N-1): 
-#         sum_diff += abs(arr[j+1] - arr[j]) 
-#     max_sum = max(max_sum, sum_diff) 
-#     arr[i] //= 2 
-# print(max_sum) 
+#     sum_diff = 0
+#     for j in range(N): 
+#         sum_diff += nums[j] * abs(i-j)
+#     ans.append(sum_diff)
+# print(min(ans))
 
+# given solution 
+import sys 
+INT_MAX = sys.maxsize
 N = int(input()) 
 nums = list(map(int,input().split())) 
-
-min_sum = 1e9 
-'''
-1-1 + 1-2 + 1-3 + 1-4 
-nums[0] * (0-0) + nums[1] * (0-1) + nums[2] * (0-2) + nums[3] * (0-3) + nums[4] * (0-4) 
-
-'''
-ans = [] 
+min_dist = INT_MAX
+# 각 i번째 집으로 모였을 때의 합 
 for i in range(N): 
-    sum_diff = 0
+    sum_dist = 0 
     for j in range(N): 
-        sum_diff += nums[j] * abs(i-j)
-    ans.append(sum_diff)
-print(min(ans))
+        sum_dist += abs(j-i) * nums[j] 
+    min_dist = min(min_dist,sum_dist)
+print(min_dist)
