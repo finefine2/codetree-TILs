597a98dc89f1ab4a15f44fedfb249ef329a6cbf1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-26 19:14:18 +0900

    Title: 모이자, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 231226/모이자/gather.py
+++ 231226/모이자/gather.py
@@ -17,11 +17,13 @@ nums = list(map(int,input().split()))
 min_sum = 1e9 
 '''
 1-1 + 1-2 + 1-3 + 1-4 
+nums[0] * (0-0) + nums[1] * (0-1) + nums[2] * (0-2) + nums[3] * (0-3) + nums[4] * (0-4) 
+
 '''
+ans = [] 
 for i in range(N): 
-    pos = nums[i] 
-    sum_dif = 0 
-    for j in range(N-1): 
-        sum_dif += (j) * abs(nums[j+1] - nums[j]) 
-    min_sum = min(min_sum,sum_dif) 
-print(min_sum+1)
+    sum_diff = 0
+    for j in range(N): 
+        sum_diff += nums[j] * abs(i-j)
+    ans.append(sum_diff)
+print(min(ans))
