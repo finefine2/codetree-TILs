f3326f4186d758aab8a30787931fbd979eb77835
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-26 13:54:25 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 231226/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 231226/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -1,29 +1,56 @@
-N,T = map(int,input().split()) 
-r, c = N // 2, N // 2 
+# 중복하여 더해도 상관없기 때문에 
+# N,T = map(int,input().split()) 
+# r, c = N // 2, N // 2 
 
-s_in = input() 
+# s_in = input() 
+# board = [list(map(int,input().split())) for _ in range(N)] 
+
+# dir_num = 0 
+# def in_range(r,c): 
+#     return 0 <= r < N and 0 <= c < N 
+
+# drs, dcs = [-1,0,1,0],[0,1,0,-1] 
+# ans = board[r][c] 
+# for s in s_in: 
+#     if s == "F": 
+#         nr,nc = r + drs[dir_num], c + dcs[dir_num]
+#         if not in_range(nr,nc):
+#             continue
+#         else: 
+#             r,c = nr,nc 
+#             ans += board[r][c]
+#     elif s == "L": 
+#         dir_num = (dir_num-1) % 4 
+#     elif s == "R": 
+#         dir_num = (dir_num+1) % 4
+# print(ans) 
+
+N, T = map(int,input().split()) 
+commands = input() 
 board = [list(map(int,input().split())) for _ in range(N)] 
-visited = [[False] * N for _ in range(N)] 
 
-dir_num = 0 
+ans = 0 
+r,c,move_dir = N // 2, N // 2, 0 
+
 def in_range(r,c): 
     return 0 <= r < N and 0 <= c < N 
 
-drs, dcs = [-1,0,1,0],[0,1,0,-1] 
-ans = board[r][c] 
-visited[r][c] = True 
-for s in s_in: 
-    if s == "F": 
-        nr,nc = r + drs[dir_num], c + dcs[dir_num]
-        if not in_range(nr,nc):
-            continue
+def simulate(): 
+    global ans 
+    global r, c, move_dir
+    drs = [-1,0,1,0]
+    dcs = [0,1,0,-1] 
+    for com in commands: 
+        if com == "R": 
+            move_dir = (move_dir + 1) % 4 
+        elif com == "L": 
+            move_dir = (move_dir - 1) % 4 
         else: 
-            r,c = nr,nc 
-            ans += board[r][c]
-    elif s == "L": 
-        dir_num = (dir_num-1) % 4 
-    elif s == "R": 
-        dir_num = (dir_num+1) % 4
-         
-     
+            nr, nc = r + drs[move_dir], c + dcs[move_dir] 
+            # 이동할 수 있으면 이동 
+            if in_range(nr,nc): 
+                ans += board[nr][nc] 
+                r,c = nr,nc 
+ans += board[r][c] 
+simulate() 
 print(ans)
