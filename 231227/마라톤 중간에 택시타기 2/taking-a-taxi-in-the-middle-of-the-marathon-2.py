a8ffa2dbf84eaab6a8219b9478eabb870f4045b3
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-27 14:31:44 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 184ms, Memory: 25MB, Status: Passed - CodeTree

--- 231227/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 231227/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -6,11 +6,11 @@ ans = 1e9
 
 for i in range(1,N-1): 
     dist = 0 
-    prev_idx = 0 
-    for j in range(1,N): 
-        if i == j: 
-            continue 
-        dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1]-arr[j][1]) 
+    prev_idx = 0
+    for j in range(1,N):
+        if j == i: 
+            continue
+        dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] - arr[j][1]) 
         prev_idx = j 
-    ans = min(ans,dist) 
+    ans = min(ans, dist) 
 print(ans)
