2133dbc14d2f2de3bab4417293fea9a073f067b9
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-28 15:18:18 +0900

    Title: 최고의 13위치 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231228/최고의 13위치 2/best-place-of-13-2.py
+++ 231228/최고의 13위치 2/best-place-of-13-2.py
@@ -4,7 +4,9 @@ board = [list(map(int,input().split())) for _ in range(N)]
 ans = 0
 for i in range(N): 
     for j in range(N-2): 
-        for k in range(i+1,N): 
-            for l in range(N-2): 
-                ans = max(ans, board[i][j]+board[i][j+1]+board[i][j+2]+board[k][l]+board[k][l+1]+board[k][l+2])
+        for k in range(N): 
+            for l in range(j+3,N-2): 
+                ans = max(ans, board[i][j] + board[i][j+1] + board[i][j+2] 
+                + board[k][l] + board[k][l+1] + board[k][l+2])
+
 print(ans)
