3a699cc327cde15a4a6e3afdea726f99ad6df9de
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-28 15:26:25 +0900

    Title: 특정 수와 근접한 합, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 231228/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 231228/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -7,4 +7,18 @@ for i in range(N):
     for j in range(i+1,N): 
         tmp = total - nums[i] - nums[j] 
         ans = min(ans, abs(tmp-S))
-print(ans)
+print(ans) 
+
+# given solution 
+# 두 원소를 일일이 골라서 계산 
+# N,S = map(int,input().split())
+# nums = list(map(int,input().split())) 
+# ans = 1e9 
+# array_sum = sum(nums) 
+
+# for i in range(N): 
+#     for j in range(i+1,N): 
+#         new_sum = array_sum - nums[i] - nums[j] 
+#         diff = abs(new_sum-S) 
+#         ans = min(diff,ans) 
+# print(ans)
