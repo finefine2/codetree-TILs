63dc60ff2c52f06e8e761a7bffa153f70e22fd21
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-28 11:02:45 +0900

    Title: Carry 피하기 2, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/Carry 피하기 2/escaping-carry-2.py
+++ 231228/Carry 피하기 2/escaping-carry-2.py
@@ -29,27 +29,37 @@
 # given solution 
 # 일의 자리, 십의 자리, 백의 자리, 천의 자리를 각각 더 해보기 
 # 왜냐면 input 조건이 10000 미만이기떄문
-N = int(input())
-nums = [int(input()) for _ in range(N)] 
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+	int(input())
+	for _ in range(n)
+]
 
-ans = -1 
-for i in range(N): 
-    for j in range(i+1,N): 
-        for k in range(j+1,N):
-            carry = False 
-            a,b,c = nums[i],nums[j],nums[k]
-            # 일의 자리 check
-            if a % 10 + b % 10 + c % 10 >= 10: 
-                carry = True             
-            # 십의 자리 check
-            if a % 100 // 10 + b % 100 // 10 + c % 100 // 10 >= 10: 
-                carry = True 
-            # 백의 자리 check 
-            if a % 1000 // 10 + b % 1000 // 10 + c % 1000 // 10 >= 10: 
-                carry = True
-            # 천의 자리 check
-            if a % 10000 // 10 + b % 10000 // 10 + c % 10000 // 10 >= 10: 
-                carry = True
-            if carry == False: 
-                ans = max(ans, a+b+c)
+# 모든 쌍을 다 잡아봅니다.
+ans = -1
+for i in range(n):
+	for j in range(i + 1, n):
+		for k in range(j + 1, n):
+			carry = False
+			
+			# 일의 자리에서 carry가 발생하는 경우
+			if arr[i] % 10 + arr[j] % 10 + arr[k] % 10 >= 10:
+				carry = True
+			
+			# 십의 자리에서 carry가 발생하는 경우
+			if arr[i] % 100 // 10 + arr[j] % 100 // 10 + arr[k] % 100 // 10 >= 10:
+				carry = True
+			
+			# 백의 자리에서 carry가 발생하는 경우
+			if arr[i] % 1000 // 100 + arr[j] % 1000 // 100 + arr[k] % 1000 // 100 >= 10:
+				carry = True
+			
+			# 천의 자리에서 carry가 발생하는 경우
+			if arr[i] % 10000 // 1000 + arr[j] % 10000 // 1000 + arr[k] % 10000 // 1000 >= 10:
+				carry = True
+			
+			if carry == False:
+				ans = max(ans, arr[i] + arr[j] + arr[k]);
+
 print(ans)
