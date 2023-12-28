89a774b030c39b64a01ef17e9c0ab2b25e6cc599
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-28 15:21:33 +0900

    Title: 최고의 13위치 2, Time: 140ms, Memory: 73MB, Status: Passed - CodeTree

--- 231228/최고의 13위치 2/best-place-of-13-2.py
+++ 231228/최고의 13위치 2/best-place-of-13-2.py
@@ -1,6 +1,8 @@
 N = int(input()) 
 board = [list(map(int,input().split())) for _ in range(N)] 
 
+# 두 사각형이 겹치지 않는 경우를 고려해야 함 
+# 두 사각형이 겹치는 경우는 i,k 가 동일하고 j와 l차이가 2 이내일 때
 ans = 0
 for i in range(N): 
     for j in range(N-2): 
