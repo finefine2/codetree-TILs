618598f264599e8de2e271773ecf251e505bb43a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-29 13:50:44 +0900

    Title: 구간 중 최대 합, Time: 112ms, Memory: 72MB, Status: Passed - CodeTree

--- 231229/구간 중 최대 합/max-sum-of-subarray.py
+++ 231229/구간 중 최대 합/max-sum-of-subarray.py
@@ -6,3 +6,12 @@ max_val = 0
 for i in range(N-K+1):
     max_val = max(max_val,sum(nums[i:i+K])) 
 print(max_val)
+
+# given solution 
+# max_sum = 0 
+# for i in range(N-K+1): 
+#     interval_sum = 0 
+#     for j in range(i,i+k): 
+#         interval_sum += nums[j] 
+#     max_sum = max(max_sum, interval_sum) 
+# print(max_sum)
