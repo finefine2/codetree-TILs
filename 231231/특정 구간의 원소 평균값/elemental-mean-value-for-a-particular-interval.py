25a8aa012168cb70d2f44f50428f26b33e96a6cc
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-31 17:05:10 +0900

    Title: 특정 구간의 원소 평균값, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 231231/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 231231/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -1,12 +1,30 @@
+# N = int(input())
+# nums = list(map(float,input().split())) 
+# cnt = 0 
+# for i in range(N): 
+#     for j in range(i,N):
+#         avg_val = sum(nums[i:j+1]) / (j+1 - i)
+#         exists = False 
+#         if avg_val in nums[i:j+1]: 
+#             exists = True 
+#         if exists: 
+#             cnt += 1
+# print(cnt)
+
 N = int(input())
-nums = list(map(float,input().split())) 
+nums = list(map(int,input().split())) 
+
 cnt = 0 
 for i in range(N): 
-    for j in range(i,N):
-        avg_val = sum(nums[i:j+1]) / (j+1 - i)
-        exists = False 
-        if avg_val in nums[i:j+1]: 
-            exists = True 
+    for j in range(i,N): 
+        sum_interval = 0 
+        for k in range(i,j+1): 
+            sum_interval += nums[k] 
+        avg = sum_interval / (j+1-i) 
+        exists = False
+        for k in range(i,j+1): 
+            if nums[k] == avg: 
+                exists = True 
         if exists: 
-            cnt += 1
+            cnt += 1 
 print(cnt)
