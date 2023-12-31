ec1e910903687f7be38cc6d5885472a746c7fd7c
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2023-12-31 17:02:15 +0900

    Title: 특정 구간의 원소 평균값, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 231231/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 231231/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -1,23 +1,12 @@
-# N = int(input()) 
-# nums = list(map(int,input().split())) 
-
-# max_sum = 0 
-# for i in range(N): 
-#     for j in range(i,N): 
-#         sum_val = 0 
-#         for k in range(i,j+1): 
-#             sum_val += nums[k] 
-#         max_sum = max(max_sum,sum_val)
-# print(max_sum)
-
 N = int(input())
-nums = list(map(int,input().split())) 
+nums = list(map(float,input().split())) 
 cnt = 0 
-
 for i in range(N): 
     for j in range(i,N):
-        avg_val = int(sum(nums[i:j+1]) / (j+1 - i))
-
-        if avg_val in list(set(nums[i:j])): 
-            cnt += 1 
+        avg_val = sum(nums[i:j+1]) / (j+1 - i)
+        exists = False 
+        if avg_val in nums[i:j+1]: 
+            exists = True 
+        if exists: 
+            cnt += 1
 print(cnt)
