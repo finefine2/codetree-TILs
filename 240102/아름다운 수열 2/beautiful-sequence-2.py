421570028df4626c139ac150f8d210a0e4ed597f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-02 09:48:09 +0900

    Title: 아름다운 수열 2, Time: 160ms, Memory: 24MB, Status: Passed - CodeTree

--- 240102/아름다운 수열 2/beautiful-sequence-2.py
+++ 240102/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,19 +1,31 @@
+# mine
+# N,M = map(int,input().split()) 
+# nums_A = list(map(int,input().split())) 
+# nums_B = list(map(int,input().split())) 
+
+# def compare_list(A,B): 
+#     A.sort() 
+#     B.sort() 
+    
+#     if A == B: 
+#         return True 
+#     else: 
+#         return False 
+# ans = 0
+# for i in range(N-M+1): 
+#     pos = nums_A[i:i+M] 
+#     # 두 리스트가 같은지 판단하는 함수 
+#     if compare_list(pos,nums_B): 
+#         ans += 1 
+# print(ans) 
+
+# given solution 
 N,M = map(int,input().split()) 
 nums_A = list(map(int,input().split())) 
-nums_B = list(map(int,input().split())) 
-
-def compare_list(A,B): 
-    A.sort() 
-    B.sort() 
-    
-    if A == B: 
-        return True 
-    else: 
-        return False 
-ans = 0
+nums_B = list(map(int,input().split()))
+# 모든 구간 시작점 카운트 
+cnt = 0 
 for i in range(N-M+1): 
-    pos = nums_A[i:i+M] 
-    # 두 리스트가 같은지 판단하는 함수 
-    if compare_list(pos,nums_B): 
-        ans += 1 
-print(ans)
+    if sorted(nums_A[i:i+M]) == sorted(nums_B): 
+        cnt += 1 
+print(cnt)
