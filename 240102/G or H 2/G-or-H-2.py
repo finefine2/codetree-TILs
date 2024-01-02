efbe4e6f3f52606f59dd005005fd4fac2dbbf470
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-02 19:31:16 +0900

    Title: G or H 2, Time: 215ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240102/G or H 2/G-or-H-2.py
@@ -0,0 +1,30 @@
+N = int(input()) 
+MAX_NUM = 100 
+placed = [0] * (1+MAX_NUM)
+
+for _ in range(N): 
+    p, a = input().split()
+    placed[int(p)] = 1 if a == "G" else 2 
+
+ans = 0 
+
+# 구간을 무작정 잡아보고 
+# G H 로만 구성되어있는지
+# 혹은 하나씩으로만 되어있는지 체크 
+
+for i in range(MAX_NUM+1): 
+    for j in range(i+1, MAX_NUM+1): 
+        # 사람있는지 확인
+        if placed[i] == 0 or placed[j] == 0: 
+            continue
+        cnt_G,cnt_H = 0,0
+        for k in range(i,j+1): 
+            if placed[k] == 1: 
+                cnt_G += 1 
+            elif placed[k] == 2: 
+                cnt_H += 1 
+        # 조건을 만족할 때 구간의 길이를 구해 최댓값과 비교 
+        if cnt_G == 0 or cnt_H == 0 or cnt_G == cnt_H: 
+            leng = j-i 
+            ans = max(leng,ans) 
+print(ans)
