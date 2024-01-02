ce66b66b8fc8fa7bd983a46a50147e38ffd80ad5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-02 10:29:02 +0900

    Title: 바구니 안의 사탕 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240102/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240102/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,8 +3,13 @@ placed = [0] * 100
 
 for _ in range(N): 
     c, p = map(int,input().split()) 
+    # 여러 바구니가 한 지점에 있을 수 있음
     placed[p-1] += c 
 ans = -5000
-for i in range(K,len(placed)-K+1): 
-    ans = max(ans,sum(placed[i-K-1:i+K]))
+
+# 구간 설정 함수 
+for i in range(100):
+    start = max(0,i-K) 
+    end = min(100,i+K+1)
+    ans = max(ans,sum(placed[start:end]))
 print(ans)
