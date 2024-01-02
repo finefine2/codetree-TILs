e8b172a25a366cd5b87ad49a0dfa2198778e3e8b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-02 10:05:41 +0900

    Title: 바구니 안의 사탕 2, Time: 147ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240102/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240102/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,7 +3,7 @@ placed = [0] * 100
 
 for _ in range(N): 
     c, p = map(int,input().split()) 
-    placed[p-1] = c 
+    placed[p] = c 
 ans = -5000
 for i in range(K,len(placed)-K): 
     ans = max(ans,sum(placed[i-K:i+K+1]))
