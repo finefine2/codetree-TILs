e69ccfcd3ab66a50215b0a97cfe1b434cda21e58
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-04 20:51:05 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 373ms, Memory: 26MB, Status: Passed - CodeTree

--- 240104/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240104/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,31 +1,15 @@
+# 각 자리에 들어갈 숫자를 일일이 카운팅하며 입력으로 주어진 숫자들과 전부 차이가 2인지 확인
 N = int(input())
-a1, b1, c1 = map(int, input().split())
-a2, b2, c2 = map(int, input().split())
-
+a1,b1,c1 = map(int,input().split()) 
+a2,b2,c2 = map(int,input().split()) 
+def calc_dist(num,a): 
+    return abs(num-a) <= 2 or abs(num-a) >= N-2
 cnt = 0 
-minus = 0
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        for k in range(1, N + 1):
-            if ((abs(a1 - i) <= 2 or abs(a1 - i) >= N -2) and\
-            (abs(b1 - j) <= 2 or abs(b1 - j) >= N -2) and\
-            (abs(c1 - k) <= 2 or abs(c1 - k) >= N -2)):
-                cnt += 1
-            if ((abs(a2 - i) <= 2 or abs(a2 - i) >= N -2) and\
-            (abs(b2 - j) <= 2 or abs(b2 - j) >= N -2) and\
-            (abs(c2 - k) <= 2 or abs(c2 - k) >= N -2)):
-                cnt += 1
-
-
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        for k in range(1, N + 1):
-            if ((abs(a1 - i) <= 2 or abs(a1 - i) >= N -2) and\
-            (abs(b1 - j) <= 2 or abs(b1 - j) >= N -2) and\
-            (abs(c1 - k) <= 2 or abs(c1 - k) >= N -2)) and\
-            ((abs(a2 - i) <= 2 or abs(a2 - i) >= N -2) and\
-            (abs(b2 - j) <= 2 or abs(b2 - j) >= N -2) and\
-            (abs(c2 - k) <= 2 or abs(c2 - k) >= N -2)):
-                minus += 1
-
-print(cnt - minus)
+for i in range(1,N+1): 
+    for j in range(1,N+1): 
+        for k in range(1,N+1): 
+            if calc_dist(i,a1) and calc_dist(j,b1) and calc_dist(k,c1): 
+                cnt += 1 
+            elif calc_dist(i,a2) and calc_dist(j,b2) and calc_dist(k,c2): 
+                cnt += 1 
+print(cnt)
