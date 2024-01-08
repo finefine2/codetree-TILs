007d9531827a89d78fcf28aa0ebd720f40451ef2
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 01:55:02 +0900

    Title: 줄 세우기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/줄 세우기/line-up-students.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, height, weight, num):
+        self.height = height
+        self.weight = weight
+        self.num = num
+
+n = int(input())
+
+user = []
+for i in range(n):
+    height, weight = tuple(map(int, input().split()))
+    user.append(User(height, weight, i+1))
+
+user.sort(key=lambda x: (-x.height, -x.weight, -x.num))
+
+for i in range(n):
+    print(f"{user[i].height} {user[i].weight} {user[i].num}")
