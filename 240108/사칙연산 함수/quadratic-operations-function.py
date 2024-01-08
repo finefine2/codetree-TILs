42dc1fa0dfa94bb0a687b9b022cd13a998b2fe0a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-08 13:59:51 +0900

    Title: 사칙연산 함수, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,13 @@
+a, b, c = map(str, input().split())
+
+if b == '+':
+    print(f"{a} + {c} = {int(a) + int(c)}")
+
+if b == '-':
+    print(f"{a} - {c} = {int(a) - int(c)}")
+
+if b == '/':
+    print(f"{a} / {c} = {int(a) / int(c)}")
+
+if b == '*':
+    print(f"{a} * {c} = {int(a) * int(c)}")
