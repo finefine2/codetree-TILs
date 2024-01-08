6b84ad5a38a47ffdc4d1653cdb80371a992cbc42
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 01:43:07 +0900

    Title: 총점 비교, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240109/총점 비교/compare-total-points.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, name, kor, eng, mat):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.mat = mat
+
+n = int(input())
+
+user = []
+for _ in range(n):
+    name, kor, eng, mat = tuple(map(str, input().split()))
+    user.append(User(name, kor, eng, mat))
+
+user.sort(key=lambda x: (int(x.kor) + int(x.eng) + int(x.mat)))
+
+for i in range(n):
+    print(f"{user[i].name} {user[i].kor} {user[i].eng} {user[i].mat}")
