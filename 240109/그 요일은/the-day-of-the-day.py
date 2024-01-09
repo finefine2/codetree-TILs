656adab5966c5a3a1945096940c141280fb6ebfc
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 16:38:43 +0900

    Title: 그 요일은, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,26 @@
+arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+a, b, c, d = map(int, input().split())
+st = input()
+idx = 0
+for i in range(len(day)):
+    if st == day[i]:
+        idx = i
+
+if b > d:
+    c -= 1
+    d += arr[c]
+
+ans = 0
+for i in range(a, c):
+    ans += arr[i]
+
+ans += d - b
+
+# 5
+num = ans // 7
+if ans % 7 <= idx:
+    num += 1
+
+print(num)
