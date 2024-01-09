dd4e11a4d1c179d7a0b3dd8c07b29910effca319
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 17:31:31 +0900

    Title: 최대로 겹치는 구간, Time: 251ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240109/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,9 +1,11 @@
 n = int(input())
 
-arr = [0] * (n+201)
+arr = [0] * (n+301)
 for _ in range(n):
     a, b = map(int, input().split())
-    for i in range(a+100, b+99):
+    for i in range(a+100, b+100):
         arr[i] += 1
+    if a+1 == b:
+        arr[a] += 1
 
 print(max(arr))
