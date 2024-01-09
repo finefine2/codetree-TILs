365893b89134000fe71a9f656a1a33c5c68b124b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 15:43:35 +0900

    Title: Date to Date, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240109/Date to Date/date-to-date.py
+++ 240109/Date to Date/date-to-date.py
@@ -4,7 +4,7 @@ arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 a, b, c, d = map(int, input().split())
 
 if a == c:
-    print(d - b)
+    print(d - b + 1)
 else:
     for i in range(a, c):
         d += arr[i] + 1
