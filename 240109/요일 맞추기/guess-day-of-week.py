e92c98bd37c3a2d6d50733e1743814119941bf2f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 16:00:12 +0900

    Title: 요일 맞추기, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,21 @@
+arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+a, b, c, d = map(int, input().split())
+
+ans = 0
+if a == c:
+   ans = (d - b)
+elif a > c:
+    for i in range(c, a):
+        b += arr[i]
+    b += 1
+    ans = b - d
+else:
+    for i in range(a, c):
+        d += arr[i]
+    d += 1
+    ans = d - b
+
+day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+print(day[ans % 7])
