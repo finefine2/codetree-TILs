654369346a9143a21dff7bd74b3ed2f159db9030
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-09 15:09:17 +0900

    Title: 원점으로부터의 거리, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, x, y, z, num):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.num = num
+
+n = int(input())
+
+user = []
+for i in range(n):
+    x, y = tuple(map(int, input().split()))
+    user.append(User(x, y, (x+y), i+1))
+
+user.sort(key=lambda x: x.z)
+
+for i in range(n):
+    print(f"{user[i].num}")
