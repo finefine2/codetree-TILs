d7a2e7fe6282e4ae6b06d50132c1444384b5c31c
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-10 20:26:05 +0900

    Title: 숫자 카운트, Time: 153ms, Memory: 73MB, Status: Passed - CodeTree

--- 240110/숫자 카운트/numeric-count.py
+++ 240110/숫자 카운트/numeric-count.py
@@ -54,17 +54,17 @@ for i in range(1,10):
                 z = num % 10 
                 c1, c2 = 0,0 
                 if x == i: 
-                    cnt1 += 1 
+                    c1 += 1 
                 if y == j: 
-                    cnt1 += 1 
+                    c1 += 1 
                 if z == k: 
-                    cnt1 += 1 
+                    c1 += 1 
                 if x == j or x == k: 
-                    cnt2 += 1
+                    c2 += 1
                 if y == i or y == k: 
-                    cnt2 += 1 
+                    c2 += 1 
                 if z == i or z == j: 
-                    cnt2 += 1
+                    c2 += 1
                 if cnt1 != c1 or cnt2 != c2: 
                     flag = False 
                     break 
