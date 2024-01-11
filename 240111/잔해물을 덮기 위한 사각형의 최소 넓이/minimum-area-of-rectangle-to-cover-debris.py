5b23e86b97bf507c4b9cb11b441e0de188b6c1f1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-11 16:56:59 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 268ms, Memory: 95MB, Status: Passed - CodeTree

--- 240111/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240111/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -6,32 +6,37 @@ arr = [[0] * 3030 for _ in range(3030)]
 cnt = 0
 for i in range(arr1[0], arr1[2]):
     for j in range(arr1[1], arr1[3]):
-        arr[i+1000][j+1000] = 1
+        arr[i+2000][j+2000] = 1
         
 
 for i in range(arr2[0], arr2[2]):
     for j in range(arr2[1], arr2[3]):
-        arr[i+1000][j+1000] = -1
+        arr[i+2000][j+2000] = -1
         
 
-Minx = 3000
-Miny = 3000
+Minx = 4000
+Miny = 4000
 Maxx = -1000
 Maxy = -1000
+hap = 0
 for i in range(3020):
     for j in range(3020):
         if arr[i][j] == 1:
-            if i <= Minx and j <= Miny:
+            if i <= Minx:
                 Minx = i 
+
+            if j <= Miny:
                 Miny = j 
                 # print(Minx, Miny)
-            if i >= Maxx and j >= Maxy:
+            if i >= Maxx:
                 Maxx = i
+
+            if j >= Maxy:
                 Maxy = j
                 # print(Maxx, Maxy)
 
 # print(Maxx, Maxy, Minx, Miny)
-if Maxx == -1000 and Maxy == -1000 and Minx == 3000 and Miny == 3000:
+if Maxx == -1000 and Maxy == -1000 and Minx == 4000 and Miny == 4000:
     print(0)
 else:
     print((Maxx - Minx + 1) * (Maxy - Miny + 1))
