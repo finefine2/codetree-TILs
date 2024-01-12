b382f4dbfe0fa11d58a8dbbc53f52cc57893f6dc
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-12 14:03:00 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240112/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240112/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -16,7 +16,7 @@ for i in range(n):
             arr1.append(a + j)
         a += x
 b = 0
-for i in range(n):
+for i in range(m):
     x, y = map(str, input().split())
     x = int(x)
     if y == 'L':
@@ -29,10 +29,18 @@ for i in range(n):
         b += x
 cnt = 0
 
-if len(arr1) > 1:
-    for i in range(1, len(arr1)):
-        if arr1[i] == arr2[i]:
+for i in range(1, max(len(arr2), len(arr1))):
+    if i >= len(arr1):
+        k = arr1[len(arr1) - 1]
+        if k == arr2[i]:
             cnt += 1
+    elif i >= len(arr1):
+        k = arr2[len(arr2) - 1]
+        if k == arr1[i]:
+            cnt += 1
+    elif arr1[i] == arr2[i]:
+        cnt += 1
+    
 
 print(cnt)
 
