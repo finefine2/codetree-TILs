f7d9ef167f37259c72004025ae1c709bedfafd7c
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-12 16:13:15 +0900

    Title: 문자에 따른 명령 2, Time: 149ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240112/문자에 따른 명령 2/text-based-commands2.py
+++ 240112/문자에 따른 명령 2/text-based-commands2.py
@@ -7,7 +7,7 @@ dy = [0,-1,0,1]
 i = 3
 for k in st:
     if k == 'L':
-        i = (i + 3) % 4
+        i = (i - 1) % 4
         # x += dx[(i-1) % 3] 
         # y += dy[(i-1) % 3]
     elif k == 'R':
