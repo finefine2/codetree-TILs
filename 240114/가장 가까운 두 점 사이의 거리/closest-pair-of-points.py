6a0574500a6984cfefed8168bf335ac5b754b175
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-14 23:31:03 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240114/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240114/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -1,24 +1,28 @@
-# # 두 개를 정하는 탐색 
+'''
+my solution 
+'''
+# N = int(input()) 
+# points = [tuple(map(int,input().split())) for _ in range(N)] 
+
+# ans = 1e9 
 # for i in range(N): 
 #     for j in range(i+1,N): 
-#         for k in range(N): 
-#             if k == i or k == j: 
-#                 continue 
-#             x1,x2 = lines[k] 
-#             for l in range(x1,x2+1): 
-#                 count[l] += 1 
-#         max_cnt = max(count) 
-#         ans = min(ans,max_cnt) 
+#         r1,c1 = points[i][0], points[i][1] 
+#         r2,c2 = points[j][0], points[j][1] 
+#         tmp = ((r1-r2) * (r1-r2)) + ((c1-c2) * (c1-c2))
+#         ans = min(ans, tmp)
 # print(ans) 
-
+import sys 
+INT_MAX = sys.maxsize 
 N = int(input()) 
-points = [tuple(map(int,input().split())) for _ in range(N)] 
+points = [tuple(map(int,input().split())) for _ in range(N)]
 
-ans = 1e9 
+def dist(i,j): 
+    r1,c1 = points[i]
+    r2,c2 = points[j] 
+    return (r1-r2) * (r1-r2) + (c1-c2) * (c1-c2) 
+min_dist = INT_MAX
 for i in range(N): 
     for j in range(i+1,N): 
-        r1,c1 = points[i][0], points[i][1] 
-        r2,c2 = points[j][0], points[j][1] 
-        tmp = ((r1-r2) * (r1-r2)) + ((c1-c2) * (c1-c2))
-        ans = min(ans, tmp)
-print(ans)
+        min_dist = min(min_dist,dist(i,j)) 
+print(min_dist)
