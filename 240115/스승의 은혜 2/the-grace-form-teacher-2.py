98e90e0ebef4b9a200e1f25b8317eb9614921430
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-15 19:23:46 +0900

    Title: 스승의 은혜 2, Time: 274ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240115/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,18 +1,21 @@
-from sys import stdin
-n, b  = list(map(int, stdin.readline().split()))
-base = [0 for _ in range(n)]
-for i in range(n):
-    base[i] = int(stdin.readline())//2
-base.sort()
-# print(base)
-
-base = [0]+base
-# print(base)
-cnt = 0
-total = 0
-for i in range(1, n+1): #0을 더해서 예외처리가 필요없음!\
-    total += base[i-1] + base[i]
-    if total > b:
-        break
-    cnt += 1
-print(cnt)
+N,B = map(int,input().split()) 
+prices = [int(input()) for _ in range(N)] 
+ans = 0 
+# 학생 하나에 쿠폰 쓸 때 선물 가능한 학생의 최대 명수 
+for i in range(N): 
+    # i번째에 쿠폰 쓸 때 선물 가능한 학생 수 구하기 
+    # tmp 배열을 만들어 i번째 학생에 쿠폰 쓸 때 각 학생의 선물 가격 저장 
+    tmp = [prices[j] for j in range(N)] 
+    tmp[i] /= 2 
+    # 가격 순 정렬 
+    # 가격이 낮은 순으로 사줄 때 많이 줄 수 있음 
+    tmp.sort() 
+    student, cnt = 0, 0
+    # 가장 많은 학생에게 줄 때, 학생 수는? 
+    for j in range(N): 
+        if cnt + tmp[j] > B: 
+            break 
+        cnt += tmp[j] 
+        student += 1 
+    ans = max(ans,student) 
+print(ans)
