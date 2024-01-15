3c46fd421fcbf3b8f69af494686aa7e87653d50e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-15 19:19:13 +0900

    Title: 스승의 은혜 2, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240115/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,28 +1,25 @@
 import sys
 
+max_ = -sys.maxsize
+
 N, B = map(int, input().split())
 presents = sorted([int(input()) for _ in range(N)])
 
-# 가장 비싼 선물을 반값으로 할인 받음
-discounted = presents[-1] // 2
-
-# 나머지 선물의 가격 합
-rest = sum(presents[:-1])
-
-# 선물을 줄 수 있는 학생 수
-cnt = 0
-budget = rest + discounted
-
-# 예산이 충분하면 모든 학생에게 선물을 줄 수 있음
-if budget <= B:
-    cnt = N
-# 예산이 부족하면 가장 싼 선물부터 주면서 예산을 초과하지 않는 선까지 선물을 줌
-else:
-    for i in range(N - 1):
-        if budget - presents[i] > B:
-            budget -= presents[i]
+# 반 값으로 할인 받는 선물
+for i in range(N):
+    cnt = 0
+    budget = 0
+    for j in range(N): # 나머지 선물
+        if j == i:
+            budget += presents[j] // 2
+            if budget >= B:
+                continue
+            cnt += 1
         else:
-            cnt = i + 1
-            break
+            budget += presents[j]
+            if budget >= B:
+                continue
+            cnt += 1
 
-print(cnt)
+    max_ = max(max_, cnt)
+print(max_)
