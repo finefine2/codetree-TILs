3283138979f1eb733db4928720cf38f32288f071
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-15 19:20:55 +0900

    Title: 스승의 은혜 2, Time: 162ms, Memory: 24MB, Status: Passed - CodeTree

--- 240115/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240115/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,39 +1,18 @@
-N, B = map(int, input().split())
-price_list = []
-for _ in range(N):
-    price = int(input())
-    price_list.append(price)
+from sys import stdin
+n, b  = list(map(int, stdin.readline().split()))
+base = [0 for _ in range(n)]
+for i in range(n):
+    base[i] = int(stdin.readline())//2
+base.sort()
+# print(base)
 
-# print(price_list)
-
-# 예산 내 선물 가능한 최대 학생 수 구하기
-def find_max(price_list, budget):
-    sorted_list = sorted(price_list)
-
-    # print(sorted_list)
-    tot = 0
-    for i in range(len(sorted_list)):
-        tot += sorted_list[i]
-
-        if tot > budget:
-            index = i-1
-            break
-        
-        index = i
-    # print(index)
-    return index
-        
-        
-
-for i in range(N):
-    # 쿠폰 적용할 선물 
-    price_list[i] //= 2
-
-    max_idx = 0
-   
-    idx = find_max(price_list, B)
-    max_idx = max(idx, max_idx)
-
-    price_list[i] *= 2
-
-print(max_idx)
+base = [0]+base
+# print(base)
+cnt = 0
+total = 0
+for i in range(1, n+1): #0을 더해서 예외처리가 필요없음!\
+    total += base[i-1] + base[i]
+    if total > b:
+        break
+    cnt += 1
+print(cnt)
