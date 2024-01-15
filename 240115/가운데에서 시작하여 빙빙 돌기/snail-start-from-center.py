52813910727f84b37abbc4bb71d99aca653276e7
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-15 15:47:13 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 619ms, Memory: 26MB, Status: Passed - CodeTree

--- 240115/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240115/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-arr = [[0] * (1+n) for _ in range(1+n)]
+arr = [[0] * n for _ in range(n)]
 
 cnt = 1
 
@@ -14,18 +14,18 @@ dir = 0
 x, y = n//2,n//2
 arr[n//2][n//2] = cnt
 cnt += 1
-for i in range(1, n*n):
-    if isin(x + dx[dir], y + dy[dir]) and arr[x+dx[dir]][y+dy[dir]] == 0:
-        x += dx[dir]
-        y += dy[dir]
-        arr[x][y] = cnt
-        cnt += 1
-    else:
-        dir = (dir + 1) % 4
-        x += dx[dir]
-        y += dy[dir]
-        arr[x][y] = cnt
-        cnt += 1
+num = 1
+for i in range(1, n * n):
+    for k in range(num):
+        if isin(x + dx[dir], y + dy[dir]) and arr[x + dx[dir]][y + dy[dir]] == 0:
+            x += dx[dir]
+            y += dy[dir]
+            arr[x][y] = cnt
+            cnt += 1
+    dir = (dir + 1) % 4  
+    if i % 2 == 0:
+        num += 1
+
 
 for i in range(n):
     for j in range(n):
