743ca8e84478ae0a5a3e4e6204256870398d6f6d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-15 16:13:21 +0900

    Title: 모이자, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240115/모이자/gather.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+Min = 100000
+for i in range(n):
+
+    dist = 0
+    for j in range(n):
+        dist += abs(arr[i] - j) * arr[j]
+    if Min > dist:
+        Min = dist
+
+print(Min)
