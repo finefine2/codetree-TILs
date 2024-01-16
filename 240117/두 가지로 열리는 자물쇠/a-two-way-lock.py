bda5565a3e47643348ac2bfd25331d5227b38d6d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-17 01:37:37 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240117/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,18 @@
+n = int(input())
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+def diff(num):
+    if num >= n//2 + 1:
+        num = n - num
+    return abs(num)
+
+
+ans = 0
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        for k in range(1,n+1):
+            if (diff(i - a1) <= 2 and diff(j - b1) <= 2 and diff(k - c1) <= 2) or (diff(i - a2) <= 2 and diff(j - b2) <= 2 and diff(k - c2) <= 2):
+                ans += 1
+
+print(ans)
