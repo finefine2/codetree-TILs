5187b855beeff711a41c89bf77b61c80dc6fb8b9
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-16 18:11:44 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240116/G or H 3/G-or-H-3.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+arr = [0] * 10010
+for _ in range(n):
+    a, b = map(str, input().split())
+    if b == 'H':
+        arr[int(a)] = 2
+    else:
+        arr[int(a)] = 1
+
+Max = -1
+for i in range(10000):
+    ans = 0
+    for j in range(i, i+k+1):
+        ans += arr[j]
+    if Max < ans:
+        Max = ans
+    
+print(Max)
