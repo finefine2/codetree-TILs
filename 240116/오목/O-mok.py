49ce058d80b53c2a9b11568d0cc5ab53bdd78fff
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-16 16:28:09 +0900

    Title: 오목, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240116/오목/O-mok.py
@@ -0,0 +1,31 @@
+arr = []
+for i in range(19):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+ans = 0
+x, y = 0,0
+for i in range(15):
+    for j in range(19):
+        if arr[i][j] == 1 and arr[i+1][j] == 1 and arr[i+2][j] == 1 and arr[i+3][j] == 1 and arr[i+4][j] == 1:
+            ans = 1
+            x = i+2
+            y = j
+        elif arr[i][j] == 2 and arr[i+1][j] == 2 and arr[i+2][j] == 2 and arr[i+3][j] == 2 and arr[i+4][j] == 2:
+            ans = 2
+            x = i+2
+            y = j
+
+for i in range(19):
+    for j in range(15):
+        if arr[i][j] == 1 and arr[i][j+1] == 1 and arr[i][j+2] == 1 and arr[i][j+3] == 1 and arr[i][j+4] == 1:
+            ans = 1
+            x = i
+            y = j+2
+        if arr[i][j] == 2 and arr[i][j+1] == 2 and arr[i][j+2] == 2 and arr[i][j+3] == 2 and arr[i][j+4] == 2:
+            ans = 2
+            x = i
+            y = j+2
+
+print(ans)
+print(x+1, y+1)
