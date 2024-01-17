139543e492c1076a0d10946c6add64d5ef5518e2
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-17 14:48:33 +0900

    Title: 개발팀의 능력, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240117/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,31 @@
+arr = list(map(int, input().split()))
+n = len(arr)
+
+def diff(a, b, c, d):
+    
+    num = sys.maxsize
+    sum1 = abs(arr[a] + arr[b])
+    sum2 = abs(arr[c] + arr[d])
+    sum3 = sum(arr) - sum1 - sum2 
+
+    Max = abs(sum1 - sum2)
+    Max = max(Max, abs(sum2 - sum3))
+    Max = max(Max, abs(sum3 - sum1))
+
+    if sum1 == sum2 or sum2 == sum3 or sum3 == sum1:
+        return num
+    
+    return Max
+
+import sys
+Min = sys.maxsize
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(n):
+            for l in range(k+1, n):
+                if i == k or i == l or j == k or j == l:
+                    continue
+                Min = min(Min, diff(i, j, k, l))
+
+print(Min)
