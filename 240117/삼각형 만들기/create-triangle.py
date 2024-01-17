8ba5bf54b5fefc1530f914314fcf6ff584f318aa
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-17 16:06:19 +0900

    Title: 삼각형 만들기, Time: 151ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240117/삼각형 만들기/create-triangle.py
+++ 240117/삼각형 만들기/create-triangle.py
@@ -15,6 +15,7 @@ for i in range(n):
                 Max = max(Max, abs(arr[i][1] - arr[j][1]) * abs(arr[j][0] - arr[k][0]))
             elif arr[i][1] == arr[j][1] and arr[j][0] == arr[k][0]:
                 Max = max(Max, abs(arr[i][0] - arr[j][0]) * abs(arr[j][1] - arr[k][1]))
-            
+            elif arr[i][1] == arr[k][1] and arr[j][0] == arr[k][0]:
+                Max = max(Max, abs(arr[i][0] - arr[j][0]) * abs(arr[j][1] - arr[k][1]))
 
 print(Max)
