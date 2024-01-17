1925ab629dfc43b1f372507398df50f33efdecb0
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-17 19:07:08 +0900

    Title: 스승의 은혜 3, Time: 185ms, Memory: 25MB, Status: Passed - CodeTree

--- 240117/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240117/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -6,25 +6,41 @@ for i in range(n):
     p, s = map(int, input().split())
     arr.append((p, s))
 
-arr.sort(lambda x : x[0] + x[1])
+# arr.sort(lambda x : x[0] + x[1])
 
-i = 0
-num = 0
-s = 0
-while True:
-    s += arr[i][0]
-    s += arr[i][1]
+ans = 0
+for i in range(n):
+    price = [0] * n
     
-    if s > b:
-        if s - (arr[i][0] * 0.5) <= b:
-            num += 1
-            break
-        else:
-            break
-    i += 1
-    num += 1
+    for j in range(n):
+        price[j] = arr[j][0] + arr[j][1]
+    price[i] = arr[i][0]//2 + arr[i][1]
 
-print(num)
+    price.sort()
 
-# for a, b in arr:
-#     print(a, b)
+    # i = 0
+    num = 0
+    s = 0
+
+    for j in range(n):
+        if price[j] + s > b:
+            break
+
+        s += price[j]
+        num += 1
+
+    ans = max(ans, num)
+    # while True:
+    #     s += arr[i][0]
+    #     s += arr[i][1]
+    
+    #     if s > b:
+    #         if s - (arr[i][0] * 0.5) <= b:
+    #             num += 1
+    #             break
+    #         else:
+    #             break
+    #     i += 1
+    #     num += 1
+
+print(ans)
