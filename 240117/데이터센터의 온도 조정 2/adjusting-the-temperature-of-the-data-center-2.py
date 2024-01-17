3f493b6291795ed8745eb9cb9c445628d064cb2e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-17 23:49:55 +0900

    Title: 데이터센터의 온도 조정 2, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240117/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,23 @@
+n, c, g, h = map(int, input().split())
+
+def tmp(num, a, b):
+    if num < a:
+        return c
+    elif a <= num <= b:
+        return g
+    else:
+        return h
+
+Max = -1
+arr = []
+for i in range(n):
+    a, b = map(int, input().split())
+    arr.append((a, b))
+
+for i in range(1000):
+    s = 0
+    for j in range(4):
+        s += tmp(i, arr[j][0], arr[j][1])
+    Max = max(Max, s)
+
+print(Max)
