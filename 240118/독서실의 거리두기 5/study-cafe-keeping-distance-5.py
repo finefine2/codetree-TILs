ab8e9f3510cac0e9c0191d6d95a874edd81b630a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-18 15:37:49 +0900

    Title: 독서실의 거리두기 5, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240118/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,12 +1,27 @@
 n = int(input())
 
 arr = input()
+for i in range(20):
+    arr += '2'
+
+import sys
+one = sys.maxsize
+idx = 0
+flag = False
+for i in range(n):
+    if arr[i] == '1':
+        if flag:
+           one = min(one, (i - idx))
+
+        flag = True
+        idx = i
+
 
 ans = 0
 for i in range(n):
     dist = 0
     if arr[i] == '0':
-        for j in range(1, n - i):
+        for j in range(1, n):
             if i - j >= 0 and i + j < n:
                 if arr[i-j] == '1' or arr[i+j] == '1':
                     dist = j
@@ -18,12 +33,14 @@ for i in range(n):
                     break
 
             elif i - j >= 0 and i + j >= n:
-                if arr[i+j] == '1':
+                if arr[i-j] == '1':
                     dist = j
                     break
             else:
                 break
-    
     ans = max(dist, ans)
 
-print(ans)
+
+
+
+print(min(ans, one))
