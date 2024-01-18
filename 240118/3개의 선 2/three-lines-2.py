1b8e9470ac9170c7ff8b9c8e0ac5c4fe37cbb6c2
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-18 12:37:56 +0900

    Title: 3개의 선 2, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240118/3개의 선 2/three-lines-2.py
+++ 240118/3개의 선 2/three-lines-2.py
@@ -9,13 +9,12 @@ check = False
 for i in range(11):
     for j in range(11):
         for k in range(11):
-            flag = 0
+            cnt = 0
             for l in range(len(arr)):
                 if arr[l][0] == i or arr[l][1] == i or arr[l][0] == j or arr[l][1] == j or arr[l][0] == k or arr[l][1] == k:
-                    flag = 2
-                else:
-                    flag = 3
-            if flag == 2:
+                    cnt += 1
+                
+            if cnt == len(arr):
                 check = True
 
 if check:
