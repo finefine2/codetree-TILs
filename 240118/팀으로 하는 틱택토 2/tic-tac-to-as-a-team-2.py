2728737a8384983fd05da72105d86762d2d8b08a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-18 13:57:39 +0900

    Title: 팀으로 하는 틱택토 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240118/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,31 @@
+arr = []
+for i in range(3):
+    tmp = input()
+    arr.append(tmp)
+
+
+def checkline(line):
+    check = [0] * 9
+    for k in line:
+        check[int(k)] += 1
+    
+    # 1부터니까 1:로 해서 확인한다. 그리고 2가 넘는게 있으면 true
+    for k in check[1:]:
+        if k >= 2:
+            return True
+    return False
+
+ans = 0
+# 행
+for row in arr:
+    if checkline(row):
+        ans += 1
+# 열
+for col in range(3):
+    if checkline([arr[row][col] for row in range(3)]):
+        ans += 1
+# 대각선
+if checkline([arr[i][i] for i in range(3)]) or checkline([arr[i][2 - i] for i in range(3)]):
+    ans += 1
+
+print(ans)
