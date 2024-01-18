601a8a25c184b1a05a529ced1769253770a3cf1f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-18 12:47:47 +0900

    Title: 3개의 선 2, Time: 164ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240118/3개의 선 2/three-lines-2.py
+++ 240118/3개의 선 2/three-lines-2.py
@@ -5,31 +5,31 @@ for i in range(n):
     a, b = map(int, input().split())
     arr.append((a, b))
 
-x = set()
-y = set()
+# x = set()
+# y = set()
 
-for xx, yy in arr:
-    x.add(xx)
-    y.add(yy)
+# for xx, yy in arr:
+#     x.add(xx)
+#     y.add(yy)
 
-if len(x) + len(y) <= 3:
-    print(1)
-else:
-    print(0)
-
-# check = False
-# for i in range(11):
-#     for j in range(11):
-#         for k in range(11):
-#             cnt = 0
-#             for l in range(len(arr)):
-#                 if arr[l][0] == i or arr[l][1] == i or arr[l][0] == j or arr[l][1] == j or arr[l][0] == k or arr[l][1] == k:
-#                     cnt += 1
-                
-#             if cnt == len(arr):
-#                 check = True
-
-# if check:
+# if len(x) <= 3 or len(y) <= 3:
 #     print(1)
 # else:
 #     print(0)
+
+check = False
+for i in range(11):
+    for j in range(11):
+        for k in range(11):
+            cnt = 0
+            for l in range(len(arr)):
+                if arr[l][0] == i or arr[l][1] == j or arr[l][0] == k:
+                    cnt += 1
+                
+            if cnt == len(arr):
+                check = True
+
+if check:
+    print(1)
+else:
+    print(0)
