0c6d2944dcd51306fbd47a77f7705e84b6eb4d41
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-18 20:27:36 +0900

    Title: 정보에 따른 숫자 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240118/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240118/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,33 +1,22 @@
-t, a, b = map(int, input().split())
+T, a, b = map(int,input().split()) 
+pos = [] 
+for _ in range(T): 
+    c,x = input().split()
+    pos.append([c,int(x)]) 
 
-arr = [0] * 1001
-for i in range(t):
-    c, x = map(str, input().split())
-    if c == 'S':
-        arr[int(x)] = 1
-    else:
-        arr[int(x)] = 2
-
-ans = 0
-for i in range(a, b+1):
-    for j in range(1000):
-        if i - j >= 0 and i + j < 1000:
-            if arr[i-j] == 1 or arr[i+j] == 1:
-                ans += 1
-                break
-            elif arr[i-j] == 2 or arr[i+j] == 2:
-                break
-        elif i - j >= 0 and i + j >= 1000:
-            if arr[i-j] == 1:
-                ans += 1
-                break
-            elif arr[i-j] == 2:
-                break
-        elif i - j < 0 and i + j < 1000:
-            if arr[i+j] == 1:
-                ans += 1
-                break
-            elif arr[i+j] == 2:
-                break
-
+# 매 위치를 갱신하면서 체크하기 
+def check_dist(pos,num): 
+    dist1 = 10000 
+    dist2 = 10000
+    for c,x in pos: 
+        if c == "S": 
+            dist1 = min(dist1, abs(num-x))
+        elif c == "N": 
+            dist2 = min(dist2, abs(num-x)) 
+    if dist1 <= dist2: 
+        return True 
+ans = 0 
+for i in range(a,b+1): 
+    if check_dist(pos,i):
+        ans += 1 
 print(ans)
