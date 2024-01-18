c3b2071fa26ac9af4e9b8257614f8734880b1d5a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-18 20:13:21 +0900

    Title: 흥미로운 숫자 2, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240118/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240118/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,16 +1,16 @@
-x, y = map(int, input().split())
+X,Y = map(int,input().split()) 
 
-def inter(num):
-    arr = list(str(num))
-    arr.sort()
-    if (arr.count(arr[0]) == 1 and arr.count(arr[1]) == len(arr) - 1) or (arr.count(arr[0]) == len(arr) - 1 and arr.count(arr[len(arr) - 1]) == 1):
-        return True
-    else:
-        return False
-        
-ans = 0
-for i in range(x, y+1):
-    if inter(i):
+# 한 자리 '만' 다른 것을 어떻게 표현하지 
+def check_num(num): 
+    num_list = list(str(num)) 
+    check = len(num_list) - 1
+    num_a = num_list[0] 
+    num_b = num_list[1] 
+    if num_list.count(num_a) == check or num_list.count(num_b) == check: 
+        return True 
+
+ans = 0 
+for i in range(X,Y+1): 
+    if check_num(i): 
         ans += 1
-
 print(ans)
