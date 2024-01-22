6f5f016e95cbacdaeb8b84e0e90c0d5385386447
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-22 14:33:44 +0900

    Title: 구간 잘 나누기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240122/구간 잘 나누기/divide-sections-well.py
+++ 240122/구간 잘 나누기/divide-sections-well.py
@@ -5,8 +5,40 @@ arr = list(map(int, input().split()))
 import sys
 MinMax = sys.maxsize
 
-for i in range(n-m+1):
-    s = sum(arr[i:i+m])
-    MinMax = min(MinMax, max(s, sum(arr[i+m:])))
+# for i in range(n-m+1):
+#     s = sum(arr[i:i+m])
+#     MinMax = min(MinMax, max(s, sum(arr[i+m:])))
 
-print(MinMax)
+def find(num):
+    s = 0
+    div = 1
+    for i in range(n):
+        if arr[i] > num:
+            return False
+        if s + arr[i] > num:
+            s = 0
+            div += 1
+        
+        s += arr[i]
+    if div <= m:
+        return True
+        
+
+for i in range(1, sum(arr)):
+    if find(i):
+        print(i)
+        break
+    # s = 0
+    # div = 1
+    # for j in range(n):
+    #     if arr[j]> i:
+    #         continue
+    #     if s + arr[j] > i:
+    #         s = 0
+    #         div += 1
+    #     s += arr[i]
+    # if div <= m:
+    #     print(i)
+    #     break
+
+# print(MinMax)
