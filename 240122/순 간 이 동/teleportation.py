e142f80252773e537f3bfddbfe3d17396e9816fd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-22 18:32:44 +0900

    Title: 순 간 이 동, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/순 간 이 동/teleportation.py
+++ 240122/순 간 이 동/teleportation.py
@@ -3,7 +3,10 @@ a, b, x, y = map(int, input().split())
 ans = 101
 
 ans = min(ans, abs(b - a))
+# a -> b
 ans = min(ans, abs(a - x) + abs(b - y))
+# a ->x -> y -> b
 ans = min(ans, abs(a - y) + abs(b - x))
-
+# a -> y -> x -> b
+# 이 중에서 min을 찾아서 넣어준다.
 print(ans)
