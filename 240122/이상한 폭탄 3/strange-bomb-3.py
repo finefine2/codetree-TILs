72ba014b35d0cc6c3c4c104674b2a38bed7ce472
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-22 13:42:36 +0900

    Title: 이상한 폭탄 3, Time: 134ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240122/이상한 폭탄 3/strange-bomb-3.py
+++ 240122/이상한 폭탄 3/strange-bomb-3.py
@@ -6,20 +6,31 @@ for i in range(n):
     arr.append(a)
 
 bomb = [0] * 1000001
+bomb_cnt = [0] * 1000001
 
-for i in range(n-k+1):
-    for j in range(i, i+k):
-        if arr[j] == 0:
-            bomb[arr[j]] = 1
-        else:
-            bomb[arr[j]] += 1
+for i in range(n):
+    for j in range(i+1, min(i+k, n-1)):
+        if arr[i] == arr[j]:
+            bomb[i] = 1
+            bomb[j] = 1
+
+for i in range(len(bomb)):
+    if bomb[i] != 0:
+        bomb_cnt[arr[i]] += 1
+        
 
 Max = 0
 Maxidx = 0
-
 for i in range(len(bomb)):
-    if bomb[i] > Max:
-        Max = bomb[i]
-        Maxidx = i
+    if bomb[i]:
+        if Maxidx < bomb_cnt[arr[i]]:
+            Maxidx = bomb_cnt[arr[i]]
+            Max = bomb_cnt[arr[i]]
 
-print(Maxidx)
+print(Max)   
+
+
+# if not flag:
+#     print(0)
+# else:
+#     print(Maxidx)
