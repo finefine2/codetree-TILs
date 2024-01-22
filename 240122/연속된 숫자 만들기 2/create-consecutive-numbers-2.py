ae8a5e8716d16949e7e897ae0664e7b4cf2bb571
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-22 18:48:16 +0900

    Title: 연속된 숫자 만들기 2, Time: 160ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240122/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -5,8 +5,9 @@ ans = 0
 
 if arr[0] + 1 == arr[1] and arr[1] + 1 == arr[2]:
     ans = 0
-elif arr[0] + 2 == arr[1] and arr[1] + 2 == arr[2]:
+elif arr[0] + 2 == arr[1] or arr[1] + 2 == arr[2]:
     ans = 1
+# 하나라도 2차이 나면 그냥 그 안에 들어가면 됨
 else:
     ans = 2
 # 어떠한 경우든 2번 안에 끝난다..
