18ec741ecb5ff2e319e6f57441c2b9ae2639692d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-23 14:49:17 +0900

    Title: 독서실의 거리두기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240123/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -3,15 +3,34 @@ n = int(input())
 st = input()
 
 arr = []
+idx = 0
 for i in range(len(st)):
     if st[i] == '1':
         arr.append(i)
+        idx = i
+
 
 dist = []
 for i in range(len(arr)-1):
     dist.append(arr[i+1] - arr[i])
 
 dist.sort()
+
+if dist[-1] // 2 < len(st) - 1 - idx:
+    dist.append(len(st) - 1 - idx)
+
+dist.sort()
+
 dist[-1] //= 2
+
+# if dist[-1] % 2 == 0:
+#     dist[-1] = dist[-1] // 2 + 1
+# else:
+#     dist[-1] //= 2
+
+# for k in dist:
+    # print(k, end = " ")
+
+
 
 print(min(dist))
