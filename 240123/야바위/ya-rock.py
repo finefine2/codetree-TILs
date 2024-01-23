08b51ad47b19c54d9cac3fa4faac7321b6c350c6
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-23 13:35:19 +0900

    Title: 야바위, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/야바위/ya-rock.py
+++ 240123/야바위/ya-rock.py
@@ -18,19 +18,14 @@ for _ in range(N):
     a,b,c = map(int,input().split()) 
     stones.append([a-1,b-1,c-1])
 # 조약돌을 1,2,3 위치 넣어보기 
-lines = [0] * 3 
 ans = 0 
 for i in range(3): 
+    lines = [0] * 3
     lines[i] = 1 
-    cnt = 0 
-    # print(f"{i} turn")
-    
+    cnt = 0    
     for a,b,c in stones: 
-        # print(lines)
         lines[a] ,lines[b] = lines[b], lines[a] 
         if lines[c] == 1: 
             cnt += 1
-        # print(f"count is {cnt}")
-        # print(lines)
     ans = max(cnt,ans) 
-print(ans-1)
+print(ans)
