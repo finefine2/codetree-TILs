fec50e5e831eea2a22b20469b25fe63ba560fd24
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-23 18:25:36 +0900

    Title: 홀수 짝수의 묶음, Time: 248ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240123/홀수 짝수의 묶음/odd-even-bundle.py
@@ -16,24 +16,23 @@ while True:
     if even <= 0 and odd <= 0:
         break
 
-    if iseven:
+    if iseven:   # 짝수를 만들때
+        # 짝수가 있을 경우 짝수만 사용
         if even > 0:
             even -= 1
-        else:
+        else:     # 짝수를 만들어야 하는데 짝수도 없고, 홀수도 1개 이하 있을 경우 
+                  # 불가능 하므로 이 홀수들을 앞의 홀수 만들때나 짝수만들때 넣어주고 ans를 하나 줄인다.
             if odd < 2:
                 ans -= 1
                 break
             
             odd -= 2
-    else:
-        if odd > 0:
-            odd -= 1
-        else:
-            if even < 2:
-                ans -= 1
-                break
+    else: # 홀수를 만들때 odd가 없으면 짝수로는 홀수를 만들 수 없음
+        if odd == 0:
+            break
             
-            even -= 2
+        # 그냥 홀수를 만들때는 odd 한개로도 가능
+        odd -= 1
     
     ans += 1
     iseven = not iseven
