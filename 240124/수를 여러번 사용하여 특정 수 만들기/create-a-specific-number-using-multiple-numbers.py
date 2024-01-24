4ececc994ef6cea4d7f2e61f4b017648bed63ce1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-24 16:28:43 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240124/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240124/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -1,9 +1,8 @@
 A,B,C = map(int,input().split()) 
-cntA = C // A 
-cntB = C // B 
+
 ans = 0 
-for i in range(1,cntA + 1):
-    for j in range(1,cntB + 1):
+for i in range(1000):
+    for j in range(1000):
         tmp = A * i + B * j 
 
         if tmp <= C: 
