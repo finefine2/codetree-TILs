4c325261ea8c0e0b2546614b0dd7ce9b8393e149
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-25 15:19:43 +0900

    Title: 팀으로 하는 틱택토 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240125/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,21 @@
+nums = [] 
+for i in range(3): 
+    num = input()
+    nums.append([num[0],num[1],num[2]])
+# 같은 줄이어야 하고, 3 개 중 2개는 같은 숫자여야 함 
+
+tmp = [[nums[0][0],nums[0][1],nums[0][2]], [nums[1][0],nums[1][1],nums[1][2]], [nums[2][0],nums[2][1],nums[2][2]],
+        [nums[0][0],nums[1][0],nums[2][0]], [nums[0][1],nums[1][1],nums[2][1]], [nums[0][2],nums[1][2],nums[2][2]], 
+        [nums[0][0],nums[1][1],nums[2][2]], [nums[0][2],nums[1][1],nums[2][0]]]
+ans = 0 
+def check(arr): 
+    num1, num2, num3 = arr[0], arr[1], arr[2] 
+    if num1 != num2 and num2 != num3 and num3 != num1: 
+        return False 
+    else: 
+        return True 
+
+for t in tmp: 
+    if check(t): 
+        ans += 1 
+print(ans)
