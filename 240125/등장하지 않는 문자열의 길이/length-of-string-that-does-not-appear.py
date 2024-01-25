7a5f181fa188ec0d3f51fef1db34868709a29e29
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-25 16:08:50 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 178ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240125/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,14 +1,22 @@
 # 부분문자열들을 구해야겠다 우선은 
-n = int(input())
-st = input()
-ans = 0
-for i in range(n):
-    for j in range(1,n):
-        cnt = 0
-        for k in range(n):
-            if st[k:k+j] == st[i:i+j]:
-                cnt += 1
-        if cnt >= 2:
-            if ans < j:
-                ans = j
-print(ans+1)
+N = int(input()) 
+s = input() 
+ans = 1 
+# 1씩 늘려가기 
+for i in range(1,N): 
+    # 모든 길이가 i인 부분 문자열에 대해 쌍을 짓고 같은지 확인 
+    twice = False 
+    for j in range(N-i+1): 
+        for k in range(j+1, N-i+1): 
+            # issame: 부분 문자열이 같은지 확인 
+            issame = True 
+            for l in range(i): 
+                if s[j+l] != s[k+l]: 
+                    issame = False 
+            if issame: 
+                twice = True 
+    if twice: 
+        ans = i + 1 
+    else: 
+        break 
+print(ans)
