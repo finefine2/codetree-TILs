be2a20dac296c4ef4f97d263f96f65bdf611f0c2
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-29 17:04:17 +0900

    Title: 트로미노, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240129/트로미노/tromino.py
@@ -0,0 +1,65 @@
+n, m = map(int, input().split())
+
+arr = []
+for i in range(n):
+    st = list(map(int, input().split()))
+    arr.append(st)
+
+Max = -1
+for i in range(n):
+    for j in range(m-3):
+        s = 0
+        for k in range(3):
+            s += arr[i][j+k]
+        if s > Max:
+            Max = s
+
+for i in range(n):
+    for j in range(m-3):
+        s = 0
+        for k in range(3):
+            s += arr[j+k][i]
+        if s > Max:
+            Max = s
+
+# --
+# |
+for i in range(n-1):
+    for j in range(m-1):
+        s = 0
+        s += arr[i][j] + arr[i+1][j] + arr[i][j+1]
+        
+        if s > Max:
+            Max = s
+# --
+#  |
+for i in range(n-1):
+    for j in range(m-1):
+        s = 0
+        s += arr[i][j] + arr[i+1][j] + arr[i+1][j+1]
+        
+        if s > Max:
+            Max = s
+
+# |
+# --
+for i in range(n-1):
+    for j in range(m-1):
+        s = 0
+        s += arr[i][j] + arr[i+1][j+1] + arr[i][j+1]
+        
+        if s > Max:
+            Max = s
+#  |
+# --
+for i in range(n-1):
+    for j in range(m-1):
+        s = 0
+        s += arr[i+1][j+1] + arr[i+1][j] + arr[i][j+1]
+        
+        if s > Max:
+            Max = s
+
+
+
+print(Max)
