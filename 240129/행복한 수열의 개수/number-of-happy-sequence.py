fca0b356124a61a649358362ab4ca558d8bdb470
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-29 16:36:02 +0900

    Title: 행복한 수열의 개수, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240129/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240129/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,20 +8,26 @@ for i in range(n):
 ans = 0
 for i in range(n):
     cnt = 0
-    for j in range(n):
+    for j in range(n - m + 1):
+        flag = False
         for k in range(m-1):
             if arr[i][j+k] == arr[i][j+k+1]:
-                cnt += 1
-    if cnt == m-1:
-        ans += 1
+                cnt += 1     
+            else:
+                flag = True
+        if cnt == m-1 and not flag:
+            ans += 1
 
 for i in range(n):
     cnt = 0
-    for j in range(n):
-        for k in range(m-1):
+    for j in range(n - m + 1):
+        flag = False
+        for k in range(m - 1):
             if arr[j+k][i] == arr[j+k+1][i]:
                 cnt += 1
-    if cnt == m-1:
-        ans += 1
+            else:
+                flag = True
+        if cnt == m-1 and not flag:
+            ans += 1
 
 print(ans)
