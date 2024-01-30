ec3ae3602a63d8b70972d584b910834b2171acb8
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-30 17:38:10 +0900

    Title: 구역 청소, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/구역 청소/district-cleaning.py
+++ 240130/구역 청소/district-cleaning.py
@@ -2,13 +2,13 @@ a,b = map(int,input().split())
 c,d = map(int,input().split()) 
 sect = [0] * 101 
 
-for i in range(101): 
-    if a <= i <= b: 
-        sect[i] += 1 
-    if c <= i <= d: 
-        sect[i] += 1 
+for i in range(a,b): 
+    sect[i] += 1 
+for i in range(c,d): 
+    sect[i] += 1 
+
 cnt = 0 
 for s in sect: 
     if s != 0: 
         cnt += 1 
-print(cnt-1)
+print(cnt)
