320878430edc04e11b77c715eb092bf1f075459e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-30 14:32:53 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 671ms, Memory: 33MB, Status: Passed - CodeTree

--- 240130/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240130/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -21,6 +21,7 @@ def check_func():
             if check[i][j] >= 2:
                 return False
     return True
+    # 2개가 넘을 경우 -> 즉 겹칠 경우에는 답이 될 수 없으므로 False
 
 def square(x1, y1, x2, y2):
     for i in range(n):
@@ -30,18 +31,23 @@ def square(x1, y1, x2, y2):
                     for ii in range(n):
                         for jj in range(m):
                             check[ii][jj] = 0
+                    # 일단 0으로 초기화
+
                     s1 = 0
                     for ii in range(x1, x2+1):
                         for jj in range(y1, y2+1):
                             check[ii][jj] += 1
                             s1 += arr[ii][jj]
+                    # 첫번째 사각형에 더하고 check 늘리기
                     s2 = 0
                     for ii in range(i, k+1):
                         for jj in range(j, l+1):
                             check[ii][jj] += 1
                             s2 += arr[ii][jj]
+                    # 두번째 사각형에 더하고 check 늘리기
                     if check_func():
                         ans.append(s1+s2)
+                        # check를 확인해서 겹치지 않는다면 ans에 추가한다.
 
 for i in range(n):
     for j in range(m):
