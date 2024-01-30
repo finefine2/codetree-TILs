f814d0c330cc5f5abaceb5aa30dbc5b0066a4098
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-30 17:44:33 +0900

    Title: 전부 겹치는 선분, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240130/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,16 @@
+N = int(input()) 
+lines = [] 
+for _ in range(N): 
+    x1,x2 = map(int,input().split()) 
+    lines.append([x1,x2]) 
+
+check = [0] * 101 
+for x1,x2 in lines: 
+    for i in range(x1,x2): 
+        check[i] += 1 
+
+for c in check: 
+    if c == N: 
+        print("Yes") 
+    else: 
+        print("No")
