affcb06210c7c9c2fe546a52e00b894df7c861e4
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-31 16:57:28 +0900

    Title: 기울어진 직사각형의 회전, Time: 217ms, Memory: 25MB, Status: Passed - CodeTree

--- 240131/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240131/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -7,33 +7,50 @@ for i in range(n):
     arr.append(st)
 
 r, c, m1, m2, m3, m4, dir = map(int, input().split())
-r -= 1
-c -= 1
-tmp = copy.deepcopy(arr)
 
-if dir == 0:
-    dx = [-1,-1,1,1]
-    dy = [1,-1,-1,1]
-    M = [m1, m2, m1, m2]
-    # 사실상 m1과 m3가 같고, m2랑 m4랑 같으니 이런식으로 하면 된다.
+def cal(r, c, m11, m22, d):
+    # tmp = copy.deepcopy(arr)
 
-else:
-    dx = [1,1,-1,-1]
-    dy = [1,-1,-1,1]
-    M = [m2, m1, m2, m1]
+    # if d == 0:
+    #     dx = [-1,-1,1,1]
+    #     dy = [1,-1,-1,1]
+    #     M = [m11, m22, m11, m22]
+    #     # 사실상 m1과 m3가 같고, m2랑 m4랑 같으니 이런식으로 하면 된다.
 
-for i in range(4):
-    for j in range(M[i]):
-        nx = r + dx[i]
-        ny = c + dy[i]
+    # else:
+    #     dx = [-1,-1,1,1]
+    #     dy = [1,-1,-1,1]
+    #     # dx = [1,1,-1,-1]
+    #     # dy = [1,-1,-1,1]
+    #     M = [m22, m11, m22, m11]
 
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            continue
 
-        tmp[nx][ny] = arr[r][c]
-        r, c = nx, ny
+    # for i in range(4):
+    #     for j in range(M[i]):
+    #         nx = r + dx[i]
+    #         ny = c + dy[i]
 
-ans = copy.deepcopy(tmp)
+    #         tmp[nx][ny] = arr[r][c]
+    #         r, c = nx, ny
+        
+    # return tmp
+    tmp = copy.deepcopy(arr)
+
+    if d == 0:
+        dx, dy=[-1, -1, 1, 1], [1, -1, -1, 1]
+        M = [m11, m22, m11, m22]
+    else:
+        dx, dy=[-1, -1, 1, 1], [-1, 1, 1, -1]
+        M = [m22, m11, m22, m11]
+
+    for i in range(4):
+        for j in range(M[i]):
+            nr, nc = r+dx[i], c+dy[i]
+            tmp[nr][nc] = arr[r][c]
+            r, c = nr, nc
+    return tmp
+
+ans = copy.deepcopy(cal(r-1, c-1, m1, m2, dir))
 
 for i in range(n):
     print(*ans[i])
