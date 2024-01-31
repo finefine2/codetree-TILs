f04bb663be272de08797d4451b2aa9862a89b453
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-31 16:47:02 +0900

    Title: 비둘기와 전기줄, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,15 @@
+N = int(input()) 
+check = [5] * 101 
+pige = []  
+for i in range(N): 
+    num, pos = map(int,input().split()) 
+    pige.append((num,pos)) 
+ans = 0
+for p in pige: 
+    if check[p[0]] == 5: 
+        check[p[0]] = p[1] 
+    else: 
+        if check[p[0]] != p[1]: 
+            ans += 1 
+            check[p[0]] = [1]
+print(ans)
