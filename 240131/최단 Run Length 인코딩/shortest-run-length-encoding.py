b4c8da10231e073d5156c43b87e8fee4add76d25
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-31 17:07:18 +0900

    Title: 최단 Run Length 인코딩, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,43 @@
+a = input()
+
+
+def run(a):
+    cnt = 0
+    tmp = ""
+    ans = ""
+    for i in range(len(a)):
+        if i == 0:
+            cnt = 1
+            tmp = a[i]
+        elif i == len(a) - 1:
+            if tmp == a[i]:
+                cnt += 1
+                ans += tmp
+                ans += str(cnt)
+            else:
+                ans += tmp
+                ans += str(cnt)
+                cnt = 1
+                ans += a[i]
+                ans += str(cnt)
+        else:
+            if tmp == a[i]:
+                cnt += 1
+            else:
+                ans += tmp
+                ans += str(cnt)
+                cnt = 1
+                tmp = a[i]
+
+    return ans
+
+Min = 100
+st = ""
+l = len(a)
+for i in range(l):
+    a = a[-1] + a[:-1]
+    b = run(a)
+    if len(b) < Min:
+        Min = len(b)
+
+print(Min)
