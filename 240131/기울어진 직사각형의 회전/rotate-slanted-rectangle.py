cec8e824a20285a2cc32b514570555489ac97253
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-01-31 16:12:42 +0900

    Title: 기울어진 직사각형의 회전, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240131/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -0,0 +1,35 @@
+import copy
+n = int(input())
+
+arr = []
+for i in range(n):
+    st = list(map(int, input().split()))
+    arr.append(st)
+
+r, c, m1, m2, m3, m4, dir = map(int, input().split())
+r -= 1
+c -= 1
+ans = copy.deepcopy(arr)
+
+if dir == 0:
+    dx = [-1,-1,1,1]
+    dy = [1,-1,-1,1]
+    M = [m1, m2, m1, m2]
+    # 사실상 m1과 m3가 같고, m2랑 m4랑 같으니 이런식으로 하면 된다.
+
+else:
+    dx = [1,1,-1,-1]
+    dy = [1,-1,-1,1]
+    M = [m2, m1, m2, m1]
+
+for i in range(4):
+    for j in range(M[i]):
+        nx = r + dx[i]
+        ny = c + dy[i]
+
+        ans[nx][ny] = arr[r][c]
+        r, c = nx, ny
+
+
+for i in range(n):
+    print(*ans[i])
