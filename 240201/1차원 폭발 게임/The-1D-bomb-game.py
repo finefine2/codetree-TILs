52c7fc43287ec6020b82a92a491584efbc97c4c8
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-01 15:39:39 +0900

    Title: 1차원 폭발 게임, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240201/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,60 @@
+# n, m = map(int, input().split())
+
+# arr = []
+# for i in range(n):
+#     a = int(input())
+#     arr.append(a)
+
+
+# while True:
+
+#     arr2 = []
+#     samecheck = False
+#     lastcheck = False
+#     for i in range(len(arr)-1):
+#         if arr[i] == arr[i+1]:
+#             if i + 1 == len(arr) - 1:
+#                 lastcheck = True
+#                 samecheck = True
+#         else:
+#             arr2.append(arr[i])
+#     if not lastcheck:
+#         arr2.append(arr[len(arr) - 1])
+    
+#     if not samecheck:
+#         break
+
+#     arr = arr2
+
+# print(len(arr))
+# for k in arr:
+#     print(k) 
+
+
+n, m = map(int, input().split())
+
+arr = []
+for i in range(n):
+    a = int(input())
+    arr.append(a)
+
+while True:
+    arr2 = []
+    i = 0
+    while i < len(arr):
+        if i < len(arr) - 1 and arr[i] == arr[i + 1]:
+            # 연속된 같은 숫자를 건너뛰기
+            while i < len(arr) - 1 and arr[i] == arr[i + 1]:
+                i += 1
+        else:  # 아닐때는 arr2에 넣어준다.
+            arr2.append(arr[i])
+        i += 1
+
+    if len(arr2) == len(arr):
+        # 더 이상 제거할 연속된 숫자가 없으면 반복 종료
+        break
+    arr = arr2
+
+print(len(arr))
+for k in arr:
+    print(k)
