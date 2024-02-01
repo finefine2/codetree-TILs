b8f31ac45034180c27bd04e0cd65cb35cfc1b0c1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-01 15:48:17 +0900

    Title: 1차원 폭발 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240201/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240201/1차원 폭발 게임/The-1D-bomb-game.py
@@ -33,32 +33,32 @@
 n, m = map(int, input().split())
 
 arr = []
-for i in range(n):
-    a = int(input())
-    arr.append(a)
+for _ in range(n):
+    arr.append(int(input()))
 
 while True:
-    arr2 = []
     i = 0
-    while i < len(arr) - 1:
-        if arr[i] == arr[i + 1]:
-            # 연속된 같은 숫자를 건너뛰기
-            while i < len(arr) - 1 and arr[i] == arr[i + 1]:
-                i += 1
-        else:
-            arr2.append(arr[i])
-        i += 1
+    same = []  # 제거할 폭탄의 인덱스를 저장하는 리스트
+    while i < len(arr):
+        cnt = 1
+        while i + count < len(arr) and arr[i] == arr[i + cnt]:
+            cnt += 1
+        
+        if cnt >= m:
+            # M개 이상 연속되면 해당 인덱스 범위 추가
+            for j in range(i, i + cnt):
+                same.append(j)
 
-    # 마지막 원소 처리
-    if len(arr) >= 1:
-        if not (i < len(arr) and arr[i] == arr[i - 1]):
-            arr2.append(arr[-1])
+        i += cnt
 
-    if len(arr2) == len(arr):
-        # 더 이상 제거할 연속된 숫자가 없으면 반복 종료
+    if not same:
+        # 더 이상 제거할 폭탄이 없으면 종료
         break
-    arr = arr2
 
+    # 제거할 폭탄을 제외하고 리스트 다시 구성
+    arr = [arr[i] for i in range(len(arr)) if i not in same]
+
+# 결과 출력
 print(len(arr))
-for k in arr:
-    print(k)
+for bomb in arr:
+    print(bomb)
