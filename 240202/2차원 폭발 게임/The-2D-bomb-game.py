22939a851ee155028912fd18cfd4ccfed2cc08e8
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-02 01:14:31 +0900

    Title: 2차원 폭발 게임, Time: 237ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240202/2차원 폭발 게임/The-2D-bomb-game.py
+++ 240202/2차원 폭발 게임/The-2D-bomb-game.py
@@ -31,27 +31,54 @@ def rotate():
         for j in range(n):
             arr[i][j] = new_arr[i][j]
 
-def explode():
-    check = False
-    for j in range(n):
-        cnt = 1
-        for i in range(1, n):
-            if arr[i][j] == arr[i-1][j] and arr[i][j] != 0:
-                cnt += 1
-            else:
-                if cnt >= m:
-                    for k in range(i - cnt, i):
-                        arr[k][j] = 0
-                    check = True
-                cnt = 1
+# def explode():
+#     check = False
+#     for j in range(n):
+#         cnt = 1
+#         for i in range(1, n):
+#             if arr[i][j] == arr[i-1][j] and arr[i][j] != 0:
+#                 cnt += 1
+#             else:
+#                 if cnt >= m:
+#                     for k in range(i - cnt, i):
+#                         arr[k][j] = 0
+#                     check = True
+#                 cnt = 1
         
-        # 마지막 용
-        if cnt >= m:
-            for k in range(n - cnt, n):
-                arr[k][j] = 0
-            check = True
+#         # 마지막 용
+#         if cnt >= m:
+#             for k in range(n - cnt, n):
+#                 arr[k][j] = 0
+#             check = True
 
-    return check
+#     return check
+
+def explode():
+    exploded = False
+    mark = [[False] * n for _ in range(n)]  # 폭발할 위치를 표시하기 위한 배열
+
+    # 각 열별로 연속된 숫자들이 m개 이상 있는지 확인
+    for j in range(n):
+        i = 0
+        while i < n:
+            if arr[i][j] == 0:
+                i += 1
+                continue
+            start = i
+            while i < n and arr[start][j] == arr[i][j]:
+                i += 1
+            if i - start >= m:
+                for k in range(start, i):
+                    mark[k][j] = True
+                exploded = True
+    
+    # 폭발할 위치의 숫자를 0으로 변경
+    for i in range(n):
+        for j in range(n):
+            if mark[i][j]:
+                arr[i][j] = 0
+
+    return exploded
 
 
 for _ in range(k):
@@ -60,11 +87,10 @@ for _ in range(k):
     rotate()
 
 
-# ans = 0
-# for i in range(n):
-#     for j in range(n):
-#         if arr[i][j] != 0:
-#             ans += 1
-ans = sum(arr[i][j] != 0 for i in range(n) for j in range(n))
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] != 0:
+            ans += 1
 
 print(ans)
