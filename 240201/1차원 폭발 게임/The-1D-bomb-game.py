d6b009cb9ce492a9e8a8547938a307a8380e0e5b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-01 15:43:20 +0900

    Title: 1차원 폭발 게임, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240201/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240201/1차원 폭발 게임/The-1D-bomb-game.py
@@ -30,7 +30,6 @@
 # for k in arr:
 #     print(k) 
 
-
 n, m = map(int, input().split())
 
 arr = []
@@ -41,14 +40,19 @@ for i in range(n):
 while True:
     arr2 = []
     i = 0
-    while i < len(arr):
-        if i < len(arr) - 1 and arr[i] == arr[i + 1]:
+    while i < len(arr) - 1:
+        if arr[i] == arr[i + 1]:
             # 연속된 같은 숫자를 건너뛰기
             while i < len(arr) - 1 and arr[i] == arr[i + 1]:
                 i += 1
-        else:  # 아닐때는 arr2에 넣어준다.
+        else:
             arr2.append(arr[i])
         i += 1
+
+    # 마지막 원소 처리
+    if len(arr) >= 1:
+        if not (i < len(arr) and arr[i] == arr[i - 1]):
+            arr2.append(arr[-1])
 
     if len(arr2) == len(arr):
         # 더 이상 제거할 연속된 숫자가 없으면 반복 종료
