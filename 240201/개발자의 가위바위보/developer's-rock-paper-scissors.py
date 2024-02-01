9b24dbb7857453a4230a775a0cea1345d6a1eee5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-01 19:09:40 +0900

    Title: 개발자의 가위바위보, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240201/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+arr = []
+for i in range(n):
+    a, b = map(int, input().split())
+    arr.append((a, b))
+
+def find(num):
+    if num == 1:
+        return (1, 2, 3)
+    elif num == 2:
+        return (1, 3, 2)
+    elif num == 3:
+        return (2, 1, 3)
+    elif num == 4:
+        return (2, 3, 1)
+    elif num == 5:
+        return (3, 1, 2)
+    else:
+        return (3, 2, 1)
+
+ans = 0
+for k in range(6):
+    a, b, c = find(k)
+    cnt = 0
+    for x, y in arr:
+        if (x, y) in [(b, a), (c, b), (a, c)]:
+            cnt += 1
+        # (a, b), (b, c), (c, a)의 역순
+    
+    ans = max(cnt, ans)
+
+print(ans)
