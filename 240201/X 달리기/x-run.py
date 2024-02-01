cd69d94c4036b297866696e3ea92f39daf5059e2
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-01 14:09:41 +0900

    Title: X 달리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240201/X 달리기/x-run.py
@@ -0,0 +1,22 @@
+'''
+속도가 감소하기 시작하는 위치를 잘 설정하여 해당 위치를 전후로 속도가 바뀌게끔하는 전략을 통해 해결
+'''
+import sys
+X = int(input()) 
+
+v = 1 
+time, dist = 0,0     
+Min = sys.maxsize
+while True: 
+    dist += v 
+    time += 1 
+
+    if dist == X: 
+        break 
+    if dist <= x - (v+1) * (v+2) / 2: 
+        v +=  1
+    elif dist <= x - (v+1) * v / 2: 
+        continue
+    else: 
+        v -= 1 
+print(time)
