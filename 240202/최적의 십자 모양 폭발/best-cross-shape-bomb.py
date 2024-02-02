4183e88230b8b4c51ceb7ae4b2bd0d399437127a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-02 14:56:40 +0900

    Title: 최적의 십자 모양 폭발, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240202/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -0,0 +1,118 @@
+import copy
+n = int(input())
+
+arr = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+def gravity():
+    for j in range(n):
+        col = []
+        for i in range(n):
+            if arr[i][j] != 0:
+                col.append(arr[i][j])
+                arr[i][j] = 0
+        for i in range(n - len(col), n):
+            arr[i][j] = col[i - (n - len(col))]
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def explode(arr2, x, y):
+    num = arr2[x][y]
+    arr2[x][y] = 0
+    for dir in range(4):
+        for i in range(1, num):
+            nx = x + dx[dir] * i
+            ny = y + dy[dir] * i
+        
+            if 0 <= nx < n and 0 <= ny < n:
+                arr2[nx][ny] = 0
+
+Max = 0
+for i in range(n):
+    for j in range(n):
+        arr2 = copy.deepcopy(arr)
+        explode(arr2, i, j)
+        gravity()
+
+        ans = 0
+        for k in range(n):
+            for l in range(n-1):  # 오른쪽 인접 쌍 확인
+                if arr2[k][l] == arr2[k][l+1] and arr2[k][l] != 0:
+                    ans += 1
+            for l in range(n):  # 아래쪽 인접 쌍 확인
+                if k < n-1 and arr2[k][l] == arr2[k+1][l] and arr2[k][l] != 0:
+                    ans += 1
+                    
+        Max = max(ans, Max)
+
+print(Max)
+
+# Max = 0
+# for i in range(n):
+
+#     for j in range(n):
+#         arr2 = copy.deepcopy(arr)
+#         explode(arr2, i, j)
+#         gravity()
+
+#         ans = 0
+#         for k in range(n):
+#             for l in range(n):
+#                 if k == 0 and l == 0:
+#                     if arr2[k][l] == arr2[k+1][l]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l+1]:
+#                         ans += 1
+#                 elif k == n-1 and l == 0:
+#                     if arr2[k][l] == arr2[k][l+1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k-1][l]:
+#                         ans += 1
+#                 elif k == 0 and l == n-1:
+#                     if arr2[k][l] == arr2[k][l-1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k+1][l]:
+#                         ans += 1
+#                 elif l == 0 and k+1 < n:
+#                     if arr2[k][l] == arr2[k-1][l]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l+1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k+1][l]:
+#                         ans += 1
+#                 elif k == 0 and l+1 < n:
+#                     if arr2[k][l] == arr2[k+1][l]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l-1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l+1]:
+#                         ans += 1
+#                 elif l == n-1 and k+1 < n:
+#                     if arr2[k][l] == arr2[k+1][l]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l-1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k-1][l]:
+#                         ans += 1
+#                 elif k == n-1 and l+1 < n:
+#                     if arr2[k][l] == arr2[k][l+1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l-1]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k-1][l]:
+#                         ans += 1
+#                 elif k == n-1 and l == n-1:
+#                     if arr2[k][l] == arr2[k-1][l]:
+#                         ans += 1
+#                     elif arr2[k][l] == arr2[k][l-1]:
+#                         ans += 1
+
+#                 if arr[k][l] == 0:
+#                     ans = 0
+
+#         Max = max(ans, Max)
+
+# print(Max)
