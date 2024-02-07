0f3024699875ecc87ff6cdfa832ff2cc0de9acfb
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-07 15:37:43 +0900

    Title: 두 점으로 만드는 정사각형, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/두 점으로 만드는 정사각형/square-made-of-two-points.py
+++ 240207/두 점으로 만드는 정사각형/square-made-of-two-points.py
@@ -1,9 +1,13 @@
-x1,y1,x2,y2 = map(int,input().split()) 
-a1,b1,a2,b2 = map(int,input().split()) 
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
 
-min_x = min(x1,a1)
-max_x = max(x2,a2)
-min_y = min(y1,b1)
-max_y = max(y2,b2)
 
-print(pow((max_x-min_x) * (max_y-min_y),2))
+minx = min(x1, a1)
+miny = min(y1, b1)
+maxx = max(x2, a2)
+maxy = max(y2, b2)
+
+k = max(maxx - minx, maxy - miny)
+
+
+print(k*k)
