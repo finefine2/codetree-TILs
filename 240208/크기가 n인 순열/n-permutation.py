fc01499263b84e0fb2b870d82df995ad956111c2
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-08 18:41:53 +0900

    Title: 크기가 n인 순열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/크기가 n인 순열/n-permutation.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = []
+check = [0] * (n+1)
+
+def choose(num):
+    if num == n:
+        for k in arr:
+            print(k, end = " ")
+        print()
+        return
+
+    for i in range(1, n+1):
+        if check[i]:
+            continue
+        
+        arr.append(i)
+        check[i] = True
+        choose(num + 1)
+        arr.pop()
+        check[i] = False
+
+choose(0,1)
