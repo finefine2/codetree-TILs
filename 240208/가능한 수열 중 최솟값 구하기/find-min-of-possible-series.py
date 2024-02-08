379f75af5e3cb0470f4e1a02118304588541e257
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-08 14:50:34 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 188ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 240208/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -10,13 +10,13 @@ def find(arr):
     cnt = 1
     while True:
         a1, b1 = len(arr) - cnt, len(arr) - 1
-        a2, b2 = a1 - cnt, b1 - 1
+        a2, b2 = a1 - cnt, a1 - 1
 
         # a1 < cnt가 되도 break
         if a2 < 0:
             break
         
-        # (cnt, 1) 구간과 (a1 - cnt, b1)의 구간이 같을 경우 False
+        # (cnt, 1) 구간과 (a1 - cnt, a1 - 1)의 구간이 같을 경우 False
         # 이 값들은 cnt가 늘어나면서 바뀐다.
         if arr[a1 : b1 + 1] == arr[a2 : b2 + 1]:
             return False
