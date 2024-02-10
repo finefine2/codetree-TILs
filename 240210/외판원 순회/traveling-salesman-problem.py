9dd72c2d8c48a9d17f525c2aa7d6769f8e1d6061
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-10 21:39:47 +0900

    Title: 외판원 순회, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240210/외판원 순회/traveling-salesman-problem.py
+++ 240210/외판원 순회/traveling-salesman-problem.py
@@ -10,15 +10,15 @@ ans = sys.maxsize
 check = [0] * (n+1)
 def choose(num, start, total):
     global ans
-    if num == n:
+    if num == n+1:
         ans = min(ans, total + arr[start][0])
         return
 
     for i in range(n):
         if check[i] == 0 and arr[start][i] > 0:
-            check[start] = 1
+            check[i] = 1
             choose(num + 1, i, total + arr[start][i])
-            check[start] = 0
+            check[i] = 0
 
 choose(1, 0, 0)
 print(ans)
