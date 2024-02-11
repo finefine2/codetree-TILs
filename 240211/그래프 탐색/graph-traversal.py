c3b81459474fe13e557e7dcf21ae0764a508cd83
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-11 20:05:34 +0900

    Title: 그래프 탐색, Time: 209ms, Memory: 26MB, Status: Passed - CodeTree

--- 240211/그래프 탐색/graph-traversal.py
+++ 240211/그래프 탐색/graph-traversal.py
@@ -1,11 +1,14 @@
 n, m = map(int, input().split())
 
-arr = [[0] * (n+1) for _ in range(n+1)]
+arr = [[] for _ in range(n + 1)]
+# arr = [[0] * (n+1) for _ in range(n+1)]
 for i in range(m):
     x, y = map(int, input().split())
     # arr.append((x, y))
-    arr[x][y] = 1
-    arr[y][x] = 1
+    # arr[x][y] = 1
+    # arr[y][x] = 1
+    arr[x].append(y)
+    arr[y].append(x)
     
 ans = 0
 check = [0] * (n+1)
@@ -17,8 +20,7 @@ def DFS(start):
             ans += 1
             DFS(now)
 
-
-for i in range(1, n):
-    DFS(i)
+check[1] = True
+DFS(1)
 
 print(ans)
