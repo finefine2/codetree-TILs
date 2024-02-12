b884c7db7621e28e4e6aa4766024499450220ae9
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-12 15:52:33 +0900

    Title: 뿌요뿌요, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/뿌요뿌요/puyo-puyo.py
+++ 240212/뿌요뿌요/puyo-puyo.py
@@ -1,6 +1,3 @@
-# import sys
-# sys.setrecursionlimit(2500)
-
 # n = int(input())
 
 # arr = []
@@ -23,7 +20,6 @@
 
 # def DFS(x, y, k):
 #     global num
-    
 #     for i in range(4):
 #         nx = x + dx[i]
 #         ny = y + dy[i]
@@ -43,13 +39,14 @@
 #             if not check[i][j]:
 #                 num = 0
 #                 DFS(i, j, k)
-#                 block.append(num)
-#                 flag = True
+#                 if num >= 2:
+#                     block.append(num)
+#                     flag = True
 #                 if num >= 4:
 #                     ans += 1
     
 
-# if not block:
+# if not flag:
 #     print(ans, 1)
 # else:
 #     print(ans, max(block))
@@ -61,48 +58,34 @@ for i in range(n):
     tmp = list(map(int, input().split()))
     arr.append(tmp)
 
+visited = [[False] * n for _ in range(n)]
 
-def isin(a, b):
-    return 0<=a<n and 0<=b<n
-def ismove(a, b, k):
-    if not isin(a, b):
-        return False
-    if check[a][b] or arr[a][b] != k:
-        return False
-    return True
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
-def DFS(x, y, k):
+def dfs(x, y, value):
     global num
+    num += 1
+    visited[x][y] = True
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and arr[nx][ny] == value:
+            dfs(nx, ny, value)
 
-        if ismove(nx, ny, k):
-            check[nx][ny] = 1
-            num += 1
-            DFS(nx, ny, k)
-
-block = []
+blocks = []
 ans = 0
-flag = False
-for k in range(1, 101):
-    check = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if not check[i][j]:
-                num = 0
-                DFS(i, j, k)
-                if num >= 2:
-                    block.append(num)
-                    flag = True
-                if num >= 4:
-                    ans += 1
-    
 
-if not flag:
-    print(ans, 1)
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            num = 0
+            dfs(i, j, arr[i][j])
+            if num >= 2:
+                blocks.append(num)
+            if num >= 4:
+                ans += 1
+
+if blocks:
+    print(ans, max(blocks))
 else:
-    print(ans, max(block))
+    print(ans, 0)  # 수정된 부분: 블록이 없는 경우에 대한 처리
