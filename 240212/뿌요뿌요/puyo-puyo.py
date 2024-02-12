ec85acd83a67a05e3b857434013f8e7a872bb896
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-12 15:32:48 +0900

    Title: 뿌요뿌요, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240212/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,46 @@
+n = int(input())
+
+arr = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+
+def isin(a, b):
+    return 0<=a<n and 0<=b<n
+def ismove(a, b, k):
+    if not isin(a, b):
+        return False
+    if check[a][b] or arr[a][b] != k:
+        return False
+    return True
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def DFS(x, y, k):
+    global num
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if ismove(nx, ny, k):
+            check[nx][ny] = 1
+            num += 1
+            DFS(nx, ny, k)
+
+block = []
+explode = []
+for k in range(1, 101):
+    check = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if not check[i][j]:
+                num = 0
+                DFS(i, j, k)
+                if num >= 2:
+                    block.append(num)
+                if num >= 4:
+                    explode.append(num)
+    
+print(len(explode), max(block))
