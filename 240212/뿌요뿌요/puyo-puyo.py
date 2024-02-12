f37d3a8f1da35b742a35fed3967c6ee2d7530d44
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-12 15:50:25 +0900

    Title: 뿌요뿌요, Time: 180ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240212/뿌요뿌요/puyo-puyo.py
+++ 240212/뿌요뿌요/puyo-puyo.py
@@ -1,5 +1,58 @@
-import sys
-sys.setrecursionlimit(2500)
+# import sys
+# sys.setrecursionlimit(2500)
+
+# n = int(input())
+
+# arr = []
+# for i in range(n):
+#     tmp = list(map(int, input().split()))
+#     arr.append(tmp)
+
+
+# def isin(a, b):
+#     return 0<=a<n and 0<=b<n
+# def ismove(a, b, k):
+#     if not isin(a, b):
+#         return False
+#     if check[a][b] or arr[a][b] != k:
+#         return False
+#     return True
+
+# dx = [0,0,1,-1]
+# dy = [1,-1,0,0]
+
+# def DFS(x, y, k):
+#     global num
+    
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+
+#         if ismove(nx, ny, k):
+#             check[nx][ny] = 1
+#             num += 1
+#             DFS(nx, ny, k)
+
+# block = []
+# ans = 0
+# flag = False
+# for k in range(1, 101):
+#     check = [[0] * n for _ in range(n)]
+#     for i in range(n):
+#         for j in range(n):
+#             if not check[i][j]:
+#                 num = 0
+#                 DFS(i, j, k)
+#                 block.append(num)
+#                 flag = True
+#                 if num >= 4:
+#                     ans += 1
+    
+
+# if not block:
+#     print(ans, 1)
+# else:
+#     print(ans, max(block))
 
 n = int(input())
 
@@ -23,7 +76,6 @@ dy = [1,-1,0,0]
 
 def DFS(x, y, k):
     global num
-    
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
@@ -43,13 +95,14 @@ for k in range(1, 101):
             if not check[i][j]:
                 num = 0
                 DFS(i, j, k)
-                block.append(num)
-                flag = True
+                if num >= 2:
+                    block.append(num)
+                    flag = True
                 if num >= 4:
                     ans += 1
     
 
-if not block:
+if not flag:
     print(ans, 1)
 else:
     print(ans, max(block))
