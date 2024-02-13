96aeb3a389e2a252b02b0090a8ae14241679c2a0
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-13 14:57:35 +0900

    Title: 최소 경로로 탈출 하기, Time: 182ms, Memory: 26MB, Status: Passed - CodeTree

--- 240213/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240213/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -9,7 +9,7 @@ for i in range(n):
 
 
 def isin(a, b):
-    return 0<=a<n and 0<=b<n
+    return 0<=a<n and 0<=b<m
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
@@ -23,20 +23,19 @@ def BFS():
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if isin(nx, ny):
-                if not check[nx][ny] and arr[nx][ny] == 1:
-                    check[nx][ny] = check[x][y] + 1
-                    q.append((nx, ny))
+            if isin(nx, ny) and not check[nx][ny] and arr[nx][ny] == 1:
+                check[nx][ny] = check[x][y] + 1
+                q.append((nx, ny))
 
 q = deque()
-check = [[0] * n for _ in range(n)]
+check = [[0] * m for _ in range(n)]
 check[0][0] = 0
 q.append((0,0))
 BFS()
 
 
 
-if check[n-1][n-1] == 0 or check[n-1][n-1] == 1:
+if check[n-1][m-1] == 0 or check[n-1][m-1] == 1:
     print(-1)
 else:
-    print(check[n-1][n-1])
+    print(check[n-1][m-1])
