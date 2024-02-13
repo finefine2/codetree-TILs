7694318087d295c6416e54ec9cccd55e691813ef
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-13 10:52:19 +0900

    Title: K번 최댓값으로 이동하기, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240213/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,53 @@
+from collections import deque
+n, k = map(int, input().split())
+
+arr = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+r, c = map(int, input().split())
+
+r -= 1
+c -= 1
+
+def isin(a, b):
+    return 0<=a<n and 0<=b<n
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def BFS(num):
+    global Max, Maxi, Maxj
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if isin(nx, ny) and not check[nx][ny] and arr[nx][ny] < num:
+                check[nx][ny] = 1
+                if arr[nx][ny] >= Max:
+                    Max = arr[nx][ny]
+                    if Maxi >= nx:
+                        Maxi = nx
+                        if Maxj >= ny:
+                            Maxj = ny
+                q.append((nx, ny))
+
+
+for _ in range(k):
+    q = deque()
+    check = [[0] * (n+1) for _ in range(n+1)]
+
+    Max = 0
+    Maxi = 101
+    Maxj = 101
+
+    check[r][c] = 1
+    q.append((r, c))
+    BFS(arr[r][c])
+
+    r, c = Maxi, Maxj
+
+print(r+1, c+1)
