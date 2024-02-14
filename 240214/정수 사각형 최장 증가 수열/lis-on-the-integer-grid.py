435c3ae8a7c681896eecc3f9f67ae618e3141edd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 16:51:10 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 5050ms, Memory: 51MB, Status: Time Limit Exceed - CodeTree

--- 240214/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240214/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,66 +1,3 @@
-# import sys
-# sys.setrecursionlimit(250000)
-# # from collections import deque
-# n = int(input())
-
-# arr = []
-# for i in range(n):
-#     tmp = list(map(int, input().split()))
-#     arr.append(tmp)
-
-
-# dx = [0,0,1,-1]
-# dy = [1,-1,0,0]
-
-# def isin(a, b):
-#     return 0<=a<n and 0<=b<n
-
-# ans = 0
-# # for i in range(n):
-# #     for j in range(n):
-# #         q = deque()
-#         # dp = [[1] * n for _ in range(n)]
-
-# #         q.append((i, j))
-# #         while q:
-# #             x, y = q.popleft()
-# #             for i in range(4):
-# #                 nx = x + dx[i]
-# #                 ny = y + dy[i]
-# #                 if isin(nx, ny) and isin(x, y) and arr[x][y] < arr[nx][ny]:
-# #                     dp[nx][ny] = max(dp[x][y] + 1, dp[nx][ny])
-# #                     q.append((nx, ny))
-        
-# #         for k in range(n):
-# #             for l in range(n):
-# #                 print(dp[k][l], end = " ")
-# #                 ans = max(ans, dp[k][l])
-# #             print()
-
-# dp = [[0] * n for _ in range(n)]
-
-# def DFS(x, y):
-
-#     if dp[x][y] != 0:
-#         return dp[x][y]
-
-#     dp[x][y] = 1
-
-#     for i in range(4):
-#         nx = x + dx[i]
-#         ny = y + dy[i]
-
-#         if isin(nx, ny) and arr[x][y] < arr[nx][ny]:
-#             dp[x][y] = max(dp[x][y], DFS(nx, ny) + 1)
-    
-#     return dp[x][y]
-
-# for i in range(n):
-#     for j in range(n):
-#         ans = max(DFS(i, j), ans)
-
-# print(ans)
-        
 from collections import deque
 
 n = int(input())
@@ -79,18 +16,58 @@ def isin(a, b):
 ans = 0
 for i in range(n):
     for j in range(n):
-        visited = [[False] * n for _ in range(n)]
-        q = deque([(i, j, 1)])  
+        q = deque()
+        dp = [[1] * n for _ in range(n)]
 
+        q.append((i, j))
         while q:
-            x, y, dist = q.popleft()
-            visited[x][y] = True
-            ans = max(ans, dist) 
-
+            if len(q) > 250000:  # 큐의 크기가 최대 요소 수를 초과하면 중단
+                break
+            x, y = q.popleft()
             for k in range(4):
                 nx = x + dx[k]
                 ny = y + dy[k]
-                if isin(nx, ny) and arr[x][y] < arr[nx][ny] and not visited[nx][ny]:
-                    q.append((nx, ny, dist + 1))
+                if isin(nx, ny) and arr[x][y] < arr[nx][ny]:
+                    dp[nx][ny] = max(dp[x][y] + 1, dp[nx][ny])
+                    q.append((nx, ny))
+
+        for k in range(n):
+            for l in range(n):
+                ans = max(ans, dp[k][l])
 
 print(ans)
+
+        
+# from collections import deque
+
+# n = int(input())
+
+# arr = []
+# for i in range(n):
+#     tmp = list(map(int, input().split()))
+#     arr.append(tmp)
+
+# dx = [0, 0, 1, -1]
+# dy = [1, -1, 0, 0]
+
+# def isin(a, b):
+#     return 0 <= a < n and 0 <= b < n
+
+# ans = 0
+# for i in range(n):
+#     for j in range(n):
+#         visited = [[False] * n for _ in range(n)]
+#         q = deque([(i, j, 1)])  
+
+#         while q:
+#             x, y, dist = q.popleft()
+#             visited[x][y] = True
+#             ans = max(ans, dist) 
+
+#             for k in range(4):
+#                 nx = x + dx[k]
+#                 ny = y + dy[k]
+#                 if isin(nx, ny) and arr[x][y] < arr[nx][ny] and not visited[nx][ny]:
+#                     q.append((nx, ny, dist + 1))
+
+# print(ans)
