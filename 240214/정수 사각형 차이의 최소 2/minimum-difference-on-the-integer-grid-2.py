f500f7162b1ac35ca732280f9075bb235c3db25d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 18:19:42 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 216ms, Memory: 26MB, Status: Passed - CodeTree

--- 240214/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240214/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,3 +1,4 @@
+import sys
 n = int(input())
 
 arr = []
@@ -5,40 +6,45 @@ for i in range(n):
     tmp = list(map(int, input().split()))
     arr.append(tmp)
 
+def find_Max():
+    dpMax = [[101] * n for _ in range(n)]
 
-dpMin = [[101] * n for _ in range(n)]
-dpMax = [[0] * n for _ in range(n)]
+    dpMax[0][0] = arr[0][0]
 
-dpMin[0][0] = arr[0][0]
-dpMax[0][0] = arr[0][0]
+    for i in range(1, n):
+        dpMax[i][0] = max(dpMax[i-1][0], arr[i][0])
 
-for i in range(1, n):
-    dpMin[i][0] = min(dpMin[i-1][0], arr[i][0])
-    dpMax[i][0] = max(dpMax[i-1][0], arr[i][0])
-
-# 첫 번째 행 초기화
-for j in range(1, n):
-    dpMin[0][j] = min(dpMin[0][j-1], arr[0][j])
-    dpMax[0][j] = max(dpMax[0][j-1], arr[0][j])
-
-
-# 최대값을 최소로 만들기
-# 최소값을 최대로 만들기
-# for i in range(1, n):
-#     for j in range(1, n):
-#         dpMin[i][j] = min(dpMin[i][j], max(arr[i-1][j], arr[i][j-1]))
-
-# for i in range(1, n):
-#     for j in range(1, n):
-#         dpMax[i][j] = max(dpMin[i][j], min(arr[i-1][j], arr[i][j-1]))
-
-for i in range(1, n):
+    # 첫 번째 행 초기화
     for j in range(1, n):
-        dpMin[i][j] = min(min(dpMin[i-1][j], dpMin[i][j-1]), arr[i][j])
-        dpMax[i][j] = max(min(dpMax[i-1][j], dpMax[i][j-1]), arr[i][j])
+        dpMax[0][j] = max(dpMax[0][j-1], arr[0][j])
 
 
-print(abs(dpMax[n-1][n-1] - dpMin[n-1][n-1]))
+    # 최대값을 최소로 만들기
+    # 최소값을 최대로 만들기
+    for i in range(1, n):
+        for j in range(1, n):
+            dpMax[i][j] = max(min(dpMax[i-1][j], dpMax[i][j-1]), arr[i][j])
+
+    return dpMax[n-1][n-1]
+
+# Maxsize = sys.maxsize
+ans = 101
+for k in range(1, 101):
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] < k:
+                arr[i][j] = 101
+
+    Max = find_Max()
+    if Max < 101:
+        ans = min(ans, abs(Max - k))
+        # k가 여기에서 lower_bound 역할을 한다.
+        # 그때 그때 path에 따라서 low와 high가 달라지므로 두개의 배열로 나누어서 하면 안된다.
+        # -> 다른 패스가 겹칠 수 있어서
+        # print(k)
+
+print(ans)
+
 
 # for i in range(n):
 #     for j in range(n):
