855694856532c7d6afa0dcf9c05fd333ba02144e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 11:16:22 +0900

    Title: 선택 정렬 구현, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/선택 정렬 구현/implement-selection-sort.py
+++ 240214/선택 정렬 구현/implement-selection-sort.py
@@ -1,5 +1,14 @@
-n = int(input()) 
-nums = list(map(int,input().split()))
-nums.sort() 
+N = int(input()) 
+nums = list(map(int,input().split())) 
+
+def selection_sort(): 
+    for i in range(N-1): 
+        min_idx = i 
+        for k in range(i+1,N): 
+            if nums[min_idx] > nums[k]: 
+                min_idx = k 
+        nums[i], nums[min_idx] = nums[min_idx], nums[i] 
+selection_sort() 
+
 for n in nums: 
-    print(n,end=" ")
+    print(n, end=" ")
