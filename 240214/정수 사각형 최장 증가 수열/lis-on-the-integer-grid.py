8d82b6c3f5ab04391131137d4fcd67e45622ab39
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 17:00:16 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 532ms, Memory: 35MB, Status: Passed - CodeTree

--- 240214/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240214/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,76 +1,4 @@
-# from collections import deque
-n = int(input())
-
-arr = []
-for i in range(n):
-    tmp = list(map(int, input().split()))
-    arr.append(tmp)
-
-
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
-def isin(a, b):
-    return 0<=a<n and 0<=b<n
-
-ans = 0
-# for i in range(n):
-#     for j in range(n):
-#         q = deque()
-        # dp = [[1] * n for _ in range(n)]
-
-#         q.append((i, j))
-#         while q:
-#             x, y = q.popleft()
-#             for i in range(4):
-#                 nx = x + dx[i]
-#                 ny = y + dy[i]
-#                 if isin(nx, ny) and isin(x, y) and arr[x][y] < arr[nx][ny]:
-#                     dp[nx][ny] = max(dp[x][y] + 1, dp[nx][ny])
-#                     q.append((nx, ny))
-        
-#         for k in range(n):
-#             for l in range(n):
-#                 print(dp[k][l], end = " ")
-#                 ans = max(ans, dp[k][l])
-#             print()
-
-dp = [[0] * n for _ in range(n)]
-
-def DFS(x, y, dist):
-
-    if dp[x][y]:
-        return dp[x][y]
-
-    Max = 0
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if isin(nx, ny) and arr[x][y] < arr[nx][ny]:
-            Max = max(Max, dist + DFS(nx, ny, dist))
-    
-    dp[x][y] = Max
-    
-    return Max
-
-for i in range(n):
-    for j in range(n):
-        if not dp[i][j]:
-            DFS(i, j, 1)
-
-print(max(map(max, dp))+1)
-
-# for i in range(n):
-#     for j in range(n):
-#         ans = max(ans, dp[i][j])
-
-# print(ans + 1)
-
-        
-# from collections import deque
-
+# # from collections import deque
 # n = int(input())
 
 # arr = []
@@ -78,27 +6,136 @@ print(max(map(max, dp))+1)
 #     tmp = list(map(int, input().split()))
 #     arr.append(tmp)
 
-# dx = [0, 0, 1, -1]
-# dy = [1, -1, 0, 0]
+
+# dx = [0,0,1,-1]
+# dy = [1,-1,0,0]
 
 # def isin(a, b):
-#     return 0 <= a < n and 0 <= b < n
+#     return 0<=a<n and 0<=b<n
 
 # ans = 0
+# # for i in range(n):
+# #     for j in range(n):
+# #         q = deque()
+#         # dp = [[1] * n for _ in range(n)]
+
+# #         q.append((i, j))
+# #         while q:
+# #             x, y = q.popleft()
+# #             for i in range(4):
+# #                 nx = x + dx[i]
+# #                 ny = y + dy[i]
+# #                 if isin(nx, ny) and isin(x, y) and arr[x][y] < arr[nx][ny]:
+# #                     dp[nx][ny] = max(dp[x][y] + 1, dp[nx][ny])
+# #                     q.append((nx, ny))
+        
+# #         for k in range(n):
+# #             for l in range(n):
+# #                 print(dp[k][l], end = " ")
+# #                 ans = max(ans, dp[k][l])
+# #             print()
+
+# dp = [[0] * n for _ in range(n)]
+
+# def DFS(x, y, dist):
+
+#     if dp[x][y]:
+#         return dp[x][y]
+
+#     Max = 0
+
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+
+#         if isin(nx, ny) and arr[x][y] < arr[nx][ny]:
+#             Max = max(Max, dist + DFS(nx, ny, dist))
+    
+#     dp[x][y] = Max
+    
+#     return Max
+
 # for i in range(n):
 #     for j in range(n):
-#         visited = [[False] * n for _ in range(n)]
-#         q = deque([(i, j, 1)])  
+#         if not dp[i][j]:
+#             DFS(i, j, 1)
 
-#         while q:
-#             x, y, dist = q.popleft()
-#             visited[x][y] = True
-#             ans = max(ans, dist) 
+# print(max(map(max, dp))+1)
 
-#             for k in range(4):
-#                 nx = x + dx[k]
-#                 ny = y + dy[k]
-#                 if isin(nx, ny) and arr[x][y] < arr[nx][ny] and not visited[nx][ny]:
-#                     q.append((nx, ny, dist + 1))
+# # for i in range(n):
+# #     for j in range(n):
+# #         ans = max(ans, dp[i][j])
 
-# print(ans)
+# # print(ans + 1)
+
+        
+# # from collections import deque
+
+# # n = int(input())
+
+# # arr = []
+# # for i in range(n):
+# #     tmp = list(map(int, input().split()))
+# #     arr.append(tmp)
+
+# # dx = [0, 0, 1, -1]
+# # dy = [1, -1, 0, 0]
+
+# # def isin(a, b):
+# #     return 0 <= a < n and 0 <= b < n
+
+# # ans = 0
+# # for i in range(n):
+# #     for j in range(n):
+# #         visited = [[False] * n for _ in range(n)]
+# #         q = deque([(i, j, 1)])  
+
+# #         while q:
+# #             x, y, dist = q.popleft()
+# #             visited[x][y] = True
+# #             ans = max(ans, dist) 
+
+# #             for k in range(4):
+# #                 nx = x + dx[k]
+# #                 ny = y + dy[k]
+# #                 if isin(nx, ny) and arr[x][y] < arr[nx][ny] and not visited[nx][ny]:
+# #                     q.append((nx, ny, dist + 1))
+
+# # print(ans)
+
+N = int(input())
+matrix = [
+    list(map(int, input().split()))
+    for _ in range(N)
+]
+
+# dp[i][j] = (i, j) 부터 시작하는 최대 이동 거리
+dp = [[0] * N for _ in range(N)]
+
+def in_range(x, y):
+    return 0 <= x < N and 0 <= y < N
+
+def travel(x, y, move_cnt):
+    if dp[x][y]:
+        return dp[x][y]
+
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    max_move_cnt = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if in_range(nx, ny) and matrix[nx][ny] > matrix[x][y]:
+            max_move_cnt = max(max_move_cnt, move_cnt + travel(nx, ny, move_cnt))
+
+    dp[x][y] = max_move_cnt
+    return max_move_cnt
+
+
+for i in range(N):
+    for j in range(N):
+        if not dp[i][j]:
+            travel(i, j, 1)
+
+print(max(map(max, dp))+1)
