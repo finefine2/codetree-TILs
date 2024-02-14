af73d07e6954cd3aebd58471c3419649a13f2e8b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 16:55:25 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1580ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240214/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240214/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,5 +1,6 @@
-from collections import deque
-
+import sys
+sys.setrecursionlimit(250000)
+# from collections import deque
 n = int(input())
 
 arr = []
@@ -7,35 +8,65 @@ for i in range(n):
     tmp = list(map(int, input().split()))
     arr.append(tmp)
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
 def isin(a, b):
-    return 0 <= a < n and 0 <= b < n
+    return 0<=a<n and 0<=b<n
 
 ans = 0
+# for i in range(n):
+#     for j in range(n):
+#         q = deque()
+        # dp = [[1] * n for _ in range(n)]
+
+#         q.append((i, j))
+#         while q:
+#             x, y = q.popleft()
+#             for i in range(4):
+#                 nx = x + dx[i]
+#                 ny = y + dy[i]
+#                 if isin(nx, ny) and isin(x, y) and arr[x][y] < arr[nx][ny]:
+#                     dp[nx][ny] = max(dp[x][y] + 1, dp[nx][ny])
+#                     q.append((nx, ny))
+        
+#         for k in range(n):
+#             for l in range(n):
+#                 print(dp[k][l], end = " ")
+#                 ans = max(ans, dp[k][l])
+#             print()
+
+dp = [[0] * n for _ in range(n)]
+
+def DFS(x, y, dist):
+
+    if dp[x][y]:
+        return dp[x][y]
+
+    Max = 0
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if isin(nx, ny) and arr[x][y] < arr[nx][ny]:
+            Max = max(Max, dist + DFS(nx, ny, dist))
+    
+    dp[x][y] = Max
+    
+    return Max
+
 for i in range(n):
     for j in range(n):
-        q = deque()
-        dp = [[1] * n for _ in range(n)]
+        if not dp[i][j]:
+            DFS(i, j, 1)
 
-        q.append((i, j))
-        while q:
-            if len(q) > 250000:  # 큐의 크기가 최대 요소 수를 초과하면 중단
-                break
-            x, y = q.popleft()
-            for k in range(4):
-                nx = x + dx[k]
-                ny = y + dy[k]
-                if isin(nx, ny) and arr[x][y] < arr[nx][ny]:
-                    dp[nx][ny] = max(dp[x][y] + 1, dp[nx][ny])
-                    q.append((nx, ny))
+for i in range(n):
+    for j in range(n):
+        ans = max(ans, dp[i][j])
 
-        for k in range(n):
-            for l in range(n):
-                ans = max(ans, dp[k][l])
-
-print(ans)
+print(ans + 1)
 
         
 # from collections import deque
