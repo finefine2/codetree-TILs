d24eaf6b81fc948fe7c93d05f4a0188485d5d769
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 15:35:52 +0900

    Title: 정수 사각형 최소 합, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240214/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ 240214/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -12,10 +12,14 @@ for i in range(n-2, -1, -1):
     dp[0][i] = arr[0][i] + dp[0][i+1]
 for i in range(1, n):
     dp[i][0] = arr[i][0] + dp[i-1][0]
+    dp[i][n-1] = arr[i][n-1] + dp[i-1][n-1]
 
 for i in range(1, n):
-    for j in range(n-2, 0, -1):
-        dp[i][j] = max(dp[i][j-1] + arr[i][j], dp[i-1][j] + arr[i][j])
+    for j in range(n-2, -1, -1):
+        if j == 0:
+            dp[i][0] = min(dp[i-1][0] + arr[i][0], dp[i][1] + arr[i][0])
+        else:
+            dp[i][j] = min(dp[i][j-1] + arr[i][j], dp[i-1][j] + arr[i][j])
 
 # for i in range(n):
 #     for j in range(n):
