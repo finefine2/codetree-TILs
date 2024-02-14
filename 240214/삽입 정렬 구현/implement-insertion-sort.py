71c980f40037d2c8b613bb4cd8cb271314280daa
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-14 11:53:53 +0900

    Title: 삽입 정렬 구현, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/삽입 정렬 구현/implement-insertion-sort.py
+++ 240214/삽입 정렬 구현/implement-insertion-sort.py
@@ -1,6 +1,14 @@
 N = int(input()) 
 nums = list(map(int,input().split())) 
 
-nums.sort() 
+def insert_sort(): 
+    for i in range(1,N): 
+        j, key = i-1, nums[i] 
+        while j >= 0 and nums[j] > key: 
+            nums[j+1] = nums[j] 
+            j -= 1 
+        nums[j+1] = key 
+insert_sort()
+
 for n in nums: 
     print(n, end = " ")
