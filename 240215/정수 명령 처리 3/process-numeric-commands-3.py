9556eedd80cd7c422294a56b15b0ee5c0e6898d4
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-15 14:11:28 +0900

    Title: 정수 명령 처리 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240215/정수 명령 처리 3/process-numeric-commands-3.py
@@ -0,0 +1,25 @@
+from collections import deque
+N = int(input()) 
+q = deque() 
+
+for _ in range(N): 
+    command = list(map(str,input().split())) 
+    if command[0] == "push_front": 
+        q.appendleft(int(command[1]))
+    elif command[0] == "push_back": 
+        q.append(int(command[1])) 
+    elif command[0] == "pop_front": 
+        print(q.popleft())
+    elif command[0] == "pop_back": 
+        print(q.pop()) 
+    elif command[0] == "size": 
+        print(len(q))
+    elif command[0] == "empty": 
+        if not q: 
+            print(1) 
+        else: 
+            print(0) 
+    elif command[0] == "front" 
+        print(q[0]) 
+    else: 
+        print(q[-1])
