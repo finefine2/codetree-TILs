bb5de674a2d569547c9edf3d13ca5d3c385e2f34
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-15 13:48:47 +0900

    Title: 정수 명령 처리 2, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240215/정수 명령 처리 2/process-numeric-commands-2.py
@@ -0,0 +1,38 @@
+from collections import deque 
+
+class Queue: 
+    def __init__(self): 
+        self.dq = deque() 
+    def push(self,item): 
+        self.dq.append(item) 
+    def empty(self): 
+        return not self.dq 
+    def size(self): 
+        return len(self.dq) 
+    def pop(self): 
+        if self.empty(): 
+            raise Exception("Queue is empty") 
+        return self.dq.popleft() 
+    def front(self): 
+        if self.empty(): 
+            raise Exception("Queue is empty") 
+        return self.dq[0] 
+
+N = int(input()) 
+q = Queue() 
+
+for _ in range(N): 
+    command = list(input().split()) 
+    if command[0] == 'push': 
+        q.push(int(command[1]))
+    elif command[0] == 'empty': 
+        if q.empty: 
+            print(1)
+        else: 
+            print(0) 
+    elif command[0] == 'size': 
+        print(q.size()) 
+    elif command[0] == 'pop': 
+        print(q.pop())
+    elif command[0] == 'front':     
+        print(q.front())
