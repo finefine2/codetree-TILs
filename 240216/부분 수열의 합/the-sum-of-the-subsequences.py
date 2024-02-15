49e29f5f3b3f20c71e1d2d2b4602a2d59fba69b7
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-16 00:29:03 +0900

    Title: 부분 수열의 합, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 240216/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -5,12 +5,14 @@ arr = [0] + list(map(int, input().split()))
 dp = [-1] * (m+1)
 
 dp[0] = 0
-for i in range(m, -1, -1):
-    for j in range(1, n+1):
-        if i >= arr[j]:
-            if dp[i - arr[j]] == -1:
+
+# arr의 원소하나에 대해서 각각 dp를 돌아야 하니까 1 ~ n이 바깥에 와야 한다.
+for i in range(1, n+1):
+    for j in range(m, -1, -1):
+        if j >= arr[i]:
+            if dp[j - arr[i]] == -1:
                 continue
-            dp[i] = max(dp[i], dp[i - arr[j]] + 1)
+            dp[j] = max(dp[j], dp[j - arr[i]] + 1)
 
 # for i in range(m+1):
 #     print(dp[i], end = " ")
