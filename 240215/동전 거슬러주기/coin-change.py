c94e602c9d86ca5f8d7e53987f1f99e5c82b39fd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-15 18:17:05 +0900

    Title: 동전 거슬러주기, Time: 200ms, Memory: 25MB, Status: Passed - CodeTree

--- 240215/동전 거슬러주기/coin-change.py
+++ 240215/동전 거슬러주기/coin-change.py
@@ -1,25 +1,29 @@
 n, m = map(int, input().split())
 
-arr = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+# coin에 있어서 인덱스로 했기에 이제까지 인덱스 오류였음..
 
-dp = [-1] * (m+1)
+import sys
+Max = sys.maxsize
+dp = [Max] * (m+1)
 # dp는 지금까지 선택한 동전의 합이 i라 했을 때 가능한 최대 동전의 개수
 
 # arr.sort()
 
 dp[0] = 0
 for i in range(1, m+1):
-    for j in range(1, n):
+    for j in range(1, n+1):
         if i >= arr[j]:
-            if dp[i - arr[j]] == -1:
+            if dp[i - arr[j]] == Max:
                 continue
 
-            dp[i] = max(dp[i], dp[i - arr[j]] + 1)
+            dp[i] = min(dp[i], dp[i - arr[j]] + 1)
 
-if dp[m] == -1:
-    print(-1)
-else:
-    print(dp[m])
+ans = dp[m]
+
+if ans == Max:
+    ans = -1
+print(ans)
 
 # for k in dp:
 #     print(k, end = " ")
