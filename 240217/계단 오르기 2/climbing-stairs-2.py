471ac1aebcf2dda5ea15fd68c95e40035945dc61
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-17 02:25:42 +0900

    Title: 계단 오르기 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240217/계단 오르기 2/climbing-stairs-2.py
+++ 240217/계단 오르기 2/climbing-stairs-2.py
@@ -1,33 +1,38 @@
 n = int(input())
 
-arr = list(map(int, input().split()))
+arr =  [0] + list(map(int, input().split()))
 
-dp = [[0] * 4 for _ in range(n)]
+dp = [[0] * 4 for _ in range(n+1)]
 
 dp[0][0] = 0
-
-for i in range(1, n):
+dp[1][1] = arr[1]
+for i in range(2, n+1):
     for j in range(4):
+
+        if j == 0:
+            dp[i][j] = dp[i-2][j] + arr[i]
+        else:
+            dp[i][j] = max(dp[i-1][j-1], dp[i-2][j]) + arr[i]
+
+
         # if i >= 2:  # i에서 두칸 전
         
-        if j > 0:   # 한칸 건너는 경우이므로 j도 하나 작다 (한칸 건너면 j가 커진다.)
-            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i])
+        # if j > 0:   # 한칸 건너는 경우이므로 j도 하나 작다 (한칸 건너면 j가 커진다.)
+        #     dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i])
 
-        # 2계단 오르기, 이전 계단을 밟지 않고 현재만 밟을 때
-        if i > 1:
-            dp[i][j] = max(dp[i][j], dp[i-2][j] + arr[i])
-
+        # # 2계단 오르기, 이전 계단을 밟지 않고 현재만 밟을 때
+        # if i >= 2:
+        #     dp[i][j] = max(dp[i][j], dp[i-2][j] + arr[i])
+        # else:
+        #     dp[i][j] = max(dp[i][j], dp[i-1][j] + arr[i], arr[i])
         # 첫번째에서 바로 2계단 오르는 경우
-        if i == 1:
-            dp[i][j] = max(dp[i][j], arr[i])
+        # if i == 1:
+        #     dp[i][j] = max(dp[i][j], arr[i])
 
-# for i in range(n):
-#     for j in range(4):
-#         print(dp[i][j], end = " ")
-#     print()
 
 ans = 0
-for j in range(4):
-    ans = max(ans, dp[n-1][j])
+# for j in range(4):
+#     ans = max(ans, dp[n][j])
+print(max(dp[n]))
 
-print(ans)
+# print(ans)
