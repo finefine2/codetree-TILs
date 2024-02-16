2c9128832bdbcf4cca35919efeb3726bc42422ab
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-16 20:17:57 +0900

    Title: 최대 합 분할, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240216/최대 합 분할/maximum-sum-partition.py
+++ 240216/최대 합 분할/maximum-sum-partition.py
@@ -1,5 +1,28 @@
-n = int(input())
+# n = int(input())
 
+# arr = [0] + list(map(int, input().split()))
+
+# Max = sum(arr)
+# dp = [0] * (Max + 1)
+
+# dp[0] = True
+# for i in range(1, n+1):
+#     for j in range(Max, 0, -1):
+#         if j - arr[i] >= 0 and dp[j - arr[i]]:
+#             dp[j] = True
+
+# ans = 0
+# for i in range(Max + 1):
+#     if dp[i] and (Max - i) % 2 == 0 and dp[(Max - i) // 2]:
+#         ans = max(ans, (Max - i) // 2)
+#         print((Max - i) / 2)
+
+# print(Max)
+# for i in range(len(dp)):
+#     print(dp[i], end = " ")
+
+# print(ans)
+n = int(input())
 arr = [0] + list(map(int, input().split()))
 
 Max = sum(arr)
@@ -7,13 +30,14 @@ dp = [0] * (Max + 1)
 
 dp[0] = True
 for i in range(1, n+1):
-    for j in range(Max):
+    for j in range(Max, 0, -1):
         if j - arr[i] >= 0 and dp[j - arr[i]]:
             dp[j] = True
 
 ans = 0
-for i in range(Max + 1):
-    if dp[i] and (Max - i) % 2 == 0 and dp[(Max - i) // 2]:
-        ans = max(ans, (Max - i) // 2)
+for i in range(Max // 2, -1, -1):
+    if dp[i] and dp[Max // 2 + i]:
+        ans = i
+        break
 
 print(ans)
