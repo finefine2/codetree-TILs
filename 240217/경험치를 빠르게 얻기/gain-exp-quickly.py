f391d2da41819f565ee33395be7ce74ffe4e105e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-17 00:32:06 +0900

    Title: 경험치를 빠르게 얻기, Time: 971ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240217/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 240217/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -5,16 +5,26 @@ for i in range(n):
     s, e = map(int, input().split())
     arr.append((s, e))
 
+s = 0
+for k in arr:
+    s += k[0]
+
 import sys
 MAX = sys.maxsize
-dp = [MAX] * 1000001
+dp = [MAX] * (s + 1)
 
 dp[0] = 0
-for i in range(m, -1, -1):
-    for j in range(n):
+for j in range(n):
+    for i in range(s, 0, -1):
         if i - arr[j][0] >= 0:
             if i - arr[j][0] == MAX:
                 continue
             dp[i] = min(dp[i], dp[i - arr[j][0]] + arr[j][1])
 
-print(dp[m])
+Min = MAX
+for i in range(m, s + 1):
+    if Min > dp[i]:
+        Min = dp[i]
+    # print(dp[i], end = " ")
+
+print(Min)
