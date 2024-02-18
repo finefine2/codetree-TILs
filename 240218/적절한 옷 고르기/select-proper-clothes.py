7f60b6a5d8642726704ca8df29cc166aa54ef544
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-18 17:58:32 +0900

    Title: 적절한 옷 고르기, Time: 309ms, Memory: 26MB, Status: Passed - CodeTree

--- 240218/적절한 옷 고르기/select-proper-clothes.py
+++ 240218/적절한 옷 고르기/select-proper-clothes.py
@@ -17,11 +17,12 @@ for i in range(1, n+1):
 
 for i in range(2, m+1):
     for j in range(1, n+1):
-        # i번째 날에 j번째 옷
+        # i번째 날에 j번째 옷을 입을 수 있고
         if arr[j][0] <= i <= arr[j][1]:
             # i-1번째 날에 k번째 옷을 입는 경우를 찾는다.
             for k in range(1, n+1):
+                # i-1번째 날에 k번째 옷을 입을 수 있어야 한다.
                 if arr[k][0] <= i - 1 <= arr[k][1]:
                     dp[i][j] = max(dp[i][j], dp[i-1][k] + abs(arr[j][2] - arr[k][2]))
-
+                    # 이렇게 해서 i날에는 j, i-1날에는 k, 거기다가 j값에서 k값의 절대값 차를 더해준다.
 print(max(dp[-1]))
