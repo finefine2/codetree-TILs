8bf12682c2aa45473873574ece817fb45283a380
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-19 15:53:53 +0900

    Title: 신전 탐험하기 2, Time: 182ms, Memory: 26MB, Status: Passed - CodeTree

--- 240219/신전 탐험하기 2/explore-temple-2.py
+++ 240219/신전 탐험하기 2/explore-temple-2.py
@@ -9,16 +9,16 @@ for i in range(n):
 dp = [[[0 for _ in range(3)] for _ in range(3)] for _ in range(n)]
 
 for i in range(3):
-    for j in range(3):
-        if i != j:  # 1층과 n층에서 같은 방을 선택하지 않는 조건
-            dp[0][i][j] = arr[0][j]
+    # for j in range(3):
+    #     if i != j:  # 1층과 n층에서 같은 방을 선택하지 않는 조건
+    dp[0][i][i] = arr[0][i]
 
 for i in range(1, n):
     for j in range(3):  # 이전 층의 방 선택
         for k in range(3):  # 현재 층의 방 선택
             for l in range(3):  # 다음 층에서 고려할 방 선택
-                if j != k:  # 연속하여 같은 방을 선택하지 않는 조건
-                    dp[i][k][l] = max(dp[i][k][l], dp[i-1][j][k] + arr[i][l])
+                if k != l:  # 연속하여 같은 방을 선택하지 않는 조건
+                    dp[i][j][l] = max(dp[i][j][l], dp[i-1][j][k] + arr[i][l])
 
 # 최종 층에서의 최대 보물 개수 찾기
 result = 0
