1bc030062443a7c1e6938daf51e27f9855d9cd79
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-19 19:31:15 +0900

    Title: 최고의 33위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240219/최고의 33위치/best-place-of-33.py
+++ 240219/최고의 33위치/best-place-of-33.py
@@ -1,16 +1,19 @@
 N = int(input()) 
-
 board = [list(map(int,input().split())) for _ in range(N)] 
 
-ans = -1 
+def count(row_s, col_s, row_e, col_e): 
+    cnt = 0 
+    for r in range(row_s,row_e+1): 
+        for c in range(col_s,col_e+1): 
+            cnt += board[r][c]
+    return cnt 
 
-for i in range(N-2): 
-    for j in range(N-2): 
-        cnt = 0 
-        for k in range(3): 
-            for l in range(3): 
-                if board[i+k][j+l] == 1: 
-                    cnt += 1 
-        if cnt > ans: 
-            ans = cnt 
-print(ans)
+max_num = 0 
+
+for r in range(N): 
+    for c in range(N): 
+        if r + 2 >= N and c + 2 > = N: 
+            continue 
+        num = count(r,c,r+2,c+2): 
+        max_num = max(max_num,num) 
+print(max_num)
