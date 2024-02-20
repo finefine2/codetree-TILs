1c694d4213ae50b23f57426050849168dc942a62
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-21 01:04:45 +0900

    Title: 최장 공통 부분 수열 3, Time: 135ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240221/최장 공통 부분 수열 3/longest-common-sequence-3.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+dp = [[0] * (m+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+i, j = n, m
+st = []
+while i > 0 and j > 0:
+    if a[i-1] == b[j-1]:  # 문자가 같은 경우, 해당 문자를 결과에 추가하고 대각선으로 이동
+        st.append(str(a[i-1]))
+        i -= 1
+        j -= 1
+    elif dp[i-1][j] > dp[i][j-1]:  # 위쪽이 더 큰 경우, 위로 이동
+        i -= 1
+    else:  # 왼쪽이 더 큰 경우, 왼쪽으로 이동
+        j -= 1
+
+print(' '.join(reversed(st)))
