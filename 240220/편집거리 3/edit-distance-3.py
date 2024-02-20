2a8d40ff370faacbb4e1020d3fdfa2156bd457bb
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-20 17:33:45 +0900

    Title: 편집거리 3, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240220/편집거리 3/edit-distance-3.py
@@ -0,0 +1,24 @@
+ai = input()
+bi = input()
+
+aa = len(ai)
+bb = len(bi)
+
+a = " " + ai
+b = " " + bi
+
+dp = [[0] * (bb + 1) for _ in range(aa + 1)]
+
+for i in range(1, aa + 1):
+    dp[i][0] = i
+for j in range(1, bb + 1):
+    dp[0][j] = j
+
+for i in range(1, aa + 1):
+    for j in range(1, bb + 1):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + 1
+
+print(dp[aa - 1][bb - 1])
