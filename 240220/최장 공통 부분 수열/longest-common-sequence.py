bd08ec54747edce97e19e6edc19a87f040699b79
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-20 16:52:55 +0900

    Title: 최장 공통 부분 수열, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240220/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,35 @@
+ai = input()
+bi = input()
+
+a = " " + ai
+b = " " + bi
+lena = len(ai)
+lenb = len(bi)
+
+dp = [[0] * (lenb + 1) for _ in range(lena + 1)]
+
+if a[1] == b[1]:
+    dp[1][1] = 1
+else:
+    dp[1][1] = 2
+
+for i in range(2, lena + 1):
+    if a[i] == b[1]:
+        dp[i][1] = i
+    else:
+        dp[i][1] = dp[i-1][1] + 1
+
+for j in range(2, lenb + 1):
+    if a[1] == b[j]:
+        dp[1][j] = j
+    else:
+        dp[1][j] = dp[1][j-1] + 1
+
+for i in range(3, lena + 1):
+    for j in range(3, lenb + 1):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+
+print(dp[lena][lenb])
