ab79284f54e4b2612bb93c6d1dab7953b02eab46
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-20 17:22:52 +0900

    Title: 편집거리, Time: 221ms, Memory: 32MB, Status: Passed - CodeTree

--- 240220/편집거리/edit-distance.py
+++ 240220/편집거리/edit-distance.py
@@ -1,37 +1,59 @@
-a = input()
-b = input()
+ai = input()
+bi = input()
 
-aa = len(a)
-bb = len(b)
+aa = len(ai)
+bb = len(bi)
+
+a = " " + ai
+b = " " + bi
 
 dp = [[0] * (bb + 1) for _ in range(aa + 1)]
 
-if a[0] == b[0]:
-    dp[0][0] = 0
-else:
-    dp[0][0] = 1
+for i in range(1, aa + 1):
+    dp[i][0] = i
 
+for j in range(1, bb + 1):
+    dp[0][j] = j
 
-for i in range(1, aa):
-    if a[i] == b[0]:
-        dp[i][0] = dp[i-1][0]
-    else:
-        dp[i][0] = dp[i-1][0] + 1
-
-for i in range(1, bb):
-    if a[0] == b[i]:
-        dp[0][i] = dp[0][i-1]
-    else:
-        dp[0][i] = dp[0][i-1] + 1
-
-for i in range(1, aa):
-    for j in range(1, bb):
+for i in range(1, aa + 1):
+    for j in range(1, bb + 1):
         if a[i] == b[j]:
             dp[i][j] = dp[i-1][j-1]
         else:
-            if i == j:
-                dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
-            else:
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+            dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
 
-print(dp[aa - 1][bb - 1])
+# for i in range(aa):
+#     for j in range(bb):
+#         print(dp[i][j], end = " ")
+#     print()
+
+print(dp[aa][bb])
+
+
+# for i in range(1, aa):
+#     if a[i] == b[0]:
+#         dp[i][0] = dp[i-1][0]
+#     else:
+#         dp[i][0] = dp[i-1][0] + 1
+
+# for i in range(1, bb):
+#     if a[0] == b[i]:
+#         dp[0][i] = dp[0][i-1]
+#     else:
+#         dp[0][i] = dp[0][i-1] + 1
+
+# for i in range(1, aa):
+#     for j in range(1, bb):
+#         if a[i] == b[j]:
+#             dp[i][j] = dp[i-1][j-1]
+#         else:
+#             if i == j:
+#                 dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+#             else:
+#                 dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+
+# for i in range(aa):
+#     for j in range(bb):
+#         print(dp[i][j], end = " ")
+#     print()
+# print(dp[aa - 1][bb - 1])
