f38f87854ee7bd43c83d577092b84a7e19343dcd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-21 12:39:36 +0900

    Title: 정수 n개의 합 2, Time: 134ms, Memory: 34MB, Status: Passed - CodeTree

--- 240221/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 240221/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -1,17 +1,32 @@
-n, k = map(int, input().split())
+# n, k = map(int, input().split())
 
+# arr = list(map(int, input().split()))
+
+# s = [0] * (n+1)
+
+# s[0] = arr[0]
+# for i in range(1, n):
+#     s[i] = s[i-1] + arr[i]
+
+# ans = 0
+# for i in range(1, n - k + 1):
+#     num = num - arr[i - 1] + arr[i + k - 1]
+#     ans = max(ans, num)
+
+# # for i in range(n-k):
+# #     for j in range(i+1, n):
+# #         ans = max(ans, s[j] - s[i] + arr[i])
+
+# print(ans)
+
+n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-s = [0] * (n+1)
+s = sum(arr[:k])
+ans = s
 
-s[0] = arr[0]
-for i in range(1, n):
-    s[i] = s[i-1] + arr[i]
-
-ans = 0
-for i in range(n):
-    for j in range(i+1, n):
-        if j < i + k:
-            ans = max(ans, s[j] - s[i] + arr[i])
+for i in range(1, n - k + 1):
+    s = s - arr[i - 1] + arr[i + k - 1]
+    ans = max(ans, s)
 
 print(ans)
