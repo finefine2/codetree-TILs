5911eadbf4e8ca8f6e82a9d2f25ed7fa46658dde
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-22 17:47:13 +0900

    Title: 연속한 K개의 숫자, Time: 152ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240222/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -0,0 +1,21 @@
+n, k, b = map(int, input().split())
+
+pre_sum = [0] * (n+1)
+arr = [0] * (n+1)
+for i in range(b):
+    a = int(input())
+    arr[a] = 1
+
+for i in range(1, n+1):
+    pre_sum[i] = pre_sum[i-1] + arr[i]
+# arr[i]가 0일 경우 별 차이 없이 그냥 지나가고
+# 1일 경우에는 하나 증가된다.
+
+import sys
+ans = sys.maxsize
+for i in range(1, n-k+2):
+    ans = min(ans, pre_sum[i + k - 1] - pre_sum[i - 1])
+
+# 여기서 pre_sum[i+k-1] - pre_sum[i-1]의 뜻은 k만큼의 간격을 가진 숫자들의 합을 구해줘서
+# 추가해줘야 하는 숫자 개수를 바로 출력하는 것이다.
+print(ans)
