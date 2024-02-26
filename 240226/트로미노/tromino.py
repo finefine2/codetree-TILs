95500f90fc6f75714cf598f8079b2db7d434fff5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-26 20:07:41 +0900

    Title: 트로미노, Time: 218ms, Memory: 26MB, Status: Passed - CodeTree

--- 240226/트로미노/tromino.py
+++ 240226/트로미노/tromino.py
@@ -1,89 +1,52 @@
 N,M = map(int,input().split()) 
 
-board = list(list(map(int,input().split())) for _ in range(N))
+board = [list(map(int,input().split())) for _ in range(N)] 
 
-# 가능한 블럭들을 나열하기 
-blocks = [
-    [[1,0],[1,1]],
-    [[0,1],[1,1]],
-    [[1,1],[0,1]],
-    [[1,1],[1,0]],
-    [[1,1,1]],
-    [[1],[1],[1]]
+shapes = [
+    [[1, 1, 0],
+    [1, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 0],
+    [0, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0]],
 ]
 
-'''
-1. 블럭을 하나 고르기 
-2. 고른 블럭을 board에 얹고 계산해보기 
-- 블럭의 좌표 그리고 board의 좌표는 따로 고려해야하나! 
-
-'''
+def get_max_sum(r,c): 
+    max_sum = 0 
+    for i in range(6): 
+        is_possible = True 
+        sum_of_nums = 0 
+        for dr in range(0,3): 
+            for dc in range(0,3): 
+                if shapes[i][dr][dc] == 0: 
+                    continue 
+                if r + dr >= N or c + dc >= M: 
+                    is_possible = False 
+                else: 
+                    sum_of_nums += board[r+dr][c+dc] 
+        if is_possible: 
+            max_sum = max(max_sum, sum_of_nums) 
+    return max_sum 
 ans = 0 
 
-
-# 2 x 2 blocks 
-def count1(r,c): 
-    ans = board[r][c] + board[r+1][c] + board[r+1][c+1] 
-    return ans 
-def count2(r,c): 
-    ans = board[r][c] + board[r+1][c] + board[r][c+1] 
-    return ans 
-def count3(r,c): 
-    ans = board[r][c] + board[r][c+1] + board[r+1][c+1] 
-    return ans 
-def count4(r,c): 
-    ans = board[r][c+1] + board[r+1][c] + board[r+1][c+1]  
-    return ans 
-# 1 x 3 
-def count5(r,c): 
-    ans = board[r][c] + board[r][c+1] + board[r][c+2] 
-    return ans 
-# 3 x 1 
-def count6(r,c): 
-    ans = board[r][c] + board[r+1][c] + board[r+2][c] 
-    return ans 
-
-for i in range(N-1): 
-    for j in range(N-1): 
-        if count1(i,j) > ans: 
-            ans = count1(i,j) 
-         
-for i in range(N-1): 
-    for j in range(N-1): 
-        if count2(i,j) > ans: 
-            ans = count2(i,j)
-         
-for i in range(N-1): 
-    for j in range(N-1): 
-        if count3(i,j) > ans: 
-            ans = count3(i,j)
-
-for i in range(N-1): 
-    for j in range(N-1): 
-        if count4(i,j) > ans: 
-            ans = count4(i,j) 
-
-for i in range(N-3): 
-    for j in range(N): 
-        if count6(i,j) > ans: 
-            ans = count6(i,j)
-
 for i in range(N): 
-    for j in range(N-3): 
-        if count5(i,j) > ans: 
-            ans = count5(i,j) 
-print(ans) 
-# for b in blocks: 
-#     row_end = len(b) 
-#     col_end = len(b[0]) 
-#     cnt = 0
-#     for i in range(len(board)): 
-#         for j in range(len(board[0])): 
-#             for r in range(row_end): 
-#                 for c in range(col_end): 
-#                     if i + r >= len(board) or j + c >= len(board[0]): 
-#                         continue 
-#                     else: 
-#                         cnt += board[i][j] * b[r][c]
-#                     ans = max(cnt,ans)
-# print(ans)
+    for j in range(M): 
+        ans = max(ans, get_max_sum(i,j)) 
+print(ans)
