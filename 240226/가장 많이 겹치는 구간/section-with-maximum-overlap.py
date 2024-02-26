7c715be8cd75e034b49a73beb01ff03566d9b14b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-26 17:34:43 +0900

    Title: 가장 많이 겹치는 구간, Time: 232ms, Memory: 28MB, Status: Passed - CodeTree

--- 240226/가장 많이 겹치는 구간/section-with-maximum-overlap.py
+++ 240226/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-check = [0] * 100001
+check = [0] * 200001
 
 arr = []
 for i in range(n):
@@ -9,13 +9,11 @@ for i in range(n):
     check[a] += 1
     check[b] -= 1
 
-s = [0] * 100001
+s = [0] * 200001
 
-for i in range(100000):
+for i in range(200000):
     s[i+1] = s[i] + check[i]
     
-
-
 print(max(s))
 
 # for i in range(100001):
