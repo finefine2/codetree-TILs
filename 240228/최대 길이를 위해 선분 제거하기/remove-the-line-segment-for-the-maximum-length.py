e219ab52ab206a5af43e0151afc0be3fb17a7226
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-02-28 18:33:05 +0900

    Title: 최대 길이를 위해 선분 제거하기, Time: 1152ms, Memory: 54MB, Status: Passed - CodeTree

--- 240228/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
+++ 240228/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
@@ -10,6 +10,7 @@ for i, (a, b) in enumerate(arr):
     point.append((a, 1, i))
     point.append((b, -1, i))
 
+# 빠지는 길이를 여기에 넣어준다.
 check = [0] * n
 point.sort()
 s = set()
@@ -24,6 +25,8 @@ for x, v, idx in point:
         total += (x - prev)
     
     # set의 크기가 1개일 경우 
+    # set의 첫번째를 target으로 해서 그 경우에 값을 더한다. 
+    # 그 값이 없을 경우의 합을 구하는 것
     if cnt == 1:
         target = list(s)[0]
         check[target] += (x - prev)
@@ -32,12 +35,15 @@ for x, v, idx in point:
         s.add(idx)
     else:
         s.remove(idx)
+    # 1일 경우 더해주고 아닐 경우 지운다.
 
     prev = x
+    # prev를 지금의 x로 갱신한다.
 
 import sys
 Min = sys.maxsize
 for i in range(n):
     Min = min(Min, check[i])
+    # 빠지게 되는 길이의 최솟값을 갱신한다.
 
 print(total - Min)
