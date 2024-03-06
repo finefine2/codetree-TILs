5a7b502c06870ee89eb52fafe4af1841fa471a78
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-06 16:20:33 +0900

    Title: 부분수열 여부 판단하기, Time: 146ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 240306/부분수열 여부 판단하기/determine-subsequence.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+def is_subsequence(a, b, n, m):
+    i = 0  
+    j = 0  
+
+    # A와 B를 순회하며 B의 모든 요소가 A에 순서대로 있는지 확인
+    while i < n and j < m:
+        if a[i] == b[j]:
+            j += 1
+        i += 1
+
+    # B의 모든 요소가 A에서 찾아졌는지 여부를 반환
+    return j == m
+
+if is_subsequence(a, b, n, m):
+    print("Yes")
+else:
+    print("No")
