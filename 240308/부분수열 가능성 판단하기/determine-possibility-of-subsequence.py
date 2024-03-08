fa8c1aee5915493188e86ff5e2e24f75f99b62a5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-08 17:26:18 +0900

    Title: 부분수열 가능성 판단하기, Time: 319ms, Memory: 34MB, Status: Passed - CodeTree

--- 240308/부분수열 가능성 판단하기/determine-possibility-of-subsequence.py
+++ 240308/부분수열 가능성 판단하기/determine-possibility-of-subsequence.py
@@ -1,27 +1,57 @@
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+a = [0] + list(map(int, input().split()))
+b = [0] + list(map(int, input().split()))
 
 
-def is_subsequence(a, b, n, m):
-    i = 0  
-    j = 0  
+# def is_subsequence(a, b, n, m):
+#     i = 0  
+#     j = 0  
 
-    # A와 B를 순회하며 B의 모든 요소가 A에 순서대로 있는지 확인
-    while i < n and j < m:
-        if a[i] == b[j]:
-            j += 1
+#     # A와 B를 순회하며 B의 모든 요소가 A에 순서대로 있는지 확인
+#     while i < n and j < m:
+#         if a[i] == b[j]:
+#             j += 1
+#         i += 1
+
+#     # B의 모든 요소가 A에서 찾아졌는지 여부를 반환
+#     return j == m
+
+# ans = 0
+
+# for k in range(len(b)):
+#     c = b[:k] + b[k+1:]
+
+#     if is_subsequence(a, c, n, m-1):
+#         ans += 1
+
+# print(ans)
+
+L = [0] * (m + 2)
+R = [0] * (m + 2)
+
+i = 1
+for j in range(1, m + 1):
+    while i <= n and a[i] != b[j]:
+        i += 1
+    L[j] = i
+    if i <= n:
         i += 1
 
-    # B의 모든 요소가 A에서 찾아졌는지 여부를 반환
-    return j == m
+i = n
+for j in range(m, 0, -1):
+    while i >= 1 and a[i] != b[j]:
+        i -= 1
+    R[j] = i
+    if i >= 1:
+        i -= 1
+
+L[0] = 0
+R[m + 1] = n + 1
 
 ans = 0
-
-for k in range(len(b)):
-    c = b[:k] + b[k+1:]
-
-    if is_subsequence(a, c, n, m-1):
+for j in range(1, m+1):
+    if L[j - 1] < R[j + 1]:
         ans += 1
+    # 검사를 할 때 L과 R을 따로 만들어서 가운데인 j만 빼고 검사하면 됨
 
 print(ans)
