f58a392df9b165572d4ae1a0b6141a450b7f8240
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-08 14:26:47 +0900

    Title: 서로 다른 k개의 문자, Time: 210ms, Memory: 25MB, Status: Passed - CodeTree

--- 240308/서로 다른 k개의 문자/k-distinct-characters.py
+++ 240308/서로 다른 k개의 문자/k-distinct-characters.py
@@ -7,20 +7,35 @@ left = 0
 ans = 0
 
 for right in range(len(st)):
-
-    if st[right] in str_idx:
-        str_idx[st[right]] += 1
-    else:
+    if st[right] not in str_idx:
         str_idx[st[right]] = 1
+    else:
+        str_idx[st[right]] += 1
     
     while len(str_idx) > k:
-        str_idx[st[right]] -= 1
+        str_idx[st[left]] -= 1
         if str_idx[st[left]] == 0:
             del str_idx[st[left]]
         left += 1
     
     ans = max(ans, right - left + 1)
     
+
+        # right_char = s[right]
+        # if right_char not in char_frequency:
+        #     char_frequency[right_char] = 0
+        # char_frequency[right_char] += 1
+
+        # # 서로 다른 문자의 수가 k를 초과하는 경우 왼쪽 포인터 이동
+        # while len(char_frequency) > k:
+        #     left_char = s[left]
+        #     char_frequency[left_char] -= 1
+        #     if char_frequency[left_char] == 0:
+        #         del char_frequency[left_char]
+        #     left += 1
+        
+        # # 현재 윈도우의 길이가 최대 길이보다 큰지 검사
+        # max_length = max(max_length, right - left + 1)
 
  
 print(ans)
