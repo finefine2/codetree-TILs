210ead43d33ee0d36196efbfb448a5c9abdbf5bd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-18 15:18:36 +0900

    Title: 정수 분배하기, Time: 139ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240318/정수 분배하기/distributing-integers.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+
+arr = []
+for i in range(n):
+    a = int(input())
+    arr.append(a)
+
+left = 1
+right = min(arr)
+ans = 0
+
+while left <= right:
+    mid = (left + right) // 2
+
+    num = 0
+    for k in arr:
+        num += (k // mid)
+    
+    if num >= m:
+        ans = max(ans, mid)
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(ans)
