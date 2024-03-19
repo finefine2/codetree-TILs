b432090ed264c02fa947ee920ec8b7bc6a95f08c
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-19 16:56:33 +0900

    Title: 부분문자열이 되는 횟수 구하기, Time: 150ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240319/부분문자열이 되는 횟수 구하기/find-the-number-of-partial-strings.py
@@ -0,0 +1,41 @@
+a = input()
+b = input()
+arr = list(map(int, input().split()))
+
+ans = 0
+n = len(a)
+m = len(b)
+check = [0] * n
+
+def find(mid):
+    idx = 0
+
+    for i in range(mid):
+        check[arr[i] - 1] = 1
+    
+    for i in range(n):
+        if check[i]:
+            continue
+        if idx < m and a[i] == b[idx]:
+            idx += 1
+        # 인덱스가 끝나지 않았는데 해당하는 단어가 있을 경우 idx를 늘려준다.
+
+    return idx == m
+    # idx가 m이 될 경우 끝
+
+left = 0
+right = n
+
+while left <= right:
+    mid = (left + right) // 2
+    if find(mid):
+        left = mid + 1
+        ans = max(ans, mid)
+    else:
+        right = mid - 1
+    
+    for i in range(mid):
+        check[arr[i] - 1] = 0
+    # mid 까지의 check를 다시 초기화한다.
+
+print(ans + 1)
