b4f5869e77384202a8c9120dec245ae01f2c052d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-19 14:54:00 +0900

    Title: 격자 칠하기 2, Time: 1364ms, Memory: 178MB, Status: Passed - CodeTree

--- 240319/격자 칠하기 2/painting-the-grid-2.py
+++ 240319/격자 칠하기 2/painting-the-grid-2.py
@@ -64,55 +64,57 @@
 # print(Min)
 
 
-from sys import stdin
-n = int(stdin.readline())
-base_2d = [list(map(int, stdin.readline().split())) for _ in range(n)]
-visited = [[False]*n for _ in range(n)]
+import sys
 
-def in_range(i, j):
-    return 0<=i<n and 0<=j<n
+sys.setrecursionlimit(10000)
 
-from collections import deque
-def bfs(i, j, D):
-    #처음 상태 초기화!
-    visited = [[False]*n for _ in range(n)]
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
 
-    q = deque()
-    q.append((i,j))
-    visited[i][j] = True
+dxs, dys = [1, -1, 0, 0], [0, 0, -1, 1]
+visited = [[False] * n for _ in range(n)]
 
-    dx, dy =[0,0,-1,1], [-1,1,0,0]
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def dfs(x, y, d):
+    if visited[x][y]:
+        return 0
+
+    visited[x][y] = True
+    count = 1
+    for dx, dy in zip(dxs, dys):
+        next_x = x + dx
+        next_y = y + dy
+
+        if in_range(next_x, next_y) and (abs(board[next_x][next_y] - board[x][y]) <= d):
+            count += dfs(next_x, next_y, d)
+
+    return count
+
+def is_possible(d):
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
     
-    total = 1
-
-    while q:
-        x, y = q.popleft()
-        for dxs, dys in zip(dx, dy):
-            next_x, next_y = x+dxs, y+dys
-            if in_range(next_x, next_y) and not visited[next_x][next_y] and abs(base_2d[x][y]-base_2d[next_x][next_y])<=D: #범위안이고 방문을 아직 안한 경우이며 현재 값보다 D이하로 차이나야함!
-                q.append((next_x, next_y))
-                total += 1
-                visited[next_x][next_y] = True
-    return total
-
-def check(D):
-    ans = 0
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                ans = max(ans, bfs(i,j,D))
-                if ans >= (n*n+1)//2:
-                    return True #반올림 처리!
+                if dfs(i, j, d) * 2 >= n * n:
+                    return True
+
     return False
 
-left = 1
+left = 0
 right = 1000000
-min_value = 1000000
+ans = 0
+
 while left <= right:
-    mid = (left+right)//2
-    if check(mid):
-        min_value = min(min_value, mid)
-        right = mid-1
+    mid = (left + right) // 2
+    if is_possible(mid):
+        right = mid - 1
+        ans = mid
     else:
-        left = mid+1
-print(min_value)
+        left = mid + 1
+
+print(ans)
