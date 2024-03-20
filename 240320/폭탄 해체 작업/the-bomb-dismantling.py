09d4f6c131f909b8417ef07ed1d6f41cfb85ea56
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-20 22:05:01 +0900

    Title: 폭탄 해체 작업, Time: 308ms, Memory: 29MB, Status: Passed - CodeTree

--- 240320/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240320/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,17 +1,24 @@
+import heapq
 n = int(input())
 
 arr = []
 for i in range(n):
     a, b = map(int, input().split())
-    arr.append((a, b))
+    arr.append((b, a))
 
-arr.sort(key=lambda x : (x[1], -x[0]))
+arr.sort()
 
-tmp = arr[0][1]
-ans = arr[0][0]
-for i in range(1, n):
-    if tmp != arr[i][1]:
-        ans += arr[i][0]
-        tmp = arr[i][1]
+q = []
+idx = n-1
+ans = 0
+
+for t in range(10000, 0, -1):
+    while idx >= 0 and arr[idx][0] >= t:
+        heapq.heappush(q, -arr[idx][1])
+        idx -= 1
+    
+    if q:
+        ans += -heapq.heappop(q)
+
 
 print(ans)
