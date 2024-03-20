5e7233b79cde5b3261043ed0ac03d187e7f9052b
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-20 17:41:06 +0900

    Title: 회의실 준비 구현, Time: 260ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240320/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+arr = []
+for i in range(n):
+    s, e = map(int, input().split())
+    arr.append((s, e))
+
+arr.sort(key=lambda x: x[1])
+
+ans = 1
+end = 0
+for i in range(len(arr)-1):
+    if end <= arr[i+1][0]:
+        ans += 1
+        end = arr[i+1][1]
+        
+
+print(ans)
