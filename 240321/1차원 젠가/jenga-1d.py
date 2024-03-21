93b1ffa9d0f6e01765752dbf3b5b1e52b8ac9773
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-21 20:46:04 +0900

    Title: 1차원 젠가, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/1차원 젠가/jenga-1d.py
+++ 240321/1차원 젠가/jenga-1d.py
@@ -3,78 +3,87 @@ tmp 초기화
 아래에서 위로 올라오며 비어있지 않을 때만 tmp 채우기 
 tmp를 다시 arr로 복사 
 '''
-# for 2-d arr 
-# BLANK = 0 
-# n = 10 
-# arr = [[0] * n for _ in range(n)] 
-# tmp = [[0] * n for _ in range(n)] 
+# n = int(input()) 
 
-# for row in range(n-1,-1,-1): 
-#     tmp[row][col] = BLANK
+# tower = [int(input()) for _ in range(n)] 
+# s1,e1 = map(int,input().split()) 
+# s2,e2 = map(int,input().split()) 
 
-# tmp_row = n-1 
-# for row in range(n-1,-1,-1): 
-#     if arr[row][col] != BLANK: 
-#         tmp[tmp_row][col] = arr[row][col] 
-#         tmp_row -= 1 
+# def remove(s,e,arr): 
+#     tmp = [0] * len(arr) 
+#     if s == e: 
+#         for i in range(len(arr)): 
+#             if i != s - 1: 
+#                 tmp[i] = arr[i] 
+#             else: 
+#                 continue
+#     else: 
+#         for i in range(len(arr)): 
+#             if i < s-1: 
+#                 tmp[i] = arr[i] 
+#             elif i > e-1: 
+#                 tmp[i] = arr[i] 
+#             else: 
+#                 continue 
+#     return tmp 
 
-# for row in range(n): 
-#     arr[row][col] = tmp[row][col]
+# def clean(arr): 
+#     ans = [] 
+#     for a in arr: 
+#         if a != 0: 
+#             ans.append(a) 
+#     return ans
 
-# for 1-d arr
-# BLANK = 0 
-# end_of_arr = 0 
-# arr = [0] * 6 
-# tmp = [0] * 6 
-# end_of_tmp = 0 
+# mid = clean(remove(s1,e1,tower))
+# ans = clean(remove(s2,e2,mid))
+# if len(ans) == 0: 
+#     print(0) 
+# else: 
+#     print(len(ans))
+#     for a in ans: 
+#         print(a)
 
+# solution 2
+# tmp를 이용, [s,e] 밖의 원소들만 tmp에 옮겨준 뒤, 다시 arr로 복사하는 작업
+# n = int(input()) 
+# numbers = [int(input()) for _ in range(n)] 
+
+# end_of_arr = n 
+# def cut_arr(start,end):
+#     global end_of_arr, numbers 
+
+#     tmp = [] 
+#     for i in range(end_of_arr): 
+#         if i < start or i > end: 
+#             tmp.append(numbers[i]) 
+#     end_of_arr = len(tmp) 
+#     for i in range(end_of_arr): 
+#         numbers[i] = tmp[i] 
+# for _ in range(2): 
+#     s,e = map(int,input().split()) 
+
+#     cut_arr(s-1,e-1) 
+
+# print(end_of_arr)
 # for i in range(end_of_arr): 
-#     if arr[i] != BLANK: 
-#         tmp[end_of_tmp] = arr[i] 
-#         end_of_tmp += 1 
+#     print(numbers[i]) 
 
-# for i in range(end_of_tmp): 
-#     arr[i] = tmp[i] 
-
-# end_of_arr = end_of_tmp
-
+# solution 3 
 n = int(input()) 
+numbers = [int(input()) for _ in range(n)] 
+end_of_arr = n 
 
-tower = [int(input()) for _ in range(n)] 
-s1,e1 = map(int,input().split()) 
-s2,e2 = map(int,input().split()) 
+def cut_arr(start, end): 
+    global end_of_arr
+    cut_len = end - start + 1
+    for i in range(end+1, end_of_arr): 
+        numbers[i-cut_len] = numbers[i] 
+    end_of_arr -= cut_len 
 
+for _ in range(2): 
+    s,e = map(int,input().split()) 
+    cut_arr(s-1,e-1) 
 
-def remove(s,e,arr): 
-    tmp = [0] * len(arr) 
-    if s == e: 
-        for i in range(len(arr)): 
-            if i != s - 1: 
-                tmp[i] = arr[i] 
-            else: 
-                continue
-    else: 
-        for i in range(len(arr)): 
-            if i < s-1: 
-                tmp[i] = arr[i] 
-            elif i > e-1: 
-                tmp[i] = arr[i] 
-            else: 
-                continue 
-    return tmp 
-
-def clean(arr): 
-    ans = [] 
-    for a in arr: 
-        if a != 0: 
-            ans.append(a) 
-    return ans
-
-mid = clean(remove(s1,e1,tower))
-ans = clean(remove(s2,e2,mid))
-if len(ans) == 0: 
-    print(0) 
-else: 
-    print(len(ans))
-    for a in ans: 
-        print(a)
+print(end_of_arr) 
+for i in range(end_of_arr): 
+    print(numbers[i])
