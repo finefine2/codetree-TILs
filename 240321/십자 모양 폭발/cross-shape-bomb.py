5c0ca2ec3bc51769aa2434d783b179426387b3c5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-21 21:24:39 +0900

    Title: 십자 모양 폭발, Time: 356ms, Memory: 26MB, Status: Passed - CodeTree

--- 240321/십자 모양 폭발/cross-shape-bomb.py
+++ 240321/십자 모양 폭발/cross-shape-bomb.py
@@ -32,18 +32,31 @@ def bomb(r,c):
     return tmp 
 
 mid = bomb(start_r,start_c)
+
+
+tmp = [[0] * N for _ in range(N)] 
+for c in range(N): 
+    next_r = N-1 
+    for r in range(N-1,-1,-1): 
+        if mid[r][c]: 
+            tmp[next_r][c] = mid[r][c]
+            next_r -= 1
+for r in range(N): 
+    for c in range(N): 
+        mid[r][c] = tmp[r][c] 
+for m in mid: 
+    print(*m)
 # for m in mid: 
 #     print(*m)
 # tmp 배열을 만들어서 0이 아닌 건 다 이동시킨다 
-def move(arr): 
-    tmp = copy.deepcopy(arr) 
-    # BLANK = 0 
-    # end_of_arr = len(tmp) 
-    for j in range(len(tmp)): 
-        for i in range(len(tmp)-1): 
-            if tmp[i+1][j] == 0 and tmp[i][j] != 0:
-                tmp[i+1][j], tmp[i][j] = tmp[i][j], tmp[i+1][j]
-    return tmp 
-ans = move(mid) 
-for a in ans: 
-    print(*a)
+# def move(arr): 
+#     tmp = copy.deepcopy(arr) 
+#     # BLANK = 0 
+#     # end_of_arr = len(tmp) 
+#     for j in range(len(tmp)): 
+#         for i in range(len(tmp)-1): 
+#             # if tmp[i+1][j] == 0 and tmp[i][j] != 0:
+#             #     tmp[i+1][j], tmp[i][j] = tmp[i][j], tmp[i+1][j]
+#             if tmp[i+1][j] == 0: 
+#                 mid = tmp[i+1][j]
+#     return tmp
