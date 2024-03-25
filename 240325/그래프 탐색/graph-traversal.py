7c2283cf18ee7c5f913103f36cb850a9dc8b7b44
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-25 23:55:17 +0900

    Title: 그래프 탐색, Time: 294ms, Memory: 33MB, Status: Passed - CodeTree

--- 240325/그래프 탐색/graph-traversal.py
+++ 240325/그래프 탐색/graph-traversal.py
@@ -7,7 +7,7 @@ v_cnt = 0
 def dfs(v): 
     global v_cnt
     for i in range(1,N+1): 
-        if graph[v_cnt][i] and not visited[i]: 
+        if graph[v][i] and not visited[i]: 
             visited[i] = True 
             v_cnt += 1
             dfs(i)
