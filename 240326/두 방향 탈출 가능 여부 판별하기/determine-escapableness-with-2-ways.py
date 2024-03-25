a389016b66e74591d7fac9488061d25427a9ac45
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-26 00:06:47 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 168ms, Memory: 28MB, Status: Passed - CodeTree

--- 240326/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240326/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -5,7 +5,7 @@ visited = [[0 for _ in range(M)] for _ in range(N)]
 
 
 def in_range(r,c): 
-    return 0 <= r < N and 0 <= c < N 
+    return 0 <= r < N and 0 <= c < M
 def can_go(r,c): 
     if not in_range(r,c): 
         return False 
@@ -14,15 +14,13 @@ def can_go(r,c):
     return True 
 
 def dfs(r,c): 
-    global order 
     drs, dcs = [0,1],[1,0] 
 
     for dr,dc in zip(drs,dcs):
         new_r, new_c = r + dr, c + dc 
-
         if can_go(new_r,new_c): 
             visited[new_r][new_c] = 1 
-            dfs(new_r)(new_c) 
+            dfs(new_r,new_c) 
 visited[0][0] = 1
 dfs(0,0) 
 
