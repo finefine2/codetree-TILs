3e470a6e95f7c9c3e398d6a6a2c445a00c1d14de
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-26 20:32:22 +0900

    Title: 마을 구분하기, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240326/마을 구분하기/seperate-village.py
+++ 240326/마을 구분하기/seperate-village.py
@@ -1,41 +1,43 @@
 N = int(input()) 
+
 board = [list(map(int,input().split())) for _ in range(N)] 
+visited = [[False for _ in range(N)] for _ in range(N)] 
+
+cnt = 0 
+cnt_list = list() 
 
 def in_range(r,c): 
     return 0 <= r < N and 0 <= c < N 
 
-visited = [[0 for _ in range(N)] for _ in range(N)] 
-
-# wall -> 0 
 def can_go(r,c): 
     if not in_range(r,c): 
-        return False 
-    if board[r][c] == 0 or visited[r][c]: 
+        return False
+    if visited[r][c] or board[r][c] == 0: 
         return False 
     return True 
-cnt = 0 
-ans = []
+
 def dfs(r,c): 
     global cnt 
-    drs,dcs = [1,0,-1,0],[0,1,0,-1] 
 
-    visited[r][c] = 1 
-    for dr, dc in zip(drs,dcs): 
+    drs,dcs = [0,1,0,-1],[1,0,-1,0] 
+
+    for dr,dc in zip(drs,dcs): 
         new_r, new_c = r + dr, c + dc 
 
         if can_go(new_r,new_c): 
-            visited[new_r][new_c] = 1 
+            visited[new_r][new_c] = True
             cnt += 1 
-            dfs(new_r, new_c) 
+            dfs(new_r,new_c) 
+    
 for i in range(N): 
     for j in range(N): 
         if can_go(i,j): 
-            visited[i][j] =1 
-            cnt = 1 
-            dfs(i,j) 
-            ans.append(cnt) 
-ans.sort() 
+            visited[i][j] = True
+            cnt = 1
 
-print(len(ans)) 
-for a in ans: 
-    print(a)
+            dfs(i,j) 
+            cnt_list.append(cnt) 
+cnt_list.sort() 
+print(len(cnt_list)) 
+for c in cnt_list: 
+    print(c)
