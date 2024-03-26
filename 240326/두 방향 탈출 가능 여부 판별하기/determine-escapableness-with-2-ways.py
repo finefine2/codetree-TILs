ab008d815ee2e26505604392779f60c74a77f5de
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-03-26 19:17:50 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 139ms, Memory: 27MB, Status: Passed - CodeTree

--- 240326/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240326/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,11 +1,11 @@
 N,M = map(int,input().split()) 
+
 board = [list(map(int,input().split())) for _ in range(N)] 
-
-visited = [[0 for _ in range(M)] for _ in range(N)] 
-
 
 def in_range(r,c): 
     return 0 <= r < N and 0 <= c < M
+visited = [[0] * M for _ in range(N)] 
+
 def can_go(r,c): 
     if not in_range(r,c): 
         return False 
@@ -13,15 +13,16 @@ def can_go(r,c):
         return False
     return True 
 
-def dfs(r,c): 
-    drs, dcs = [0,1],[1,0] 
 
-    for dr,dc in zip(drs,dcs):
+def dfs(r,c): 
+    drs, dcs = [1,0], [0,1] 
+    for dr,dc in zip(drs, dcs): 
         new_r, new_c = r + dr, c + dc 
         if can_go(new_r,new_c): 
             visited[new_r][new_c] = 1 
             dfs(new_r,new_c) 
-visited[0][0] = 1
+
+visited[0][0] = 1 
 dfs(0,0) 
 
 print(visited[N-1][M-1])
