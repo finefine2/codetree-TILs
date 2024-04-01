1cf95073c14a62b2c3fa7afa8c2dec9dd20a05c1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-04-01 21:44:57 +0900

    Title: 빙하, Time: 192ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240401/빙하/glacier.py
@@ -0,0 +1,38 @@
+from collections import deque
+N,M = map(int,input().split()) 
+
+board = [list(map(int,input().split())) for _ in range(N)] 
+
+def in_range(r,c): 
+    return 0 <= r < N and 0 <= c < N 
+
+drs,dcs = [0,0,1,-1],[1,-1,0,0]
+def bfs(): 
+    num = 0 
+    while q: 
+        r,c = q.popleft()
+        for dr,dc in zip(drs,dcs): 
+            new_r, new_c = r + dr, c + dc 
+            if in_range(new_r,new_c) and not check[new_r][new_c]: 
+                if board[new_r][new_c] == 1: 
+                    check[new_r][new_c] = 1 
+                    num += 1 
+                    board[new_r][new_c] = 0 
+                else: 
+                    check[new_r][new_c] = 1 
+                    q.append((new_r,new_c)) 
+    return num 
+ans = 0 
+t = 0 
+
+while True: 
+    t += 1 
+    q = deque() 
+    check = [[0] * M for _ in range(N)] 
+    q.append((0,0)) 
+    cnt = bfs() 
+    if cnt: 
+        ans = cnt 
+    else: 
+        break 
+print(t - 1, ans)
