908d3209e28f08209229af620cb85deb7c3591ba
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-04-10 18:23:57 +0900

    Title: 코드트리 빵, Time: 211ms, Memory: 28MB, Status: Passed - CodeTree

--- 240410/코드트리 빵/codetree-mon-bread.py
+++ 240410/코드트리 빵/codetree-mon-bread.py
@@ -1,57 +1,90 @@
-from collections import deque
-dir = [(-1,0),(0,-1),(0,1),(1,0)] # 북 서 동 남
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-customer = {}
-customerlist = []  # 딕셔너리에 쉽게 접근하기 위한 리스트
+import sys 
+from collections import deque 
 
-time = 1
-while True:
+INT_MAX = sys.maxsize 
+EMPTY = (-1,-1) 
 
-    # 1. 이동
-    for i in customerlist:
-        if i in customer:
-            visited = [[False]*N for _ in range(N)]
-            visited[customer[i][0]][customer[i][1]] = True
-            q = deque([(customer[i][0],customer[i][1],-1,-1)])  # 뒤는 첫 좌표 표시용
+N,M = tuple(map(int,input().split())) 
+board = [list(map(int,input().split())) for _ in range(N)]
 
-            while q:
-                x, y, dx, dy = q.popleft()
-                if (x,y) == (customer[i][2],customer[i][3]):
-                    customer[i][0], customer[i][1] = dx, dy
+store_list = [] 
+for _ in range(M): 
+    r,c = tuple(map(int,input().split()))
+    store_list.append((r-1,c-1)) 
 
-                    # 2. 편의점 도착
-                    if (dx,dy) == (customer[i][2], customer[i][3]):
-                        graph[dx][dy] = -1
-                        del customer[i]
-                    break
-                for j in range(4):
-                    nx, ny = x+dir[j][0], y+dir[j][1]
-                    if 0<=nx<N and 0<=ny<N and not visited[nx][ny] and graph[nx][ny] != -1:
-                        visited[nx][ny] = True
-                        if (dx,dy) == (-1,-1): q.append((nx,ny,nx,ny))
-                        else: q.append((nx,ny,dx,dy))
+people = [EMPTY] * M 
+curr_t = 0 
+# U L R D 
+drs, dcs = [-1,0,0,1],[0,-1,1,0] 
+step = [[0] * N for _ in range(N)] 
+visited = [[False] * N for _ in range(N)] 
 
-    # 3. t번 사람 투입
-    if 0<time<M+1:
-        visited = [[False]*N for _ in range(N)]
-        a, b = map(int, input().split())
-        q = deque([(a-1,b-1)])
-        visited[a-1][b-1] = True
-        while q:
-            x, y = q.popleft()
-            if graph[x][y] == 1:
-                graph[x][y] = -1  # 사람이 들어간 베이스캠프.
-                customer[time] = [x,y,a-1,b-1]  # 처음 베이스캠프 = 현재위치, 목적지인 편의점
-                customerlist.append(time)
-                break
-            for i in range(4):
-                nx, ny = x+dir[i][0], y+dir[i][1]
-                if 0<=nx<N and 0<=ny<N and not visited[nx][ny] and graph[nx][ny] != -1:  # 이미 꽉 차서 못가는곳은 -1로 표시해줄 예정
-                    visited[nx][ny] = True
-                    q.append((nx,ny))
+def in_range(r,c): 
+    return 0 <= r < N and 0 <= c < N 
+def can_go(r,c): 
+    return in_range(r,c) and not visited[r][c] and board[r][c] != 2
 
-    if not customer:
-        print(time)
-        break
-    time += 1
+def bfs(start_pos): 
+    for i in range(N): 
+        for j in range(N): 
+            visited[i][j] = False
+            step[i][j] = 0 
+    q = deque() 
+    q.append(start_pos) 
+    sr, sc = start_pos
+    visited[sr][sc] = True 
+    step[sr][sc] = 0 
+
+    while q: 
+        r,c = q.popleft() 
+        for dr, dc in zip(drs, dcs): 
+            nr, nc = r + dr, c + dc 
+            if can_go(nr,nc): 
+                visited[nr][nc] = True 
+                step[nr][nc] = step[r][c] + 1 
+                q.append((nr,nc)) 
+
+def simulate(): 
+    # step1 - let those who stay in the grid move toward store direction 
+    for i in range(M): 
+        if people[i] == EMPTY or people[i] == store_list[i]: 
+            continue 
+        bfs(store_list[i]) 
+        pr, pc = people[i] 
+        min_dist = INT_MAX
+        min_r, min_c = -1,-1 
+
+        for dr, dc in zip(drs, dcs): 
+            nr,nc = pr + dr, pc + dc 
+            if in_range(nr,nc) and visited[nr][nc] and min_dist > step[nr][nc]:
+                min_dist = step[nr][nc] 
+                min_r, min_c = nr,nc 
+        people[i] = (min_r, min_c) 
+    for i in range(M): 
+        if people[i] == store_list[i]: 
+            pr, pc = people[i] 
+            board[pr][pc] = 2
+    if curr_t > M: 
+        return 
+    bfs(store_list[curr_t-1]) 
+
+    min_dist = INT_MAX
+    min_r, min_c = -1,-1 
+    for i in range(N): 
+        for j in range(N): 
+            if visited[i][j] and board[i][j] == 1 and min_dist > step[i][j]: 
+                min_dist = step[i][j]
+                min_r, min_c = i,j 
+    people[curr_t - 1] = (min_r, min_c) 
+    board[min_r][min_c] = 2 
+def end(): 
+    for i in range(M): 
+        if people[i] != store_list[i]: 
+            return False 
+    return True 
+while True: 
+    curr_t += 1
+    simulate() 
+    if end(): 
+        break 
+print(curr_t)
