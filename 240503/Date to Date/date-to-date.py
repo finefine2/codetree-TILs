26f4f80c727bd90fa7e62c9719f6c22e2b40b372
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-05-03 16:14:33 +0900

    Title: Date to Date, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/Date to Date/date-to-date.py
+++ 240503/Date to Date/date-to-date.py
@@ -5,7 +5,7 @@ m1, d1, m2, d2 = map(int, input().split())
 answer = 0
 
 if m1 == m2:
-    answer = (d2 - d1)
+    answer = (d2 - d1) + 1
 else:
     for i in range(m1, m2):
         answer += month[i]
