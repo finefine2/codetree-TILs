b5dfc1418d2456230f726f218dc2325cbb06e467
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-05-03 16:14:09 +0900

    Title: Date to Date, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/Date to Date/date-to-date.py
@@ -0,0 +1,14 @@
+month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+m1, d1, m2, d2 = map(int, input().split())
+
+answer = 0
+
+if m1 == m2:
+    answer = (d2 - d1)
+else:
+    for i in range(m1, m2):
+        answer += month[i]
+    answer += (d2 - d1) + 1
+
+print(answer)
