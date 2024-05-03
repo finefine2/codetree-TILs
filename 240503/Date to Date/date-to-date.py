c7940acfaf5097972637ef62b73e87fac0babfd8
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-05-03 16:14:59 +0900

    Title: Date to Date, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/Date to Date/date-to-date.py
+++ 240503/Date to Date/date-to-date.py
@@ -12,3 +12,15 @@ else:
     answer += (d2 - d1) + 1
 
 print(answer)
+
+# arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# a, b, c, d = map(int, input().split())
+
+# if a == c:
+#     print(d - b + 1)
+# else:
+#     for i in range(a, c):
+#         d += arr[i]
+#     d += 1
+#     print(d - b)
