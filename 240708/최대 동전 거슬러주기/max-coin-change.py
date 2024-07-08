820acb9a56dfcc2d32b59629bb9f1be49cc712c6
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-08 17:24:13 +0900

    Title: 최대 동전 거슬러주기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/최대 동전 거슬러주기/max-coin-change.py
+++ 240708/최대 동전 거슬러주기/max-coin-change.py
@@ -7,6 +7,8 @@ dp = [0] * 10001
 for i in range(1, m + 1):
     for j in range(1, n + 1):
         if i - arr[j] >= 0:
+            if dp[i - arr[j]] == 0:
+                continue
             dp[i] = max(dp[i - arr[j]] + 1, dp[i])
 
 if dp[m] == 0:
