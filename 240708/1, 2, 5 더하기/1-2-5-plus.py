c4b6880852600f7f47628155de6b79883fd9b7a5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-08 16:29:21 +0900

    Title: 1, 2, 5 더하기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240708/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,36 @@
+import sys
+n = int(input())
+
+dp = [0] * 1001
+
+arr = [1, 2, 5]
+
+dp[0] = 1
+for i in range(1, n+1):
+    for k in arr:
+        if i >= k:
+            dp[i] += dp[i - k] % 10007
+
+print(dp[n] % 10007)
+
+
+
+
+
+
+# n = int(input())
+
+# dp = [0] * 1001
+
+# num = [1, 2, 5]
+
+# dp[0] = 1
+# for i in range(1, n+1):
+#     for k in num:
+#         if i >= k:
+#             dp[i] += dp[i - k] % 10007
+
+# print(dp[n] % 10007)
+
+# # for k in range(n+2):
+# #     print(dp[k], end = " ")
