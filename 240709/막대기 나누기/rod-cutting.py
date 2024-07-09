b0117ff7f09c3a5253884a55a99a927414e6c093
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-09 17:19:26 +0900

    Title: 막대기 나누기, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240709/막대기 나누기/rod-cutting.py
+++ 240709/막대기 나누기/rod-cutting.py
@@ -11,3 +11,26 @@ for i, num in enumerate(arr):
             dp[j] = max(dp[j], dp[j - i - 1] + num)
 
 print(max(dp))
+
+
+
+
+# n = int(input())
+
+# arr = [0] + list(map(int, input().split()))
+
+# # MAX = 10001
+# dp = [-1] * 10001
+
+# dp[0] = 0
+# for i in range(1, len(dp)):
+#     for j in range(1, len(arr)):
+#         if i >= j:
+#             if dp[i - j] == -1:
+#                 continue
+#             dp[i] = max(dp[i], dp[i - j] + arr[j])
+
+# # for i in range(20):
+# #     print(dp[i], end = " ")
+
+# print(dp[n])
