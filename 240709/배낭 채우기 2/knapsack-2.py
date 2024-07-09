1ef6402090e368c7cde9870af01632b2da6fb6c3
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-09 17:44:27 +0900

    Title: 배낭 채우기 2, Time: 120ms, Memory: 73MB, Status: Passed - CodeTree

--- 240709/배낭 채우기 2/knapsack-2.py
+++ 240709/배낭 채우기 2/knapsack-2.py
@@ -14,3 +14,26 @@ for i in range(n):
             dp[j] = max(dp[j], dp[j - arr[i][0]] + arr[i][1])
 
 print(max(dp))
+
+
+
+
+
+# n, m = map(int, input().split())
+
+# arr = []
+
+# for i in range(n):
+#     a, b = map(int, input().split())
+#     arr.append((a, b))
+
+# dp = [-1] * 10001
+
+# dp[0] = 0
+# for i in range(1, m+1):
+#     for j in range(n):
+#         if i >= arr[j][0]:
+#             dp[i] = max(dp[i], dp[i - arr[j][0]] + arr[j][1])
+
+
+# print(max(dp))
