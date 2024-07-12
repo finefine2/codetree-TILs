f50513c2d0f6246c71c204f7187ede0e69f65edd
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-12 18:28:15 +0900

    Title: 둘 중 하나 잘 고르기, Time: 193ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240712/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+red = [0]
+blue = [0]
+for i in range(2 * n):
+    a, b = map(int, input().split())
+    red.append(a)
+    blue.append(b)
+
+import sys
+MIN = -sys.maxsize
+dp = [[MIN] * (2 * n + 1) for _ in range(2 * n + 1)]
+
+dp[0][0] = 0
+for i in range(1, 2 * n + 1):
+    # j는 지금까지 뽑은 빨간색 수를 뜻한다.
+    for j in range(i+1):
+        if j > 0:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + red[i])
+            # 따라서 레드를 추가할때는 i-1, j-1
+        
+        dp[i][j] = max(dp[i][j], dp[i-1][j] + blue[i])
+
+print(dp[2 * n][n])
