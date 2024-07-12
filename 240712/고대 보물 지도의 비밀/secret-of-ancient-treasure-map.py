ad70350b02823db3e2f1f431e3481fd0b612284f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-12 17:46:05 +0900

    Title: 고대 보물 지도의 비밀, Time: 154ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240712/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+import sys
+ans = -sys.maxsize
+for i in range(n):
+    if arr[i] < 0:
+        for j in range(1, k+1):
+            dp[i][j] = max(dp[i][j], dp[i-1][j] + arr[i])
+            ans = max(ans, dp[i][j])
+    else:
+        for j in range(k+1):
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i])
+            ans = max(ans, dp[i][j])
+
+print(ans)
