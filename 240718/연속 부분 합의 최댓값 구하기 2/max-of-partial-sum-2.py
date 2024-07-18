74c1c6b6e1cb2fe7042983087bab3eb803f0ceed
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-18 18:38:28 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 119ms, Memory: 31MB, Status: Passed - CodeTree

--- 240718/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 240718/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -12,3 +12,44 @@ for i in range(n):
     ans = max(ans, s)
 
 print(ans)
+
+
+
+
+
+
+
+
+
+# n = int(input())
+
+# arr = list(map(int, input().split()))
+
+# import sys
+# ans = -sys.maxsize
+# s = 0
+
+# for i in range(n):
+#     if s < 0:
+#         s = arr[i]
+#     else:
+#         s += arr[i]
+    
+#     ans = max(ans, s)
+
+
+
+# # while idx < n-1:
+
+# #     if s + arr[idx] < 0:
+# #         ans = max(ans, s)
+# #         idx += 1
+# #         s = arr[idx]
+# #         print(ans)
+# #     else:
+# #         s += arr[idx]
+# #         idx += 1
+# #         ans = max(ans, s)
+
+
+# print(ans)
