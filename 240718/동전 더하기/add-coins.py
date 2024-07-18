fec5f478980ebb97c46279d4bf73dbe2db12a8ff
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-18 18:11:35 +0900

    Title: 동전 더하기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/동전 더하기/add-coins.py
+++ 240718/동전 더하기/add-coins.py
@@ -1,16 +1,31 @@
 n, k = map(int, input().split())
 
 arr = []
-for _ in range(n):
+for i in range(n):
     a = int(input())
     arr.append(a)
 
-# 바로 전 동전의 가치의 배수로 주어지므로 뒤에서 부터 큰 순서로 하면 될듯
 ans = 0
-for i in range(n-1, -1, -1):
-    if k // arr[i] > 0:
-        num = (k // arr[i])
-        k -= (num * arr[i])
-        ans += num
+
+for coin in arr[::-1]:
+    ans += k // coin
+    k %= coin
 
 print(ans)
+
+# n, k = map(int, input().split())
+
+# arr = []
+# for _ in range(n):
+#     a = int(input())
+#     arr.append(a)
+
+# # 바로 전 동전의 가치의 배수로 주어지므로 뒤에서 부터 큰 순서로 하면 될듯
+# ans = 0
+# for i in range(n-1, -1, -1):
+#     if k // arr[i] > 0:
+#         num = (k // arr[i])
+#         k -= (num * arr[i])
+#         ans += num
+
+# print(ans)
