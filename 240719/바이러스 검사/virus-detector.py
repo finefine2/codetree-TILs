97e9478098902cc994fb16b2461054813154772f
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-19 12:00:00 +0900

    Title: 바이러스 검사, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240719/바이러스 검사/virus-detector.py
@@ -0,0 +1,13 @@
+# R 식당 개수 
+# P 식당 별 사람 수 
+R = int(input()) 
+P = list(map(int,input().split())) 
+# l 리더 검사인원 
+# m 팀원 검사인원 
+l, m = map(int,input().split()) 
+ans = 0 
+mid = 0 
+for p in P: 
+    mid = (p - l) % m 
+    ans += mid 
+print(ans)
