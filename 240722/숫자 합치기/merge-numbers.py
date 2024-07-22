f5b8d295ae91d9f27175248953519767578c7f65
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-22 15:44:49 +0900

    Title: 숫자 합치기, Time: 364ms, Memory: 33MB, Status: Passed - CodeTree

--- 240722/숫자 합치기/merge-numbers.py
+++ 240722/숫자 합치기/merge-numbers.py
@@ -1,15 +1,22 @@
+import heapq
 n = int(input())
 arr = list(map(int, input().split()))
 
 arr.sort()
-
-idx = 1
-num = arr[0]
 ans = 0
+q = []
 
-while idx <= len(arr) - 1:
-    ans += (num + arr[idx])
-    num += arr[idx]
-    idx += 1
+for k in arr:
+    heapq.heappush(q, k)
+
+while len(q) > 1:
+    x1 = heapq.heappop(q)
+    x2 = heapq.heappop(q)
+
+    ans += x1 + x2
+    heapq.heappush(q, x1 + x2)
+    # heapq를 사용하면 더한 값을 새로 넣었을때
+    # 만약 더한 값이 안에 있는 값보다 더 클 수도 있으니 heapq를 통하여 정렬해주어서
+    # 다시 계산해주면서 진행해야 한다.
 
 print(ans)
