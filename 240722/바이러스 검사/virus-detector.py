3ac576e46fe7073e1b4c510cf25f7cd7f9f4aa51
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-22 15:12:08 +0900

    Title: 바이러스 검사, Time: 422ms, Memory: 125MB, Status: Passed - CodeTree

--- 240722/바이러스 검사/virus-detector.py
+++ 240722/바이러스 검사/virus-detector.py
@@ -1,23 +1,18 @@
-N = int(input()) 
-
-rest = list(map(int,input().split())) 
-
-L, T = map(int,input().split()) 
-
-
+# R 식당 개수 
+# P 식당 별 사람 수 
+R = int(input()) 
+P = list(map(int,input().split())) 
+# l 리더 검사인원 
+# m 팀원 검사인원 
+l, m = map(int,input().split()) 
 ans = 0 
-
-# 먼저 팀장은 무조건 한명씩이라도 사용함 
-# 고객수는 레스토랑마다 존재함 
-for r in rest: 
-    # 오로지 한명 있는 팀장은 일단 제거 
-    r -= L
+ 
+for p in P: 
+    p -= l 
     ans += 1 
-
-    # 만약 식당에 손님수가 남았다는 가정 하에 
-    if r > 0:
-        if r % T == 0: 
-            ans += (r // N)
+    if p > 0: 
+        if p % m == 0: 
+            ans += p // m 
         else: 
-            ans += (r // N)  +1
+            ans += p // m + 1
 print(ans)
