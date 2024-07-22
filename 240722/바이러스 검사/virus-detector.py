c1cd54bded53c93f834fd2594b1885d6cdb9c67c
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-22 15:06:49 +0900

    Title: 바이러스 검사, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240722/바이러스 검사/virus-detector.py
@@ -0,0 +1,23 @@
+N = int(input()) 
+
+rest = list(map(int,input().split())) 
+
+L, T = map(int,input().split()) 
+
+
+ans = 0 
+
+# 먼저 팀장은 무조건 한명씩이라도 사용함 
+# 고객수는 레스토랑마다 존재함 
+for r in rest: 
+    # 오로지 한명 있는 팀장은 일단 제거 
+    r -= 1
+    ans += 1 
+
+    # 만약 식당에 손님수가 남았다는 가정 하에 
+    if r > 0:
+        if r % T == 0: 
+            ans += (r // N) 
+        else: 
+            ans += (r // N) + 1 
+print(ans)
