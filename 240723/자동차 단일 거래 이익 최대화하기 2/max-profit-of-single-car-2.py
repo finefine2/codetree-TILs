f04abf1e0b9cc05816981523b338a017e436f5de
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-23 15:56:18 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 126ms, Memory: 35MB, Status: Passed - CodeTree

--- 240723/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240723/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,14 +1,16 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-ans = -1
-num = arr[0]
-flag = False
-for i in range(1, len(arr)-1):
-    if num > arr[i]:
-        num = arr[i]
-        flag = True
-    if arr[i+1] - num >= 0:
-        ans = max(ans, arr[i+1] - num)
+Max = 0
+Min = arr[0]
+for i in range(n):
+    cost = arr[i] - Min
 
-print(ans)
+    if cost > Max:
+        Max = cost
+    
+    if Min > arr[i]:
+        Min = arr[i]
+
+
+print(Max)
