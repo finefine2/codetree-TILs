c0be8923be10811e905b2052f01b32ea791d63ac
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-24 16:59:35 +0900

    Title: 회의실 겹치지 않게 하기, Time: 342ms, Memory: 30MB, Status: Passed - CodeTree

--- 240724/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 240724/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -5,14 +5,13 @@ for i in range(n):
     a, b = map(int, input().split())
     arr.append((a, b))
 
+arr.sort(key=lambda x : x[1])
+# 끝나는 순으로 정렬한 다음에 앞에서부터 조건에 맞을 경우 차례대로 가져가면 된다.
 ans = 0
-arr.sort(key=lambda x : -x[1])
-
-
-end = 100000
+end = 0
 for k in arr:
-    if k[1] <= end:
-        end = k[0]
+    if k[0] >= end:
+        end = k[1]
         ans += 1
 
 print(n - ans)
