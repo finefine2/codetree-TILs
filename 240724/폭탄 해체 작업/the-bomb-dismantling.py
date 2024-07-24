0206d7bd8e81b8a5d251ee5bc5155b9f6e1587d3
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-07-24 15:42:16 +0900

    Title: 폭탄 해체 작업, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240724/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240724/폭탄 해체 작업/the-bomb-dismantling.py
@@ -16,7 +16,7 @@ ans = 0
 for t in range(10000, 0, -1):
     while idx >= 0 and arr[idx][0] >= t:
         heapq.heappush(q, -arr[idx][1])
-        idx -=1 
+        idx -= 1
     # 아직 idx가 0 이상이고, 시간제한이 아직 t이상일 경우에
     # heap에다가 점수를 내림차순으로 해주기 위해 -로 넣어준다.
     # push 해주었으므로 idx는 1 줄여줌
@@ -24,5 +24,6 @@ for t in range(10000, 0, -1):
     if q:
         ans += -heapq.heappop(q)
     # q가 있을 경우에 for문의 한 차례에서 ans에 더해준다.
+
 
 print(ans)
