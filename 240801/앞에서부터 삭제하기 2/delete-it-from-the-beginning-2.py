a0a151c08f51d5ff84470c717ea46689aef21351
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-08-01 18:58:30 +0900

    Title: 앞에서부터 삭제하기 2, Time: 174ms, Memory: 32MB, Status: Passed - CodeTree

--- 240801/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240801/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -2,18 +2,22 @@ import heapq
 n = int(input())
 arr = list(map(int, input().split()))
 
-total = sum(arr)
-
+q = []
+s = arr[n-1]
 ans = 0
-for k in range(1, n-1):
-    q = arr[k:]
-    heapq.heapify(q)
-    small = heapq.heappop(q)
-    # heapify 해주면 간단
+heapq.heappush(q, arr[n-1])
 
-    s = total - sum(arr[:k]) - small
+for k in range(n-2, 0, -1):
+    heapq.heappush(q, arr[k])
+    # 뒤에서부터 넣어주면서 확인한다.
+    
+    s += arr[k]
+    # 합도 뒤에서부터 추가해준다.
+    # n-1번째는 위에서 미리 넣고 시작
+    small = q[0]
+    av = (s - small) / (n - k - 1)
 
-    if len(q) != 0:
-        ans = max(ans, s / len(q))
+    ans = max(ans, av)
+        
 
 print(f"{ans:.2f}")
