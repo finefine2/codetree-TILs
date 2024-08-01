98561a34f66ddc4a30a1cde65736a34f774a64f1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-08-01 18:53:35 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5047ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240801/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240801/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -2,15 +2,16 @@ import heapq
 n = int(input())
 arr = list(map(int, input().split()))
 
+total = sum(arr)
 
 ans = 0
 for k in range(1, n-1):
     q = arr[k:]
     heapq.heapify(q)
-    heapq.heappop(q)
+    small = heapq.heappop(q)
     # heapify 해주면 간단
 
-    s = sum(q)
+    s = total - sum(arr[:k]) - small
 
     if len(q) != 0:
         ans = max(ans, s / len(q))
