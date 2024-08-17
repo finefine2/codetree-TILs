5730405e63098c71b9c85625a8ab3f02a702bcb7
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-08-17 15:46:30 +0900

    Title: 조삼모사, Time: 1226ms, Memory: 93MB, Status: Passed - CodeTree

--- 240817/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240817/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -9,12 +9,24 @@ def calculate_strength(P, tasks):
             strength += P[tasks[i]][tasks[j]] + P[tasks[j]][tasks[i]]
     return strength
 
+def gen_combi(arr, n):
+    result = []
+    if n == 0:
+        return [[]]
+
+    for i in range(0, len(arr)):
+        elem = arr[i]
+        for C in gen_combi(arr[i + 1:], n - 1):
+            result.append([elem] + C)
+            
+    return result
+
 def min_strength_difference(n, P):
     tasks = list(range(n))
     half_n = n // 2
     min_difference = float('inf')
 
-    for morning_tasks in combinations(tasks, half_n):
+    for morning_tasks in gen_combi(tasks, half_n):
         evening_tasks = [task for task in tasks if task not in morning_tasks]
         
         morning_strength = calculate_strength(P, morning_tasks)
