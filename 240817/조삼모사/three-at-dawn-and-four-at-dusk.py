db56e2092b8d36ca946ea527bfe8737de6cf5c0e
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-08-17 16:13:39 +0900

    Title: 조삼모사, Time: 1269ms, Memory: 94MB, Status: Passed - CodeTree

--- 240817/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240817/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -1,39 +1,42 @@
-from itertools import combinations
+# 아침 조합을 구한다 
+# 저녁 조합을 구한다 
+# 둘 간의 차를 구한다 
 
-N = int(input())
-P = [list(map(int,input().split())) for _ in range(N)] 
-def calculate_strength(P, tasks):
-    strength = 0
-    for i in range(len(tasks)):
-        for j in range(i + 1, len(tasks)):
-            strength += P[tasks[i]][tasks[j]] + P[tasks[j]][tasks[i]]
-    return strength
+N = int(input()) 
+board = [list(map(int,input().split())) for _ in range(N)] 
 
-def gen_combi(arr, n):
+
+def gen_combi(arr,n): 
     result = []
-    if n == 0:
+
+    if n == 0: 
         return [[]]
 
-    for i in range(0, len(arr)):
-        elem = arr[i]
-        for C in gen_combi(arr[i + 1:], n - 1):
-            result.append([elem] + C)
-            
+    for i in range(0,len(arr)): 
+        elem = arr[i] 
+        for C in gen_combi(arr[i+1:], n-1): 
+            result.append([elem]+C) 
     return result
 
-def min_strength_difference(n, P):
-    tasks = list(range(n))
-    half_n = n // 2
-    min_difference = float('inf')
+def calculate_ans(numbers, board): 
+    ans = 0 
 
-    for morning_tasks in gen_combi(tasks, half_n):
-        evening_tasks = [task for task in tasks if task not in morning_tasks]
-        
-        morning_strength = calculate_strength(P, morning_tasks)
-        evening_strength = calculate_strength(P, evening_tasks)
-        
-        difference = abs(morning_strength - evening_strength)
-        min_difference = min(min_difference, difference)
+    for i in range(len(numbers)): 
+        for j in range(i+1,len(numbers)): 
+            ans += board[numbers[i]][numbers[j]] + board[numbers[j]][numbers[i]]
 
-    return min_difference
-print(min_strength_difference(N,P))
+    return ans 
+
+numbers = list(range(N))
+morning_numbers = gen_combi(numbers,N//2) 
+evening_numbers = [num for num in numbers if num not in morning_numbers]
+
+min_diff = 1e9 
+for morning_num in morning_numbers: 
+    evening_num = [num for num in numbers if num not in morning_num] 
+    
+    morning_sum = calculate_ans(morning_num,board) 
+    evening_sum = calculate_ans(evening_num,board)
+
+    min_diff = min(min_diff, abs(morning_sum-evening_sum))
+print(min_diff)
