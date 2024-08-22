9a1435ab4538e40f81715a13f13ceca399c52d61
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-08-22 21:39:47 +0900

    Title: K번 최댓값으로 이동하기, Time: 295ms, Memory: 28MB, Status: Passed - CodeTree

--- 240822/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 240822/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -1,67 +1,77 @@
-from collections import deque
+from collections import deque 
 
-N, K = tuple(map(int,input().split()))
-board = [list(map(int,input().split())) for _ in range(N)]
-r,c = tuple(map(int,input().split()))
+N,K = tuple(map(int,input().split())) 
+board = [list(map(int,input().split())) for _ in range(N)] 
+r,c = tuple(map(int,input().split())) 
+
+curr_pos = (r-1,c-1) 
 
 visited = [[0] * N for _ in range(N)]
-curr_pos = (r-1,c-1)
-NOT_EXISTS = (-1,-1)
+NOT_EXISTS = (-1,-1) 
 
-def initialize():
-    for r in range(N):
-        for c in range(N):
+def initialize(): 
+    for r in range(N): 
+        for c in range(N): 
             visited[r][c] = 0
-def in_range(r,c):
-    return 0<=r<N and 0<=c<N
 
-def can_go(r,c,target_num):
-    return in_range(r,c) and not visited[r][c] and target_num > board[r][c]
+def in_range(r,c): 
+    return 0<=r<N and 0<=c<N 
 
-q = deque()
-def bfs():
-    drs,dcs = [1,0,-1,0],[0,1,0,-1]
+def can_move(r,c,target_num): 
+    return in_range(r,c) and not visited[r][c] and board[r][c] < target_num
+
+q = deque() 
+
+def bfs(): 
+    drs, dcs = [1,0,-1,0],[0,1,0,-1]
     cr,cc = curr_pos
-    visited[cr][cc] = 1
-    q.append(curr_pos)
+    q.append(curr_pos) 
+    visited[cr][cc] = 1 
+    target_num = board[cr][cc] 
 
-    target_num = board[cr][cc]
-    while q:
-        cr,cc = q.popleft()
-        for dr, dc in zip(drs,dcs):
-            nr,nc = cr + dr, cc + dc
-            if can_go(nr,nc,target_num):
-                q.append((nr,nc))
-                visited[nr][nc] = 1
+    while q: 
+        cr,cc = q.popleft() 
+        for dr,dc in zip(drs,dcs): 
+            nr,nc = cr + dr, cc + dc 
 
-def check_update(best_pos,new_pos):
-    if best_pos == NOT_EXISTS:
-        return True
+            if can_move(nr,nc,target_num): 
+                q.append((nr,nc)) 
+                visited[nr][nc] = 1 
+
+def need_update(new_pos,best_pos): 
+    if best_pos == NOT_EXISTS: 
+        return True 
     nr,nc = new_pos
     br,bc = best_pos
-    return (board[nr][nc],-nr,-nc) > (board[br][bc],-br,-bc)
 
-def move():
+    return (board[nr][nc],-nr,-nc) > (board[br][bc],-br,-bc) 
+
+def move(): 
     global curr_pos
-    initialize()
-    bfs() # visited 를 반환
+    initialize() 
+    bfs() 
     best_pos = NOT_EXISTS
-    for r in range(N):
-        for c in range(N):
-            if not visited[r][c] or (r,c) == curr_pos:
-                continue
-            new_pos = (r,c)
-            if check_update(new_pos,best_pos):
+
+    for r in range(N): 
+        for c in range(N): 
+            if not visited[r][c] or (r,c) == curr_pos: 
+                continue 
+            new_pos = (r,c) 
+
+            if need_update(new_pos, best_pos): 
                 best_pos = new_pos
-    if best_pos == NOT_EXISTS:
-        return False
-    else:
+    if best_pos == NOT_EXISTS: 
+        return False 
+    else: 
         curr_pos = best_pos
         return True
-for _ in range(K):
-    is_moved = move()
 
-    if not is_moved:
-        break
-final_r, final_c = curr_pos
-print(final_r+1,final_c+1)
+
+for _ in range(K): 
+    is_moved = move() 
+
+    if not is_moved: 
+        break 
+
+fr, fc = curr_pos
+print(fr+1,fc+1)
