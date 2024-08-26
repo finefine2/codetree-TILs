83870c620dc21e10a9954985024e603ac9d94f77
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-08-26 14:07:19 +0900

    Title: 돌 잘 치우기, Time: 370ms, Memory: 28MB, Status: Passed - CodeTree

--- 240826/돌 잘 치우기/clear-stones-well.py
+++ 240826/돌 잘 치우기/clear-stones-well.py
@@ -1,6 +1,22 @@
 import copy
 from collections import deque
 
+N,K,M = tuple(map(int,input().split()))
+board = []
+# 1: stone, 0: blank,
+stones = []
+for i in range(N):
+    board.append((list(map(int,input().split()))))
+    for j in range(N):
+        if board[i][j] == 1:
+            stones.append((i,j))
+starts = []
+for i in range(K):
+    r,c = tuple(map(int,input().split()))
+    starts.append((r-1,c-1))
+
+visited = [[0] * N for _ in range(N)]
+
 def gen_combi(arr,n):
     res = []
     if n == 0:
@@ -10,38 +26,32 @@ def gen_combi(arr,n):
         for C in gen_combi(arr[i+1:],n-1):
             res.append([elem]+C)
     return res
-N,K,M = tuple(map(int,input().split()))
-board = []
-visited = [[0] * N for _ in range(N)]
-stones = []
-starts = []
-for i in range(N):
-    board.append(list(map(int,input().split())))
-    for j in range(N):
-        if board[i][j] == 1:
-            stones.append((i,j))
-for i in range(K):
-    r,c = tuple(map(int,input().split()))
-    starts.append((r-1,c-1))
-# print(stones)
+
 def initialize_visit():
-    for r in range(N):
-        for c in range(N):
-            if (r,c) not in starts:
-                visited[r][c] = 0
+    for i in range(N):
+        for j in range(N):
+            visited[i][j] = 0
+new_board = copy.deepcopy(board)
+
+def initialize_board():
+    for i in range(N):
+        for j in range(N):
+            board[i][j] = new_board[i][j]
 
 def in_range(r,c):
     return 0<=r<N and 0<=c<N
 
 def bfs():
-    drs,dcs = [1,0,-1,0],[0,1,0,-1]
+    drs, dcs = [1,0,-1,0],[0,1,0,-1]
     while q:
         r,c = q.popleft()
         for dr,dc in zip(drs,dcs):
             nr,nc = r + dr, c + dc
-            if in_range(nr,nc) and not visited[nr][nc] and board[nr][nc] == 1:
+            if in_range(nr,nc) and not visited[nr][nc] and board[nr][nc] == 0:
                 q.append((nr,nc))
                 visited[nr][nc] = 1
+stone_combi = gen_combi(stones,M)
+
 def calc_visit():
     cnt = 0
     for i in range(N):
@@ -49,30 +59,26 @@ def calc_visit():
             if visited[i][j] == 1:
                 cnt += 1
     return cnt
-
-stones_select = gen_combi(stones,M)
-
-# 영역의 최대화 - 돌이 이어진 곳이 최소화
-min_ans = 10000
-for i in range(len(stones_select)):
+# q = deque()
+# for sr,sc in starts:
+#     print(sr, sc)
+#     visited[sr][sc] = 1
+#     q.append((sr,sc))
+# bfs()
+# print(calc_visit())
+max_ans = -1
+for i in range(len(stone_combi)):
+    initialize_board()
     initialize_visit()
-    stones_remove = stones_select[i]
     q = deque()
-    for sr,sc in stones:
-        # print(sr,sc)
-        if (sr,sc) in stones_remove:
-            # print(f"but those have to be removed {sr, sc}")
-            continue
-        else:
-            q.append((sr,sc))
-            visited[sr][sc] = 1
-    # print(f"final queue looks like {q} ")
-    # bfs()
-    mid = calc_visit()
-    # print(mid)
-    min_ans = min(mid,min_ans)
+    for sr,sc in starts:
+        q.append((sr,sc))
+        visited[sr][sc] = 1
 
-    # for b in board:
-    #     print(*b)
-    # print("##############")
-print(N*N - min_ans)
+    stone_remove = stone_combi[i]
+    for sr,sc in stone_remove:
+        board[sr][sc] = 0 
+    bfs()
+    max_ans = max(max_ans,calc_visit())
+
+print(max_ans)
