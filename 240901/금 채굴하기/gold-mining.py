886b65d479824562cbfa235ae2de93ffa920528d
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-09-01 13:04:29 +0900

    Title: 금 채굴하기, Time: 623ms, Memory: 27MB, Status: Passed - CodeTree

--- 240901/금 채굴하기/gold-mining.py
+++ 240901/금 채굴하기/gold-mining.py
@@ -8,11 +8,11 @@ def get_expense(k):
 def in_range(r,c):
     return 0<=r<N and 0<=c<N
 
-def get_board(start_r,start_c):
+def get_gold(start_r,start_c,k):
     cnt = 0
     for i in range(N):
         for j in range(N):
-            if abs(start_r-i) + abs(start_c-j) and in_range(i,j) and board[i][j] == 1:
+            if abs(start_r-i) + abs(start_c-j) <= k and in_range(i,j) and board[i][j] == 1:
                 cnt += 1
     return cnt
 cnt = 0
@@ -20,6 +20,6 @@ cnt = 0
 for k in range(2*N-1):
     for r in range(N):
         for c in range(N):
-            if get_board(r,c) * M - get_expense(k) > 0:
-                cnt = max(cnt,get_board(r,c))
+            if get_gold(r,c,k) * M - get_expense(k) >= 0:
+                cnt = max(cnt,get_gold(r,c,k))
 print(cnt)
