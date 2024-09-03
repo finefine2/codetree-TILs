04f5e1646991591820bb26a1d76e7ebb3098ee75
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-09-03 21:58:12 +0900

    Title: 십자 모양 폭발, Time: 192ms, Memory: 26MB, Status: Passed - CodeTree

--- 240903/십자 모양 폭발/cross-shape-bomb.py
+++ 240903/십자 모양 폭발/cross-shape-bomb.py
@@ -6,16 +6,16 @@ r,c = tuple(map(int,input().split()))
 r -= 1
 c -= 1
 bomb_length = board[r][c]
-
+bomb_length -= 1
 def in_range(r,c):
     return 0<=r<N and 0<=c<N
 
 def find_board(r,c,l):
     start_r,start_c = r,c
-    for s_r in range(r-l-1,r+l+1):
+    for s_r in range(r-l,r+l+1):
         if in_range(s_r,start_c):
             board[s_r][start_c] = 0
-    for s_c in range(c-l-1,c+l+1):
+    for s_c in range(c-l,c+l+1):
         if in_range(start_r,s_c):
             board[start_r][s_c] = 0
 
