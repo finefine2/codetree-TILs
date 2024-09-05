72d13eab9216be87d3f7d1c9563f1206ec7e0e29
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-09-05 20:46:51 +0900

    Title: 자율주행 자동차, Time: 5096ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240905/자율주행 자동차/autonomous-driving.py
+++ 240905/자율주행 자동차/autonomous-driving.py
@@ -1,31 +1,39 @@
-from collections import deque
-
 N,M = tuple(map(int,input().split()))
 r,c,d = tuple(map(int,input().split()))
 board = [list(map(int,input().split())) for _ in range(N)]
 visited = [[0] * M for _ in range(N)]
 
-
 def in_range(r,c):
     return 0<=r<N and 0<=c<M
 
 def can_move(r,c):
-    return board[r][c] == 0
-def bfs():
+    return not visited[r][c] and board[r][c] == 0
+
+def simulate():
+    global r,c,d
     drs,dcs = [-1,0,1,0],[0,1,0,-1]
+    for i in range(4):
+        nd = (d-1) % 4
+        nr,nc = r + drs[nd], c + dcs[nd]
+        if in_range(nr,nc) and can_move(nr,nc):
+            r,c = nr,nc
+            d = nd
+            visited[nr][nc] = 1
+            return True
+        else:
+            d = nd
+    nr,nc = r - drs[d], c - dcs[d]
+    if board[nr][nc] == 1:
+        return False
+    else:
+        return True
 
-    while q:
-        r,c = q.popleft()
-        for dr,dc in zip(drs,dcs):
-            nr,nc = r + dr, c + dc
-            if in_range(nr,nc) and can_move(nr,nc):
-                visited[nr][nc] = 1
-                q.append((nr,nc))
 
-q = deque()
-q.append((r,c))
 visited[r][c] = 1
-bfs()
+while True:
+    is_moved = simulate()
+    if not is_moved:
+        break
 ans = sum(visited[i][j]
           for i in range(N)
           for j in range(M))
