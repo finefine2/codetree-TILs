abead23e2eb204756fd4b18de3cfa82a567bf063
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-09-05 20:55:53 +0900

    Title: 자율주행 자동차, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240905/자율주행 자동차/autonomous-driving.py
+++ 240905/자율주행 자동차/autonomous-driving.py
@@ -1,41 +1,44 @@
 N,M = tuple(map(int,input().split()))
 r,c,d = tuple(map(int,input().split()))
 board = [list(map(int,input().split())) for _ in range(N)]
+
 visited = [[0] * M for _ in range(N)]
 
 def in_range(r,c):
     return 0<=r<N and 0<=c<M
 
-def can_move(r,c):
+def can_move(r,c): 
     return not visited[r][c] and board[r][c] == 0
 
-def simulate():
-    global r,c,d
-    drs,dcs = [-1,0,1,0],[0,1,0,-1]
+def simulate(): 
+    global r,c,d 
+    drs,dcs = [-1,0,1,0],[0,1,0,-1] 
+    # 4방향에 대해서 모두 탐색을 진행한다
     for i in range(4):
-        nd = (d-1) % 4
-        nr,nc = r + drs[nd], c + dcs[nd]
-        if in_range(nr,nc) and can_move(nr,nc):
-            r,c = nr,nc
+        nd = (d-1) % 4 
+        nr,nc = r + drs[nd], c + dcs[nd] 
+        if in_range(nr,nc) and can_move(nr,nc): 
+            visited[nr][nc] = 1 
+            r,c = nr,nc 
             d = nd
-            visited[nr][nc] = 1
             return True
-        else:
+        else: 
             d = nd
-    nr,nc = r - drs[d], c - dcs[d]
-    if board[nr][nc] == 1:
-        return False
-    else:
+    # 만약 못 가면 후진
+    nr,nc = r - drs[d], c - dcs[d] 
+    # 후진하게 되면 좌표 변경 
+    if board[nr][nc] == 1: 
+        return False 
+    else: 
         r,c = nr,nc
         return True
-
-
+    
 visited[r][c] = 1
-while True:
-    is_moved = simulate()
-    if not is_moved:
-        break
+while True: 
+    is_moved = simulate() 
+    if not is_moved: 
+        break 
+        
 ans = sum(visited[i][j]
           for i in range(N)
           for j in range(M))
-print(ans)
