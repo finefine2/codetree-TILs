bfbdfa1aedc1c4f349dcb867d88dd1e1031edeca
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-09-15 21:49:21 +0900

    Title: 나무박멸, Time: 728ms, Memory: 33MB, Status: Passed - CodeTree

--- 240915/나무박멸/tree-kill-all.py
+++ 240915/나무박멸/tree-kill-all.py
@@ -1,77 +1,121 @@
-INF = -10000
-N, M, K, C = map(int, input().split())
-C = -(C+1)                      # 제초제는 음수처리(C+1년 후에 사라짐)
-arr = [[INF]*(N+2)]+[[INF]+list(map(int, input().split()))+[INF] for _ in range(N)]+[[INF]*(N+2)]
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        if arr[i][j]==-1:
-            arr[i][j]=INF       # 건물(벽)을 영구적인 제초제 저리(나무 못자라고, 제초제 못 뻗어감)
+from pprint import pprint
+# board N, period M, range K, lifetime C
+N,M,K,C = tuple(map(int,input().split()))
+# blank 0, wall -1, tree > 0
+board = [list(map(int,input().split())) for _ in range(N)]
 
+spray = [[0] * N for _ in range(N)]
+new_board = [[0] * N for _ in range(N)]
 ans = 0
-for _ in range(M):              # M년동안 진행
-    # [0] 1년의 시작 (제초제 감소)
-    for i in range(1,N+1):
-        for j in range(1,N+1):
-            if arr[i][j]<0:     # 제초제가 뿌려져 있다면 감소 (건물은 -10000 이므로 절대 0 되지 않음)
-                arr[i][j]+=1
+def in_range(r,c):
+    return 0<=r<N and 0<=c<N
 
-    # [1] 인접한 네칸 중 나무있는 칸 수만큼 동시에 성장
-    narr = [x[:] for x in arr]
-    for i in range(1,N+1):
-        for j in range(1,N+1):
-            if arr[i][j]>0:     # 나무가 있다면, 인접 나무수만큼 성장
-                for ni,nj in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
-                    if arr[ni][nj]>0:
-                        narr[i][j]+=1
-    arr=narr
+def grow():
+    drs,dcs = [1,-1,0,0],[0,0,1,-1]
+    # 모든 나무들에 대해서 진행한다
+    for r in range(N):
+        for c in range(N):
+            # 나무는 양수로 주어짐
+            cnt = 0
+            if board[r][c] > 0:
+                for dr,dc in zip(drs,dcs):
+                    nr,nc = r + dr, c + dc
+                    if in_range(nr,nc) and board[nr][nc] > 0:
+                        cnt += 1
+            board[r][c] += cnt
 
-    # [2] 인접한 빈칸에 번식(나무수//빈칸수 => 동시)
-    narr = [x[:] for x in arr]
-    for i in range(1,N+1):
-        for j in range(1,N+1):
-            if arr[i][j]>0:     # 내가 나무면 번식
-                tlst = []       # 빈칸 좌표 저장
-                for ni,nj in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
-                    if arr[ni][nj]==0:
-                        tlst.append((ni,nj))
-                if len(tlst)>0: # 빈칸이 있는 경우 => 번식
-                    d = arr[i][j]//len(tlst)
-                    for ti,tj in tlst:
-                        narr[ti][tj]+=d
-    arr=narr
+def spread():
+    drs,dcs = [1,-1,0,0],[0,0,1,-1]
+    # new_board 초기화 작업
+    for r in range(N):
+        for c in range(N):
+            new_board[r][c] = 0
+    # 모든 나무들에 대해서 진행한다
+    # 번식 가능한 칸에 대해 조사하기
+    for r in range(N):
+        for c in range(N):
+            if board[r][c] > 0:
+                tree_cnt = 0
+                tree_pos = []
+                for dr,dc in zip(drs,dcs):
+                    nr,nc = r + dr, c + dc
+                    # 번식 가능한 칸 갯수 카운팅 + 좌표 기록
+                    if in_range(nr,nc) and board[nr][nc] == 0 and spray[nr][nc] == 0:
+                        tree_cnt += 1
+                        tree_pos.append([nr,nc])
+                for nnr,nnc in tree_pos:
+                    new_board[nnr][nnc] += board[r][c] // tree_cnt
 
-    # [3-1] 가장 많이 박멸되는 칸을 찾기
-    mx, mx_i, mx_j = 0, 0, 0
-    for i in range(1,N+1):
-        for j in range(1,N+1):
-            if arr[i][j]>0:     # 나무 있는 칸에 뿌려야 제초제 확산됨
-                cnt = arr[i][j] # 내 자리(중심) 포함
-                for di,dj in ((-1,-1),(-1,1),(1,-1),(1,1)):
-                    for mul in range(1,K+1):    # 뻗어가면서 처리
-                        ni,nj=i+di*mul, j+dj*mul
-                        if arr[ni][nj]<=0:      # 빈땅, 제초제, 건물
-                            break               # 그 방향은 그만!
-                        else:                   # 나무 있는 경우
-                            cnt+=arr[ni][nj]
-                # 최대값이면 갱신
-                if mx < cnt:
-                    mx, mx_i, mx_j = cnt, i, j
-    if mx==0:   # 0이라면 나무가 한 그루도 없는것! => break
-        break
-    ans+=mx
+    for r in range(N):
+        for c in range(N):
+            board[r][c] += new_board[r][c]
 
-    # [3-2] 제초제 살표
-    # 전파되는 도중 벽이 있거나 나무가 아얘 없는 칸이 있는 경우,
-    # 그 칸 까지는 제초제가 뿌려지며 그 이후의 칸으로는 제초제가 전파되지 않습니다
-    arr[mx_i][mx_j]=C       # 중앙자리에 제초제 뿌림
-    for di, dj in ((-1, -1), (-1, 1), (1, -1), (1, 1)):
-        for mul in range(1, K + 1):  # 뻗어가면서 처리
-            ni, nj = mx_i + di * mul, mx_j + dj * mul
-            # 벽(건물)에 제초제 뿌리면 건물이 시간지나면 빈땅이됨!!!
-            if arr[ni][nj]<=0:      # 뻗어가는것이 종료되는 조건: 빈땅, 제초제뿌려진 빈땅, 벽(건물)을 제외!!
-                if C<=arr[ni][nj]:  # 제초제 뿌리는 조건
-                    arr[ni][nj] = C # 뿌리고
-                break
-            else:                   # 나무면
-                arr[ni][nj] = C
+# pprint(board)
+# grow()
+# print("######")
+# pprint(board)
+# print("######")
+# spread()
+# pprint(board)
+def remove():
+    global ans
+    # 제초제를 뿌리고 사라지는 과정
+    frs,fcs = [1,-1,1,-1],[1,1,-1,-1]
+    # 제초제를 뿌릴 위치 고르기
+    max_del, max_r, max_c = 0,0,0
+    for r in range(N):
+        for c in range(N):
+            # 나무가 있는 칸에 뿌려야 제초제가 전파가 된다
+            if board[r][c] > 0:
+                cnt = board[r][c]
+                for fr, fc in zip(frs,fcs):
+                    for i in range(1,K+1):
+                        nr,nc = r + fr*i, c + fc*i
+                        if in_range(nr,nc):
+                            if board[nr][nc] > 0:
+                                cnt += board[nr][nc]
+                            else:
+                                break
+                if max_del < cnt:
+                    max_del = cnt
+                    max_r, max_c = r,c
+    # print(f"start position is {max_r} and {max_c}")
+
+    ans += max_del
+    # 제초제를 뿌릴 위치를 찾았다
+    # 대각선 방향으로 전파
+    # 4개 방향으로 k칸만큼
+    for fr,fc in zip(frs,fcs):
+        start_r, start_c = max_r,max_c
+        board[start_r][start_c] = 0
+        spray[start_r][start_c] = C
+        for i in range(1,K+1):
+            nr,nc = start_r + fr * i, start_c + fc * i
+            # 제초제에 대한 처리
+            # 제초제 여부는 크게 상관하지 않고 새로 뿌려지면 다시 C년동안 유지가 됨
+            if in_range(nr,nc):
+                # 벽이 있으면
+                if board[nr][nc] == -1:
+                    break
+                # 나무가 아예 없으면 제초제가 뿌려지고 다음칸은 ㄴㄴ
+                elif board[nr][nc] == 0:
+                    spray[nr][nc] = C
+                    break
+                # 나무가 있으면?
+                # 나무가 있는 자리를 제거해야함
+                elif board[nr][nc] > 0:
+                    spray[nr][nc] = C
+                    board[nr][nc] = 0
+def decrease():
+    for r in range(N):
+        for c in range(N):
+            if spray[r][c] > 0:
+                spray[r][c] -= 1
+for i in range(M):
+    # print(f"current turn is {i+1}")
+    grow()
+    spread()
+    decrease()
+    remove()
+
 print(ans)
