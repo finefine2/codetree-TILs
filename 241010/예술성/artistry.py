dbf046c12d21104cc941a017c6df36f5ef2f5ce9
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2024-10-10 15:11:51 +0900

    Title: 예술성, Time: 443ms, Memory: 27MB, Status: Passed - CodeTree

--- 241010/예술성/artistry.py
+++ 241010/예술성/artistry.py
@@ -1,54 +1,65 @@
+from collections import deque 
 N = int(input()) 
 board = [list(map(int,input().split())) for _ in range(N)] 
 
-M = N//2 
+ans = 0 
+def rotate(board): 
+    nboard = [x[:] for x in board] 
+    M = N // 2 
+    # counter clockwise rot for cross 
+    for r in range(N): 
+        nboard[r][M] = board[M][N-r-1] 
+    for c in range(N): 
+        nboard[M][c] = board[c][M] 
+    
+    # clockwise rot for square region 
+    for sr,sc in ((0,0),(0,M+1),(M+1,0),(M+1,M+1)): 
+        for r in range(M): 
+            for c in range(M): 
+                nboard[sr+r][sc+c] = board[sr+M-c-1][sc+r] 
+    return nboard
 
-from collections import deque 
-def bfs(sr,sc): 
+def in_range(r,c): 
+    return 0<=r<N and 0<=c<N 
+
+def find_bfs(r,c): 
     q = deque() 
 
-    q.append((sr,sc)) 
-    v[sr][sc] = 1 
-    groups[-1].add((sr,sc)) 
+    v[r][c] = 1 
+    q.append((r,c)) 
+    groups[-1].add((r,c)) 
 
     while q: 
         cr,cc = q.popleft() 
-        for nr,nc in ((cr-1,cc),(cr+1,cc),(cr,cc-1),(cr,cc+1)): 
-            if 0<=nr<N and 0<=nc<N and v[nr][nc] == 0 and board[cr][cc] == board[nr][nc]: 
+
+        for nr,nc in ((cr-1,cc),(cr,cc+1),(cr+1,cc),(cr,cc-1)): 
+            if in_range(nr,nc) and v[nr][nc] == 0 and board[nr][nc] == board[cr][cc]: 
                 q.append((nr,nc)) 
-                v[nr][nc] = 1 
                 groups[-1].add((nr,nc)) 
-ans = 0 
+                v[nr][nc] = 1 
+ans = 0
 for k in range(4): 
     groups = [] 
     nums = [] 
+    v = [[0] * N for _ in range(N)] 
 
-    v = [[0]*N for _ in range(N)] 
     for r in range(N): 
         for c in range(N): 
             if v[r][c] == 0: 
                 groups.append(set()) 
                 nums.append(board[r][c]) 
-                bfs(r,c) 
+                find_bfs(r,c) 
     CNT = len(nums) 
+
     for i in range(0,CNT-1): 
         for j in range(i+1,CNT): 
-            point = (len(groups[i]) + len(groups[j]))*nums[i]*nums[j]
+            point = (len(groups[i])+len(groups[j])) * nums[i] * nums[j] 
             for cr,cc in groups[i]: 
-                for nr,nc in ((cr-1,cc),(cr+1,cc),(cr,cc-1),(cr,cc+1)): 
+                for nr,nc in ((cr-1,cc),(cr,cc-1),(cr+1,cc),(cr,cc+1)): 
                     if (nr,nc) in groups[j]: 
                         ans += point
     if k == 3: 
         break 
-    nboard = [x[:] for x in board] 
-    for r in range(N): 
-        nboard[M][r] = board[r][M] 
-    for c in range(N): 
-        nboard[c][M] = board[M][N-c-1] 
+    board = rotate(board) 
 
-    for (sr,sc) in ((0,0),(0,M+1),(M+1,0),(M+1,M+1)): 
-        for r in range(M): 
-            for c in range(M): 
-                nboard[sr+r][sc+c] = board[sr+M-c-1][sc+r] 
-    board = nboard
 print(ans)
