a4a714cb3c64a86246608d0bc79114358defaa4a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-04-06 14:54:38 +0900

    Title: 코드트리 빵, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250406/코드트리 빵/codetree-mon-bread.py
+++ 250406/코드트리 빵/codetree-mon-bread.py
@@ -1,145 +1,100 @@
 from collections import deque
 
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
-
-# 베이스캠프와 편의점 위치 저장
-stores = []  # 편의점 위치
-camps = []   # 베이스캠프 위치
-
-for i in range(n):
-    for j in range(n):
-        if board[i][j] == 1:
-            camps.append((i, j))
-
-for _ in range(m):
-    r, c = map(int, input().split())
-    stores.append((r-1, c-1))  # 0-based index로 변환
-
-def in_range(r, c):
-    return 0 <= r < n and 0 <= c < n
-
-def find_shortest_path(sr, sc, er, ec, blocked):
-    """
-    (sr,sc)에서 (er,ec)까지의 최단 경로를 찾는 BFS
-    blocked: 이미 막힌 위치들의 집합
-    """
-    if (sr, sc) == (er, ec):
-        return None
-        
-    q = deque([(sr, sc)])
-    visited = [[False] * n for _ in range(n)]
-    parent = {}  # 경로 추적을 위한 부모 노드 저장
+def init():
+    N, M = map(int, input().split())
+    # 패딩된 보드 생성
+    board = [[1] * (N+2)] + [[1] + list(map(int, input().split())) + [1] 
+            for _ in range(N)] + [[1] * (N+2)]
     
+    # 베이스캠프와 편의점 정보 저장
+    basecamp = set()
+    for r in range(1, N+1):
+        for c in range(1, N+1):
+            if board[r][c] == 1:
+                basecamp.add((r, c))
+                board[r][c] = 0
+    
+    stores = {}
+    for m in range(1, M+1):
+        r, c = map(int, input().split())
+        stores[m] = (r, c)
+    
+    return N, M, board, basecamp, stores
+
+def find_nearest(sr, sc, dests, board, N):
+    """최단 거리의 목적지들을 찾는 BFS"""
+    drs, dcs = [-1, 0, 0, 1], [0, -1, 1, 0]
+    q = deque([(sr, sc)])
+    visited = [[False] * (N+2) for _ in range(N+2)]
     visited[sr][sc] = True
     
     while q:
-        r, c = q.popleft()
+        candidates = []
+        size = len(q)  # 현재 레벨의 크기
         
-        # 4방향 탐색 (상좌우하 우선순위)
-        for dr, dc in [(-1,0), (0,-1), (0,1), (1,0)]:
-            nr, nc = r + dr, c + dc
-            
-            if not in_range(nr, nc):
-                continue
-            if (nr, nc) in blocked:
-                continue
-            if visited[nr][nc]:
-                continue
-                
-            visited[nr][nc] = True
-            q.append((nr, nc))
-            parent[(nr, nc)] = (r, c)
-            
-            if (nr, nc) == (er, ec):
-                # 경로 역추적
-                path = []
-                curr = (nr, nc)
-                while curr in parent:
-                    path.append(curr)
-                    curr = parent[curr]
-                path.append((sr, sc))
-                return path[::-1]
-    
-    return None
-
-def find_nearest_basecamp(store_r, store_c, blocked):
-    """
-    특정 편의점에서 가장 가까운 베이스캠프를 찾는 BFS
-    """
-    q = deque([(store_r, store_c, 0)])
-    visited = [[False] * n for _ in range(n)]
-    visited[store_r][store_c] = True
-    
-    min_dist = float('inf')
-    candidates = []
-    
-    while q:
-        r, c, dist = q.popleft()
-        
-        if dist > min_dist:
-            break
-            
-        if board[r][c] == 1 and (r, c) not in blocked:
-            min_dist = dist
-            candidates.append((r, c))
-            continue
-            
-        for dr, dc in [(-1,0), (0,-1), (0,1), (1,0)]:
-            nr, nc = r + dr, c + dc
-            
-            if not in_range(nr, nc):
-                continue
-            if visited[nr][nc]:
-                continue
-                
-            visited[nr][nc] = True
-            q.append((nr, nc, dist + 1))
-    
-    if not candidates:
-        return None
-    
-    # 가장 위쪽, 왼쪽 베이스캠프 선택
-    return min(candidates)
-
-def simulate():
-    time = 0
-    blocked = set()  # 이미 사용된 공간
-    people = {}      # 현재 이동 중인 사람들 {사람번호: 현재위치}
-    arrived = set()  # 도착한 사람들
-    
-    while len(arrived) < m:
-        # 1. 모든 사람이 한 칸씩 이동
-        new_people = {}
-        for person, pos in people.items():
-            if person in arrived:
-                continue
-                
-            path = find_shortest_path(pos[0], pos[1], 
-                                    stores[person][0], stores[person][1], 
-                                    blocked)
-            if path and len(path) > 1:
-                new_people[person] = path[1]
+        # 현재 레벨의 모든 노드 처리
+        for _ in range(size):
+            cr, cc = q.popleft()
+            if (cr, cc) in dests:
+                candidates.append((cr, cc))
             else:
-                new_people[person] = pos
+                for dr, dc in zip(drs, dcs):
+                    nr, nc = cr + dr, cc + dc
+                    if not visited[nr][nc] and board[nr][nc] == 0:
+                        visited[nr][nc] = True
+                        q.append((nr, nc))
+        
+        # 현재 레벨에서 목적지를 찾았다면 가장 우선순위가 높은 것 반환
+        if candidates:
+            return min(candidates)  # 정렬 대신 min 사용
+    return -1
+
+def solve(N, M, board, basecamp, stores):
+    people = deque()  # (r, c, person_num)
+    arrived = [0] * (M + 1)
+    time = 1
+    
+    while people or time <= M:
+        # 1. 이동 처리
+        next_people = deque()
+        arrived_positions = []
+        
+        for cr, cc, person in people:
+            if arrived[person]:
+                continue
                 
-            # 편의점 도착 체크
-            if new_people[person] == stores[person]:
-                arrived.add(person)
-                blocked.add(stores[person])
+            sr, sc = stores[person]
+            next_pos = find_nearest(sr, sc, 
+                                  {(cr-1,cc), (cr+1,cc), (cr,cc-1), (cr,cc+1)},
+                                  board, N)
+            
+            if next_pos == (sr, sc):  # 편의점 도착
+                arrived[person] = time
+                arrived_positions.append(next_pos)
+            else:
+                next_people.append((next_pos[0], next_pos[1], person))
         
-        people = new_people
+        people = next_people
         
-        # 2. 새로운 사람 출발 (시간이 사람 번호보다 크거나 같을 때)
-        if time < m:
-            store = stores[time]
-            camp = find_nearest_basecamp(store[0], store[1], blocked)
-            if camp:
-                people[time] = camp
-                blocked.add(camp)
+        # 2. 도착 처리
+        for r, c in arrived_positions:
+            board[r][c] = 1
+        
+        # 3. 새로운 사람 베이스캠프 배치
+        if time <= M:
+            sr, sc = stores[time]
+            camp_pos = find_nearest(sr, sc, basecamp, board, N)
+            basecamp.remove(camp_pos)
+            board[camp_pos[0]][camp_pos[1]] = 1
+            people.append((camp_pos[0], camp_pos[1], time))
         
         time += 1
-        
-    return time
+    
+    return max(arrived)
 
-print(simulate())
+def main():
+    N, M, board, basecamp, stores = init()
+    print(solve(N, M, board, basecamp, stores))
+
+if __name__ == "__main__":
+    main()
