02d74f4d7e4082c75e134a848d161aab9a59bdb5
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-04-09 14:27:03 +0900

    Title: 예술성, Time: 122ms, Memory: 20MB, Status: Passed - Codetree

--- 250409/예술성/artistry.py
+++ 250409/예술성/artistry.py
@@ -1,55 +1,50 @@
-# # 단골 부분회전 -> 목적지 좌표 기준
-# # groups nums
 N = int(input())
 board = [list(map(int,input().split())) for _ in range(N)]
-M = N // 2
-
-from collections import deque
-
-
 def in_range(r,c):
     return 0<=r<N and 0<=c<N
-
-def rotate_board(arr):
-    narr = [x[:] for x in arr]
-    # 십자모양
-    for r in range(N):
-        narr[M][r] = arr[r][M]
-    for c in range(N):
-        narr[c][M] = arr[M][N-c-1]
-
-    # 그 외의 영역들
-    for sr,sc in ((0,0),(M+1,0),(0,M+1),(M+1,M+1)):
-        for r in range(M):
-            for c in range(M):
-                narr[sr+r][sc+c] = arr[sr+M-c-1][sc+r]
-    
-    return narr
+from collections import deque
 
 def bfs(sr,sc,value):
     q = deque()
-    v[sr][sc] = 1
-    group = set()
 
-    group.add((sr,sc))
     q.append((sr,sc))
+    v[sr][sc] = 1
+
+    group = set()
+    group.add((sr,sc))
 
     while q:
         cr,cc = q.popleft()
 
         for nr,nc in ((cr-1,cc),(cr,cc+1),(cr+1,cc),(cr,cc-1)):
             if in_range(nr,nc) and v[nr][nc] == 0 and board[nr][nc] == value:
-                q.append((nr,nc))
-                v[nr][nc] = 1
                 group.add((nr,nc))
+                v[nr][nc] = 1
+                q.append((nr,nc))
     return group
+def rotate(arr):
+    narr = [x[:] for x in arr]
+    N = len(arr)
+    M = N // 2
 
+    # 십자모양 회전
+    for r in range(N):
+        narr[M][r] = arr[r][M]
+    for c in range(N):
+        narr[c][M] = arr[M][N-c-1] 
 
+    # 반시계모양 회전
+    for sr,sc in ((0,0),(0,M+1),(M+1,0),(M+1,M+1)):
+        for r in range(M):
+            for c in range(M):
+                narr[sr+r][sc+c] = arr[sr+M-c-1][sc+r]
+    return narr
 
 ans = 0
 for k in range(4):
-    nums = []
+    score = 0
     groups = []
+    nums = []
     v = [[0] * N for _ in range(N)]
 
     for r in range(N):
@@ -59,21 +54,19 @@ for k in range(4):
                 groups.append(count_group)
                 nums.append(board[r][c])
 
-
-
-    score = 0
-    for i in range(len(groups)-1):
-        for j in range(i+1,len(groups)):
-            point = (len(groups[i])+len(groups[j])) * nums[i] * nums[j]
-            for cr,cc in groups[i]:
-                for nr,nc in ((cr-1,cc),(cr+1,cc),(cr,cc-1),(cr,cc+1)):
-                    if (nr,nc)  in groups[j]:
+    # score 계산하는 법
+    M = len(groups)
+    for i in range(M-1):
+        for j in range(i+1,M):
+            for gr,gc in groups[i]:
+                point = nums[i] * nums[j] * (len(groups[i]) + len(groups[j]))
+                for nr,nc in ((gr-1,gc),(gr,gc+1),(gr+1,gc),(gr,gc-1)):
+                    if (nr,nc) in groups[j]:
                         score += point
+
     ans += score
     if k == 3:
         break
 
-
-    board = rotate_board(board)
-
+    board = rotate(board)
 print(ans)
