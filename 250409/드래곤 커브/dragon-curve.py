6d992cef2ef538729818e3028dbb9a49b9a1c604
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-04-09 15:14:03 +0900

    Title: 드래곤 커브, Time: 60ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250409/드래곤 커브/dragon-curve.py
@@ -0,0 +1,27 @@
+N = int(input())
+arr = [[0]*101 for _ in range(101)]
+
+di = [0,-1,0,1]
+dj = [1,0,-1,0]
+for _ in range(N):                      # 드래곤커브 개수만큼 입력 받기
+    sj,si,dr,g = map(int, input().split())
+    lst = [(si,sj)]                     #시작위치 저장
+    lst.append((si+di[dr], sj+dj[dr]))  # 0세대 위치 저장
+    for _ in range(g):                  # 세대 회수만큼 뻗어감
+        # lst의 끝 좌표 기준으로 90도 회전
+        ei,ej = lst[-1]
+        for i in range(len(lst)-2, -1, -1):
+            ci,cj = lst[i]
+            lst.append((ei-(ej-cj), ej+(ei-ci)))
+
+    # arr에 드래곤커브 표시
+    for i,j in lst:
+        arr[i][j]=1
+
+# 2*2 네칸이 모두 1인 경우 찾기
+ans = 0
+for i in range(100):
+    for j in range(100):
+        if arr[i][j]==arr[i][j+1]==arr[i+1][j]==arr[i+1][j+1]==1:
+            ans+=1
+print(ans)
