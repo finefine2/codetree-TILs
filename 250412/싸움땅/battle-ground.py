1cbc0be165bc9cd25046cda7f0cf306797ee4fca
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-04-12 22:32:58 +0900

    Title: 싸움땅, Time: 110ms, Memory: 18MB, Status: Passed - Codetree

--- 250412/싸움땅/battle-ground.py
+++ 250412/싸움땅/battle-ground.py
@@ -1,93 +1,90 @@
-N,M,K = map(int,input().split())
+N, M, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+gun = [[[] for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if arr[i][j]>0:
+            gun[i][j].append(arr[i][j])
 
-board = []
-guns = [[[] for _ in range(N)] for _ in range(N)]
-
-for r in range(N):
-    board.append(list(map(int,input().split())))
-    for c in range(N):
-        if board[r][c] > 0:
-            guns[r][c].append(board[r][c])
-            board[r][c] = 0
-
-drs,dcs = [-1,0,1,0],[0,1,0,-1]
-players = {}
+arr = [[0]*N for _ in range(N)]
+# [0]:i, [1]:j, [2]:dir, [3]:power,[4]:gun,[5]:score  방향 0,1,2,3 (↑, →, ↓, ←)
+players = {}            # 1~M 플레이어
 for m in range(1,M+1):
-    r,c,d,s = map(int,input().split())
-    players[m] = (r-1,c-1,d,s,0,0)
-    board[r-1][c-1] = m 
-opp = {0:2,1:3,2:0,3:1}
-def in_range(r,c): 
-    return 0<=r<N and 0<=c<N
+    i,j,d,p = map(int, input().split())
+    players[m]=[i-1,j-1,d,p,0,0]
+    arr[i-1][j-1]=m
 
-# leave(enemy, er, ec, ed, es, 0, ep)
-def leave(num,cr,cc,cd,cs,cg,cp): 
-    for k in range(4): 
-        nr,nc = cr + (cd+k) % 4, cc + (cd+k) % 4
-        if in_range(nr,nc) and board[nr][nc] == 0: 
-            board[cr][cc] = 0
-            if len(guns[nr][nc]) > 0:
-                cg = max(guns[nr][nc]) 
-                guns[nr][nc].remove(mx_g) 
-            board[nr][nc] = num 
-            players[num] = [nr,nc,(cd+k)%4,cs,cg,cp]
-            return 
-for _ in range(K): 
-    # 순차적으로 이동 
-    # 1-1 첫 플레이어부터 본인 방향대로 한 칸 이동 
-    # 맵 밖으로 빠지게 되면 방향은 반대로
-    for idx in players: 
-        cr,cc,cd,cs,cg,cp = players[idx] 
-        nr,nc = cr + drs[cd], cc + dcs[cd] 
-        if not in_range(nr,nc): 
-            cd = opp[cd] 
-            nr,nc = cr + drs[cd], cc + dcs[cd] 
-        
-        board[cr][cc] = 0 
-        
-        #2-1 이동한 곳에 플레이어가 없다
-        if board[nr][nc] == 0:
-            # 총이 있는지 체크 
-            if len(guns[nr][nc]) > 0: 
-                mx_g = max(guns[nr][nc]) 
-                if cg < mx_g: 
-                    if cg > 0: 
-                        guns[nr][nc].append(cg) 
-                    guns[nr][nc].remove(mx_g) 
-                    cg = mx_g
-            board[nr][nc] = idx 
-            players[idx] = [nr,nc,cd,cs,cg,cp]
-        #2-2-1 이동한 곳에 플레이어가 있다
+opp = {0:2, 1:3, 2:0, 3:1}
+#방향 상,우,하,좌
+di = [-1, 0, 1, 0]
+dj = [ 0, 1, 0,-1]
+
+def leave(num,ci, cj, cd, cp, cg, cs):
+    for k in range(4):      # 현재방향부터 시계방향 90도씩 빈칸 찾기 (최소한 내가온(상대방이 온)칸은 비어있음)
+        ni,nj=ci+di[(cd+k)%4], cj+dj[(cd+k)%4]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+            # 총이 있다면 가장 강한총 획득
+            if len(gun[ni][nj])>0:
+                cg = max(gun[ni][nj])
+                gun[ni][nj].remove(cg)
+            arr[ni][nj]=num # 내 정보 갱신 후 리턴
+            players[num]=[ni,nj,(cd+k)%4,cp,cg,cs]
+            return
+
+for _ in range(K):      # K라운드 동안 경기진행
+    # 1번~M번까지 전체 플레이어 번호순대로 처리
+    for i in players:
+        # [1] 한 칸 이동(격자 벗어나면 반대방향)
+        ci,cj,cd,cp,cg,cs = players[i]
+        ni,nj=ci+di[cd], cj+dj[cd]
+        if ni<0 or N<=ni or nj<0 or N<=nj:  # 범위를 벗어남
+            cd = opp[cd]                    # 방향 반대로
+            ni,nj=ci+di[cd], cj+dj[cd]      # 한 칸 이동
+        arr[ci][cj]=0                       # 이전위치에서 플레이어 표시 제거
+
+        # [2-1] 이동한 위치가 빈칸인 경우 => 강한총 획득
+        if arr[ni][nj]==0:
+            if len(gun[ni][nj])>0:          # 총이 있는 경우
+                mx = max(gun[ni][nj])
+                if cg<mx:                   # 더 강한총이면 교체
+                    if cg>0:                # 총이 있는 경우
+                        gun[ni][nj].append(cg)  # 내총을 바닥에..반납
+                    gun[ni][nj].remove(mx)
+                    cg=mx
+            arr[ni][nj]=i                   # 위치이동
+            players[i]=[ni,nj,cd,cp,cg,cs]  # 정보갱신
+        # [2-2] 빈칸아닌경우 => 싸울 상대방이 있는 경우
         else:
-            # fight
-            enemy = board[nr][nc] 
-            er,ec,ed,es,eg,ep = players[enemy] 
-            if (cs+cg) > (es+eg) or ((cs+cg)==(es+eg) and cs > es): 
-            # 내가 이긴다
-                cp += (cs+cg) - (es+eg)
-                leave(enemy,er,ec,ed,es,0,ep) 
-                if eg > cg: 
-                    if cg > 0: 
-                        guns[nr][nc].append(cg) 
-                    cg = eg
-                else: 
-                    if eg > 0: 
-                        guns[nr][nc].append(eg) 
-                board[nr][nc] = idx 
-                players[idx] = [nr,nc,cd,cs,cg,cp] 
-            else: 
-            # 쟤가 이긴다 
-                ep += (es+eg) - (cs+cg) 
-                leave(idx,nr,nc,cd,cs,0,cp) 
-                if cg > eg: 
-                    if eg > 0: 
-                        guns[nr][nc].append(eg) 
-                    eg = cg 
-                else: 
-                    if cg > 0:
-                        guns[nr][nc].append(cg) 
-                board[nr][nc] = enemy
-                players[enemy] = [nr,nc,ed,es,eg,ep]
-                
-for p in players: 
-    print(p[5],end=" ") 
+            enemy=arr[ni][nj]               # 상대방 번호 확인
+            ei,ej,ed,ep,eg,es = players[enemy]
+            if cp+cg>ep+eg or (cp+cg==ep+eg and cp>ep): # 내가 이기는 경우
+                cs+=(cp+cg)-(ep+eg)         # 공격력 차이만큼 점수 획득
+                leave(enemy,ni,nj,ed,ep,0,es)   # 상대방은 총을 놓고 떠남
+
+                # 이긴 플레이어는 가장 강한 총 얻기: 상대방총 vs 내총
+                if cg<eg:
+                    if cg>0:
+                        gun[ni][nj].append(cg)
+                    cg=eg
+                else:
+                    if eg>0:
+                        gun[ni][nj].append(eg)
+                arr[ni][nj]=i
+                players[i]=[ni,nj,cd,cp,cg,cs]
+            else:                                       # 지는 경우
+                es+=(ep+eg)-(cp+cg)         # 공격력 차이만큼 점수 획득
+                leave(i,ni,nj,cd,cp,0,cs)   # 내가 총 놓고 떠남
+
+                # 이긴 플레이어는 가장 강한 총 얻기: 상대방총 vs 내총
+                if eg<cg:
+                    if eg>0:
+                        gun[ni][nj].append(eg)
+                    eg=cg
+                else:
+                    if cg>0:
+                        gun[ni][nj].append(cg)
+                arr[ni][nj]=enemy
+                players[enemy]=[ni,nj,ed,ep,eg,es]
+
+for i in players:
+    print(players[i][5],end=' ')    # 각 플레이어 점수출력
