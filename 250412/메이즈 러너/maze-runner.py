379f0774c82dddc13c74b3f635c361134af540cb
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-04-12 21:10:56 +0900

    Title: 메이즈 러너, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250412/메이즈 러너/maze-runner.py
+++ 250412/메이즈 러너/maze-runner.py
@@ -1,75 +1,85 @@
-# 10*10 격자, 100턴 (10명 이동, 회전, 값 감소 처리): 시간 충분!
-N, M, K = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-for _ in range(M):
-    i, j = map(lambda x:int(x)-1, input().split())
-    arr[i][j]-=1            # 사람은 -1 (같은 위치에 여러명 있을 수 있음)
+# 0 빈칸
+# 1~9 벽
+# -11 출구
+# 사람 -1
 
-ei,ej=map(lambda x:int(x)-1, input().split())
-arr[ei][ej]=-11             # 비상구 -11
+N,M,K = map(int,input().split())
+
+board = [list(map(int,input().split())) for _ in range(N)]
+
+for _ in range(M):
+    r,c = map(int,input().split())
+    r -= 1
+    c -= 1
+    board[r][c] -= 1
+
+er,ec = map(int,input().split())
+er -= 1
+ec -= 1
+board[er][ec] = -11
 
 def find_square(arr):
-    # [1] 비상구와 모든 사람간의 가장짧은 가로 또는 세로거리 구하기 => L
-    mn = N
-    for i in range(N):
-        for j in range(N):
-            if -11<arr[i][j]<0:     # 사람인 경우
-                mn=min(mn, max(abs(ei-i), abs(ej-j)))
+    # sr,sc,L
+    mn = 2 * N
+    for r in range(N):
+        for c in range(N):
+            if -11 < arr[r][c] <0:
+                mn = min(mn,max(abs(er-r),abs(ec-c)))
 
-    # [2] (0,0)부터 순회하면서 길이 L인 정사각형에 비상구와 사람있는지 체크 => 리턴 L+1
-    for si in range(N-mn):
-        for sj in range(N-mn):                  # 가능한 모든 시작위치
-            if si<=ei<=si+mn and sj<=ej<=sj+mn: # 비상구가 포함된 사각형!
-                for i in range(si, si+mn+1):
-                    for j in range(sj, sj+mn+1):
-                        if -11<arr[i][j]<0:     # 사람인 경우 리턴!
-                            return si,sj,mn+1
-
+    # mn을 찾았으면 이제 참가자 한명 포함하는 정사각형 찾기
+    for sr in range(N-mn):
+        for sc in range(N-mn):
+            if sr <= er <= sr+mn and sc <= ec <= sc + mn:
+                for r in range(sr,sr+mn+1):
+                    for c in range(sc,sc+mn+1):
+                        if -11 < arr[r][c] < 0:
+                            return sr,sc, mn+1
 def find_exit(arr):
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j]==-11:
-                return i,j
-
-# K턴 또는 모두탈출까지 모든 사람의 이동거리 누적, 모두 탈출했으면 종료
-ans = 0
+    for r in range(N):
+        for c in range(N):
+            if arr[r][c] == -11:
+                return r,c
+def in_range(r,c):
+    return 0<=r<N and 0<=c<N
+# 모든 참가자 동시 움직인다 -> nboard
 cnt = M
+ans = 0
+# 모든 참가자 탈출 시 break
 for _ in range(K):
-    # [1] 모든 참가자 (동시에)한 칸 이동(출구 최단거리 방향 상/하 우선)
-    # 출구에 도착하면 즉시 탈출
-    narr = [x[:] for x in arr]
-    for i in range(N):
-        for j in range(N):
-            if -11<arr[i][j]<0: # 사람인 경우
-                dist = abs(ei-i)+abs(ej-j)
-                # 네방향(상하우선), 범위내, 벽아니고 <=0, 거리가 dist보다 작으면
-                for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-                    ni,nj = i+di, j+dj
-                    if 0<=ni<N and 0<=nj<N and arr[ni][nj]<=0 and dist>(abs(ei-ni)+abs(ej-nj)):
-                        ans+=arr[i][j]          # 현재 인원수가 이동하는 것이니 이동거리에 누적
-                        narr[i][j]-=arr[i][j]   # 이동처리
-                        if arr[ni][nj]==-11:    # 비상구인 경우
-                            cnt+=arr[i][j]      # 탈출!
-                        else:                   # 일반빈칸 또는 사람있는자리
-                            narr[ni][nj]+=arr[i][j] # 들어온 인원 추가
+    nboard = [x[:] for x in board]
+    # 1 모든 참가자가 한칸씩 동시에 움직임
+        # 상하 좌우
+        # 현재 거리보다
+        # 벽 없는 곳으로 이동
+    for r in range(N):
+        for c in range(N):
+            if -11<board[r][c]<0:
+                dist = abs(er-r) + abs(ec-c)
+                for dr,dc in ((-1,0),(1,0),(0,-1),(0,1)):
+                    nr,nc = r + dr, c + dc
+                    if in_range(nr,nc) and board[nr][nc] <=0 and dist > (abs(er-nr)+abs(ec-nc)):
+                        ans += board[r][c]
+                        nboard[r][c] -= board[r][c]
+                        if board[nr][nc] == -11:
+                            cnt += board[r][c]
+                        else:
+                            nboard[nr][nc] += board[r][c]
                         break
-    arr = narr
-    if cnt==0:
+    board = nboard
+    if cnt == 0:
         break
+    # 미로 회전
+    # 참가자와 출구 찾는 가장 작은 정사각형 찾기
+    sr,sc,L = find_square(board)
+    nboard = [x[:] for x in board]
+    for r in range(L):
+        for c in range(L):
+            nboard[sr+r][sc+c] = board[sr+L-c-1][sc+r]
+            if nboard[sr+r][sc+c] > 0:
+                nboard[sr+r][sc+c] -= 1
+    board = nboard
 
-    # [2] 미로회전(출구와 한 명이상 참가자를 포함하는 가장 작은 정사각형
-    # 시계방향 90도: 같은크기->좌상단행열, 내구도 -1
-    si,sj,L = find_square(arr)
-
-    narr = [x[:] for x in arr]
-    for i in range(L):
-        for j in range(L):
-            narr[si+i][sj+j]=arr[si+L-1-j][sj+i]
-            if narr[si+i][sj+j]>0:      # 벽이면 회전시 1감소
-                narr[si+i][sj+j]-=1
-    arr = narr
-    # 회전으로 달라졌으므로... 비상구 위치 저장
-    ei,ej = find_exit(arr)
+    er,ec = find_exit(board)
 
 print(-ans)
-print(ei+1,ej+1)
+print(er+1,ec+1)
